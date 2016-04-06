2f40209fc7e78eb481d854d5118e348c3a0d0c6d
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-06 16:50:27 +0200

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process

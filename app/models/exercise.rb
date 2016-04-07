653667f2f7e8cf5ccbb3993149cb4de12527b75a
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-07 21:01:22 +0200

    load tests from file

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,15 +1,20 @@
 class Exercise
-  DATA_DIR = "#{Rails.root}/data/exercises".freeze
+  DATA_DIR = Rails.root.join('data', 'exercises').freeze
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
+  def tests
+    file = File.join(DATA_DIR, name, 'tests.js')
+    File.read(file) if FileTest.exists?(file)
+  end
+
   def self.all
     Dir.entries(DATA_DIR)
-       .select { |entry| File.directory?(File.join(DATA_DIR, entry)) && !(entry == '.' || entry == '..') }
-       .map { |entry| Exercise.new(entry) }
+      .select { |entry| File.directory?(File.join(DATA_DIR, entry)) && !(entry == '.' || entry == '..') }
+      .map { |entry| Exercise.new(entry) }
   end
 
   def self.find(name)

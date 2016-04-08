14033b244f76caf2489f4d186ad2c81c5d518499
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-08 15:55:07 +0200

    use github flavoured markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def markdown(source)
-    Kramdown::Document.new(source).to_html.html_safe
+    Kramdown::Document.new(source, :input => 'GFM').to_html.html_safe
   end
 end

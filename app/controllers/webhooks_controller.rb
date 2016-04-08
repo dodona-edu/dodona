cce98cf7157137f8aa4925a67a01a5306a69ff7b
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-08 14:35:25 +0200

    skip auth checking for webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,6 @@
 class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def update_exercises
     response = Exercise.refresh
     message = response[1]

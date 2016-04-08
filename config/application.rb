422d8b78e999a01958581d663ab18d768d3f6195
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-08 14:10:33 +0200

    require English

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require "English"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)

c5b6a8ad97fa3fd7c226ed812f4c8f7894ad654e
dmouriss <dieter.mourisse@ugent.be>
2016-04-14 14:05:41 +0200

    lightbox

---
+++ app/assets/javascripts/lightbox.js
@@ -0,0 +1,15 @@
+$(function() {
+    $("img[class=lightbox]").click(function() {
+        var imagesrc = $(this).attr('src');
+        var alttext = $(this).attr('alt')
+        alttext = alttext ? alttext : imagesrc.split("/").pop();
+        Strip.show({
+            url: imagesrc,
+            caption: alttext,
+            options: {
+                side: 'top',
+                maxHeight: 10
+            },
+        });
+    });
+});

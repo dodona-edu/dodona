6f91ce5bde55ec54b726864fa152d027b0609337
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-17 22:05:09 +0200

    fix attribute permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,6 +74,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.fetch(:user, {})
+    params.require(:user).permit(:username, :ugent_id, :first_name, :last_name, :email)
   end
 end

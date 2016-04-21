65eda5f1eef5a08804b50704c4e9f19d953bc161
dmouriss <dieter.mourisse@ugent.be>
2016-04-21 17:14:10 +0200

    online python tutor: first version

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,5 @@ class Submission
 
     def sourceCode
         file = File.join(Rails.root, 'testcode.js')
-        File.read(file)
     end
 end
--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -7,4 +7,6 @@
 //=require onlinepythontutor/pytutor.js
 //=require onlinepythontutor/jquery.jsPlumb-1.3.10-all-min
 //=require onlinepythontutor/d3.v2.min.js
+//=require onlinepythontutor/jquery.simplemodal.js
+//=require onlinepythontutor/jquery.ba-bbq.min.js
 //=require onlinepythontutor/jquery-ui-1.8.24.custom.min

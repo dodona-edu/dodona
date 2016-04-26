2bcbaf5a9f734e571b4f21c1d987ca53c9a6c7a3
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-04-26 13:40:30 +0200

    view submissions

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -1,5 +1,5 @@
 class SubmissionsController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_submission, only: [:show]
 
   def index
     authorize Submission

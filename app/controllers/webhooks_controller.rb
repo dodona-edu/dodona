fdd68fa78e276333297f8083044bd62f9e34b537
dmouriss <dieter.mourisse@ugent.be>
2016-04-27 15:02:13 +0200

    Fixes #45

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,10 +1,24 @@
+require 'set'
+
 class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update_exercises
-    response = Exercise.refresh
+    commits = params['commits']
+
+    #Build set with all exercises that need to be updated
+    changed = Set.new
+    for commit in commits
+        ['added', 'removed', 'modified'].each { |type| changed |= commit[type].map {|filename| filename.split('/')[0]}}
+    end
+
+    response = Exercise.refresh(changed)
     message = response[1]
     status = response[0] == 0 ? 200 : 500
     render plain: message, status: status
+  end
+
+  def test_page
+    @title = 'webhook request'
   end
 end

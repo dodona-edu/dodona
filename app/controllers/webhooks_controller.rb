0abfd203824f97ff265d3b6a562c243b0ec20d01
dmouriss <dieter.mourisse@ugent.be>
2016-04-28 10:15:35 +0200

    update webhook so that empty request updates all exercises

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,17 +4,25 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update_exercises
-    commits = params['commits']
 
     #Build set with all exercises that need to be updated
     changed = Set.new
-    for commit in commits
-        ['added', 'removed', 'modified'].each { |type| changed |= commit[type].map {|filename| filename.split('/').first}}
+    if params.key?('commits')
+      commits = params['commits']
+
+      for commit in commits
+          ['added', 'removed', 'modified'].each { |type| changed |= commit[type].map {|filename| filename.split('/').first}}
+      end
+    else
+      changed.add("UPDATE_ALL")
     end
 
     response = Exercise.refresh(changed)
     message = response[1]
     status = response[0] == 0 ? 200 : 500
     render plain: message, status: status
+  end
+
+  def test_page
   end
 end

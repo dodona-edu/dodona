0bb7f76edd342d0484b5d732d13ddb8e4714cb60
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-07-15 15:32:28 +0200

    clean up

--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -5,17 +5,17 @@ class RepositoriesControllerTest < ActionDispatch::IntegrationTest
     @repository = repositories(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get repositories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_repository_url
     assert_response :success
   end
 
-  test "should create repository" do
+  test 'should create repository' do
     assert_difference('Repository.count') do
       post repositories_url, params: { repository: { judge_id: @repository.judge_id, name: @repository.name, path: @repository.path, remote: @repository.remote } }
     end
@@ -23,22 +23,22 @@ class RepositoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to repository_url(Repository.last)
   end
 
-  test "should show repository" do
+  test 'should show repository' do
     get repository_url(@repository)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_repository_url(@repository)
     assert_response :success
   end
 
-  test "should update repository" do
+  test 'should update repository' do
     patch repository_url(@repository), params: { repository: { judge_id: @repository.judge_id, name: @repository.name, path: @repository.path, remote: @repository.remote } }
     assert_redirected_to repository_url(@repository)
   end
 
-  test "should destroy repository" do
+  test 'should destroy repository' do
     assert_difference('Repository.count', -1) do
       delete repository_url(@repository)
     end

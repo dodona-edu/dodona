6bc3c7b9d65dc08dc371b100212b4fe5eff3181b
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-08-03 18:33:31 +0200

    load submission results from exercise page

--- app/assets/javascripts/i18n/translations.js
+++ app/assets/javascripts/i18n/translations.js
@@ -1,3 +1,3 @@
 I18n.translations || (I18n.translations = {});
-I18n.translations["en"] = I18n.extend((I18n.translations["en"] || {}), {"js":{"solution-saved":"Solution saved"}});
-I18n.translations["nl"] = I18n.extend((I18n.translations["nl"] || {}), {"js":{"solution-saved":"Oplossing opgeslagen"}});
+I18n.translations["en"] = I18n.extend((I18n.translations["en"] || {}), {"js":{"submission-failed":"Oops, something went wrong while saving your solution. Reload this page, try again, or contact the teaching assistant.","submission-saved":"Submission saved"}});
+I18n.translations["nl"] = I18n.extend((I18n.translations["nl"] || {}), {"js":{"submission-failed":"Oeps, er ging iets fout bij het opslaan van je oplossing. Herlaad de pagina, probeer opnieuw, of contacteer de assistent.","submission-saved":"Oplossing opgeslagen"}});

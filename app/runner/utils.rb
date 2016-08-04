2874574cd5b4e925db4cb2059ef0a8678b3f1995
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-08-04 11:39:33 +0200

    do merge hashes when values have a different class. Closes #107

--- app/runner/utils.rb
+++ app/runner/utils.rb
@@ -3,10 +3,7 @@ class Utils
     source.keys.each do |key|
       value = source[key]
       if original.include?(key)
-        if value.class != original[key].class
-          # merging in this case wouldn't make sense
-          raise 'merging incompatible hashes'
-        elsif value.class == Hash
+        if value.is_a?(Hash) && original[key].is_a?(Hash)
           # hashes get merged recursively
           update_config(original[key], value)
         else

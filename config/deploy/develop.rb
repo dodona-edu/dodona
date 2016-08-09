27483cff4f01e6a3f30f7ee46714705b7ed2e882
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-08-09 17:52:04 +0200

    deploy develop

--- config/deploy/develop.rb
+++ config/deploy/develop.rb
@@ -5,5 +5,5 @@ server 'naos.ugent.be', user: 'dodona', roles: [:web, :app, :db], ssh_options: {
   port: 4840
 }
 
-set :branch, 'feature/feedback-table2'
+set :branch, 'develop'
 set :rails_env, :development

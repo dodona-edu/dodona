37163c586b66fef8852fcb2d398c1799c384eb78
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-08-15 21:04:56 +0900

    deploy production to naos

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'dodona.ugent.be', user: 'dodona', port: '4840', roles: %w(app db web)
+server 'naos.ugent.be', user: 'dodona', port: '4840', roles: %w(app db web)
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 

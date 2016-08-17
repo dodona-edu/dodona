57e6a469ca43504fd4f7dad200283f0e830f125e
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-08-17 02:55:51 +0200

    better rendering of precourse exercises

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,8 @@ class PagesController < ApplicationController
 
   def precourse
     @title = 'Precourse'
+    @exercises = {
+      'Lecture 1': [[53, 46, 52, 43, 25, 57], [45, 40, 37, 39]]
+    }
   end
 end

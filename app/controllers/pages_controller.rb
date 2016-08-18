c81268c239a68ff42206dcfa72f1d48e253c1d0c
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-08-18 04:02:47 +0200

    add exercises for lecture 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,9 @@ class PagesController < ApplicationController
 
   def precourse
     @title = 'Precourse'
-    @exercises = {
-      'Lecture 1': [[99, 94, 127, 139, 128, 104], [114, 138, 105, 97]]
-    }
+    @exercises = [
+      ['Lecture 1', [99, 94, 127, 139, 128, 104], [114, 138, 105, 97]],
+      ['Lecture 2', [151, 174, 175, 154, 191, 161], [169, 149, 168, 185, 167]],
+    ]
   end
 end

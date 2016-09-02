c5085c26418a54f2e4339cd077cf1d3d26c11c73
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-02 15:06:12 +0200

    remove precourse page and links to it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,4 @@ class PagesController < ApplicationController
   def home
     @title = 'Home'
   end
-
-  def precourse
-    @title = 'Precourse'
-    @exercises = [
-      ['Lecture 1', [99, 94, 127, 139, 128, 104], [114, 138, 105, 97]],
-      ['Lecture 2', [151, 174, 175, 155, 191, 161], [169, 149, 168, 185, 167]],
-      ['Lecture 3', [197, 248, 247, 207, 198, 224], [220, 243, 249, 215]]
-    ]
-  end
 end

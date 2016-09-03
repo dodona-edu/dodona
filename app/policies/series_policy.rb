20e824bc7440e8bd1a81cc6369196b6c6844d545
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-09-03 14:08:35 +0200

    add policy

---
+++ app/policies/series_policy.rb
@@ -0,0 +1,43 @@
+class SeriesPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def index?
+    user && user.admin?
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    user && user.admin?
+  end
+
+  def edit?
+    user && user.admin?
+  end
+
+  def create?
+    user && user.admin?
+  end
+
+  def update?
+    user && user.admin?
+  end
+
+  def destroy?
+    user && user.admin?
+  end
+
+  def permitted_attributes
+    if user && user.admin?
+      [:name, :description, :course_id, :visibility, :order]
+    else
+      []
+    end
+  end
+end

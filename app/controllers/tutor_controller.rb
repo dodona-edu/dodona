571dab08834a217ef76590ce3ec54e8be067de13
dmouriss <dieter.mourisse@ugent.be>
2016-09-09 14:50:46 +0200

    Update tutor - cleaned up fullscreen code

--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -9,27 +9,24 @@ class TutorController < ApplicationController
         submission = Submission.find(params[:submission_id])
         authorize submission
 
-        if params.key?("fullscreen")
-            @fullscreen = true
-            file = File.join(DATA_DIR, params[:submission_id] + '-' + params[:id])
-            f = File.new(file, "w")
-            f.write(submission.code)
-            f.write("\n")
-            f.write("echo('I am Dieter')")
-            f.close
+        file = File.join(DATA_DIR, params[:submission_id] + '-' + params[:id])
+        f = File.new(file, "w")
+        f.write(submission.code)
+        f.write("\n")
+        f.write("echo('I am Dieter')")
+        f.close
 
-            codefile = file
-            @json_traceback = `node --expose-debug-as=Debug ~/jslogger.js --jsondump=true #{codefile}`
-              .strip
-              .gsub!('\\n', '\\\\\\n')
-              .gsub!("\"", "\\\"")
-              .gsub!("\'", "\\\\\'")
-              .html_safe
+        codefile = file
+        @json_traceback = `node --expose-debug-as=Debug ~/jslogger.js --jsondump=true #{codefile}`
+          .strip
+          .gsub!('\\n', '\\\\\\n')
+          .gsub!("\"", "\\\"")
+          .gsub!("\'", "\\\\\'")
+          .html_safe
 
-            @json_traceback = ('"' + @json_traceback + '"').html_safe
+        @json_traceback = ('"' + @json_traceback + '"').html_safe
 
-            render layout: false
-            File.delete(file)
-        end
+        render layout: false
+        File.delete(file)
     end
 end

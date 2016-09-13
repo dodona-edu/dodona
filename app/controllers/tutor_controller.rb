d97cceb6c1d608cd3ea92db23f4c8c501fdb4895
dmouriss <dieter.mourisse@ugent.be>
2016-09-13 15:35:44 +0200

    Update tutor - using static code

--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -1,7 +1,5 @@
 class TutorController < ApplicationController
 
-    DATA_DIR = Rails.root.join('tmp', 'tutor').freeze
-
     def index
     end
 
@@ -9,24 +7,21 @@ class TutorController < ApplicationController
         submission = Submission.find(params[:submission_id])
         authorize submission
 
-        file = File.join(DATA_DIR, params[:submission_id] + '-' + params[:id])
-        f = File.new(file, "w")
-        f.write(submission.code)
-        f.write("\n")
-        f.write("echo('I am Dieter')")
-        f.close
+        @tutor_code = "def f():\n    return 2\n\ndef (g):\n    return 3\n\nd = dict()\nd['a'] = ['1', '2', '3']\n".strip
+                          .gsub("\n", '\\n')
+                          .gsub("\"", "\\\"")
+                          .gsub("\'", "\\\\\'")
+                          .html_safe
 
-        codefile = file
-        @json_traceback = `node --expose-debug-as=Debug ~/jslogger.js --jsondump=true #{codefile}`
-          .strip
-          .gsub!('\\n', '\\\\\\n')
-          .gsub!("\"", "\\\"")
-          .gsub!("\'", "\\\\\'")
-          .html_safe
+        @tutor_code = '"' + @tutor_code + '"'
+        @tutor_code = @tutor_code.html_safe
 
-        @json_traceback = ('"' + @json_traceback + '"').html_safe
+        @tutor_input = '["1", "2"]'.strip()
+                          .gsub("\"", "\\\"")
+                          .gsub("\'", "\\\\\'").html_safe
+        @tutor_input = '"' + @tutor_input + '"'
+        @tutor_input = @tutor_input.html_safe
 
-        render layout: false
-        File.delete(file)
+
     end
 end

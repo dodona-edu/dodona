163f1425684e1354e5136d6e8120c727f0ecdcae
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-16 16:52:02 +0200

    move runtime metrics to pythia runner

--- app/runners/submission_runner.rb
+++ app/runners/submission_runner.rb
@@ -264,30 +264,6 @@ class SubmissionRunner
   end
 
   def add_runtime_metrics(result)
-    metrics = result['runtime_metrics']
-
-    metrics = {} if metrics.nil?
-
-    unless metrics.key?('wall_time')
-      value = last_timestamp(File.join(@path, 'resources', 'user_time.logs')) / 1000.0
-      metrics['wall_time'] = value
-
-      value = logged_value_range(File.join(@path, 'resources', 'user_time.logs')) / 100.0
-      metrics['user_time'] = value
-
-      value = logged_value_range(File.join(@path, 'resources', 'system_time.logs')) / 100.0
-      metrics['system_time'] = value
-    end
-
-    unless metrics.key?('peak_memory')
-      value = logged_value_range(File.join(@path, 'resources', 'memory_usage.logs'))
-      metrics['peak_memory'] = value
-
-      value = logged_value_range(File.join(@path, 'resources', 'anonymous_memory.logs'))
-      metrics['peak_anonymous_memory'] = value
-    end
-
-    result['runtime_metrics'] = metrics
   end
 
   def finalize

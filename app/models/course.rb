9e6845be319c34478a0cdbf7a65bea4edd0c4e35
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-09-18 09:54:45 +0200

    add description to courses in database

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,14 +2,16 @@
 #
 # Table name: courses
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  year       :string(255)
-#  secret     :string(255)
-#  open       :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  year        :string(255)
+#  secret      :string(255)
+#  open        :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :text(65535)
 #
+
 require 'securerandom'
 
 class Course < ApplicationRecord

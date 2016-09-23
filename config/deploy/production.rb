bcdbe4c76fbf5bee7c7c90777d68b9d3d12441e3
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-09-23 17:51:03 +0200

    tweak deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,12 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'dodona.ugent.be', user: 'dodona', port: '4840', roles: %w(app web db worker)
-#server 'sisyphus.ugent.be', user: 'dodona', port: '4840', roles: %w(app worker)
+server 'dodona.ugent.be', user: 'dodona', port: '4840', roles: %w(app web db)
+server 'sisyphus.ugent.be', user: 'dodona', port: '4840', roles: %w(app worker)
 
-set :branch, 'develop'
+set :branch, 'production'
 
-set :delayed_job_workers, 5
+set :delayed_job_workers, 3
 
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}

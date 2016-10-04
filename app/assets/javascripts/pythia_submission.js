336b81addf11b88de868257a23233d4ed698fd4f
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-10-04 16:04:31 +0200

    clean code

--- app/assets/javascripts/pythia_submission.js
+++ app/assets/javascripts/pythia_submission.js
@@ -1,15 +1,15 @@
 function init_pythia_submission_show(submissionCode) {
     var vars;
 
-    function init() {  
-      $('.tutorlink').click(function() {
-        var $element = $(this).parents(".group");
-        var stdin = $element.data('stdin').slice(0, -1);
-        var statements = $element.data('statements');
-        loadTutor(submissionCode, statements, JSON.stringify(stdin.split('\n')));
-        return false;
-      })
-    };
+    function init() {
+        $('.tutorlink').click(function () {
+            var $element = $(this).parents(".group");
+            var stdin = $element.data('stdin').slice(0, -1);
+            var statements = $element.data('statements');
+            loadTutor(submissionCode, statements, JSON.stringify(stdin.split('\n')));
+            return false;
+        });
+    }
 
     function loadTutor(studentCode, statements, stdin) {
         var lines = studentCode.split('\n');
@@ -17,18 +17,18 @@ function init_pythia_submission_show(submissionCode) {
         var i = 0;
         var remove = false;
         var source_array = [];
-        while ( i < lines.length) {
-          if (remove && !(lines[i].match(/\s+.*/g))) {
-            remove = false;
-          }
+        while (i < lines.length) {
+            if (remove && !(lines[i].match(/\s+.*/g))) {
+                remove = false;
+            }
 
-          if (lines[i].match(/if\s+__name__\s*==\s*(['"])__main__\s*\1:\s*/g)) {
-            remove = true;
-          }
-          if (!remove) {
-            source_array.push(lines[i]);
-          }
-          i += 1;
+            if (lines[i].match(/if\s+__name__\s*==\s*(['"])__main__\s*\1:\s*/g)) {
+                remove = true;
+            }
+            if (!remove) {
+                source_array.push(lines[i]);
+            }
+            i += 1;
         }
 
         source_array.push(statements);
@@ -36,31 +36,34 @@ function init_pythia_submission_show(submissionCode) {
         var source_code = source_array.join('\n');
 
         $.ajax({
-              type: 'POST',
-              url: 'http://localhost:8080/cgi-bin/build_trace.py',
-              dataType: 'json',
-              data: {code: source_code, input: stdin},
-              success: function(data) {
-                  createTutor(data);
-              },
-              error: function(data) {
-                  $('<div style="display:none" class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert"><span>&times;</span></button>' + I18n.t("js.tutor-failed") + '</div>').insertBefore(".feedback-table").show("fast");
-              }
-          });
+            type: 'POST',
+            url: 'http://localhost:8080/cgi-bin/build_trace.py',
+            dataType: 'json',
+            data: {
+                code: source_code,
+                input: stdin
+            },
+            success: function (data) {
+                createTutor(data);
+            },
+            error: function (data) {
+                $('<div style="display:none" class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert"><span>&times;</span></button>' + I18n.t("js.tutor-failed") + '</div>').insertBefore(".feedback-table").show("fast");
+            }
+        });
 
-          var createTutor = function(codeTrace) {
-            showInfoModal("Python Tutor",'<div id="tutorcontent"><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-success active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Loading</div></div></div>');
+        var createTutor = function (codeTrace) {
+            showInfoModal("Python Tutor", '<div id="tutorcontent"><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-success active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Loading</div></div></div>');
 
-            $("#tutor #info-modal").on("shown.bs.modal", function(e) {
-              $("#tutorcontent").html('<iframe id="tutorviz" width="100%" frameBorder="0" src="/tutorviz/tutorviz.html"></iframe>');
-              $('#tutorviz').load(function() {
-                var content = $("#tutorviz").get(0).contentWindow;
-                content.load(codeTrace);
-                $("#tutorviz").height(content.document.body.scrollHeight);
-              });
+            $("#tutor #info-modal").on("shown.bs.modal", function (e) {
+                $("#tutorcontent").html('<iframe id="tutorviz" width="100%" frameBorder="0" src="/tutorviz/tutorviz.html"></iframe>');
+                $('#tutorviz').load(function () {
+                    var content = $("#tutorviz").get(0).contentWindow;
+                    content.load(codeTrace);
+                    $("#tutorviz").height(content.document.body.scrollHeight);
+                });
 
             });
-          }
+        };
     }
 
     init();

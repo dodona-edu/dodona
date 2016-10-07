ef761c8bcb6c8607438dac98d8ea99fddccdfc4e
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-10-07 11:57:34 +0200

    handle empty exercise descriptions

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def markdown(source)
+    source ||= ''
     Kramdown::Document.new(source, input: 'GFM', syntax_highlighter: 'rouge', math_engine_opts: { preview: true }).to_html.html_safe
   end
 

0e8b14890d73f0fe890b8f120ffd333261d8193f
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-10-07 11:35:00 +0200

    better google analytics tracking. closes #266

--- app/assets/javascripts/exercise.js
+++ app/assets/javascripts/exercise.js
@@ -115,6 +115,7 @@ function init_exercise_show(exerciseId, programmingLanguage, loggedIn, editorSho
     }
 
     function submitSolution(code) {
+        ga('send', 'pageview');
         return $.post("/submissions.json", {
             submission: {
                 code: code,
@@ -124,6 +125,7 @@ function init_exercise_show(exerciseId, programmingLanguage, loggedIn, editorSho
     }
 
     function feedbackLoaded() {
+        ga('send', 'pageview');
         $('#feedback').removeClass("hidden");
         $('#exercise-feedback-link').removeClass("hidden");
         $('#exercise-feedback-link').tab('show');
@@ -136,6 +138,7 @@ function init_exercise_show(exerciseId, programmingLanguage, loggedIn, editorSho
             var status = $submissionRow.data("status");
             if (status == "queued" || status == "running") {
                 setTimeout(function () {
+                    ga('send', 'pageview');
                     $.get("submissions.js");
                 }, 1000);
             } else {
@@ -162,6 +165,7 @@ function init_exercise_show(exerciseId, programmingLanguage, loggedIn, editorSho
     function submissionSuccessful(data) {
         lastSubmission = data.id;
         showNotification(I18n.t("js.submission-saved"));
+        ga('send', 'pageview');
         $.get("submissions.js");
         $('#exercise-submission-link').tab('show');
         disableSubmitButton();

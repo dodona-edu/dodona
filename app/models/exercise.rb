2c2c10ad10ca86699d4df2d27069a4e3f908aa26
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-10-07 15:55:36 +0200

    add filtering by repository

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -43,6 +43,8 @@ class Exercise < ApplicationRecord
   before_save :check_validity
   before_update :update_config
 
+  scope :in_repository, -> (repository) { where repository_id: repository.id }
+
   scope :by_name, -> (name) { where('name_nl LIKE ? OR name_en LIKE ? OR path LIKE ?', "%#{name}%", "%#{name}%", "%#{name}%") }
   scope :by_status, -> (status) { where(status: status.in?(statuses) ? status : -1) }
   scope :by_visibility, -> (visibility) { where(visibility: visibility.in?(visibilities) ? visibility : -1) }

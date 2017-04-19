35936ca7a281a47b916e1ce8d175835e51ba9a7f
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-04-19 16:56:20 +0200

    mark the actual line

--- app/assets/javascripts/submission.js
+++ app/assets/javascripts/submission.js
@@ -6,9 +6,13 @@ function init_submission_show() {
 
     function initCodeLinks() {
         $("span.source-link").click(function () {
+            var line = $(this).data("line");
+            var Range = ace.require('ace/range').Range
             $(".feedback-table .nav-tabs > li a").filter(function () {
                 return $(this).attr("href").indexOf("#code") == 0;
             }).tab('show');
+            var editor = ace.edit("editor-result");
+            editor.getSession().addMarker(new Range(line - 1, 0, line, 0), "ace_active-line line-marker", "line");
         });
     }
 

3faeb363df1d6a7e0ee14fc2649b3ca1ed7ccb05
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-05-11 15:53:55 +0200

    show unsolved series with deadlines per course

--- app/models/course.rb
+++ app/models/course.rb
@@ -160,4 +160,8 @@ class Course < ApplicationRecord
     series.select {|s| s.pending? and not s.completed?(self) }
   end
 
+  def incomplete_series
+    series.select {|s| not s.completed?(self) }
+  end
+
 end

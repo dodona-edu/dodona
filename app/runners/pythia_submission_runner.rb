0a23ec6e71b7e14942415391fa9b629588f6f0df
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-05-19 10:42:08 +0200

    copy in stead of mount

--- app/runners/pythia_submission_runner.rb
+++ app/runners/pythia_submission_runner.rb
@@ -7,21 +7,21 @@ class PythiaSubmissionRunner < SubmissionRunner
     metrics = {} if metrics.nil?
 
     unless metrics.key?('wall_time')
-      value = last_timestamp(File.join(@path, 'logs', 'user_time.logs')) / 1000.0
+      value = last_timestamp(@mountsrc + @hidden_path + 'logs' + 'user_time.logs') / 1000.0
       metrics['wall_time'] = value
 
-      value = logged_value_range(File.join(@path, 'logs', 'user_time.logs')) / 100.0
+      value = logged_value_range(@mountsrc + @hidden_path + 'logs' + 'user_time.logs') / 100.0
       metrics['user_time'] = value
 
-      value = logged_value_range(File.join(@path, 'logs', 'system_time.logs')) / 100.0
+      value = logged_value_range(@mountsrc + @hidden_path + 'logs' + 'system_time.logs') / 100.0
       metrics['system_time'] = value
     end
 
     unless metrics.key?('peak_memory')
-      value = logged_value_range(File.join(@path, 'logs', 'memory_usage.logs'))
+      value = logged_value_range(@mountsrc + @hidden_path + 'logs' + 'memory_usage.logs')
       metrics['peak_memory'] = value
 
-      value = logged_value_range(File.join(@path, 'logs', 'anonymous_memory.logs'))
+      value = logged_value_range(@mountsrc + @hidden_path + 'logs' + 'anonymous_memory.logs')
       metrics['peak_anonymous_memory'] = value
     end
 

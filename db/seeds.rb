b5f549817cac9298f48512767b859496a239bae1
Rien Maertens <maertensrien@gmail.com>
2017-07-24 17:44:36 +0200

    Create basic seeds. Ignore the local paths for a moment, temporary hack.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+zeus = User.create username: 'zeus', first_name: 'Zeus', last_name: 'Kronosson', email: 'zeus@ugent.be', permission: :zeus
+
+staff = User.create username: 'staff', first_name: 'Stijn', last_name: 'Taff', email: 'staff.coppens@ugent.be', permission: :staff
+
+student = User.create username: 'rbmaerte', first_name: 'Rien', last_name: 'Maertens', email: 'rien.maertens@ugent.be', permission: :student
+
+testcourse = Course.create description: 'This is a test course.', name: 'Test Course' , year: '2017-2018'
+
+# Add student to course
+student.courses << testcourse
+
+judge = Judge.create name: 'Testjudge', image: 'dodona-anaconda3.dockerfile', remote: 'git@github.ugent.be:dodona/docker-images.git', renderer: FeedbackTableRenderer,
+  runner: SubmissionRunner
+
+repo = Repository.create name: 'Testrepo', remote: '../test-exercises', judge: judge
+
+
+
+
+

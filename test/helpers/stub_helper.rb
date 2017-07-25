e2c9b0b3b01924a7252e4af21bd4184059bfc61b
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-25 15:01:34 +0200

    refine stub helper

--- test/helpers/stub_helper.rb
+++ test/helpers/stub_helper.rb
@@ -1,9 +1,13 @@
 module StubHelper
   # stub all git methods
-  def self.stub_git(obj)
+  def stub_git(obj)
     obj.stubs(:pull)
     obj.stubs(:reset)
     obj.stubs(:clone_repo)
     obj.stubs(:repo_is_accessible).returns(true)
+  end
+
+  refine FactoryGirl::SyntaxRunner do
+    include StubHelper
   end
 end

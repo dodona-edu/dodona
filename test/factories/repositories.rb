b18145fb82103bf938d8e2b6741c6143f9645b4d
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-25 15:24:58 +0200

    repository factory

---
+++ test/factories/repositories.rb
@@ -0,0 +1,16 @@
+require 'helpers/stub_helper'
+using StubHelper
+
+FactoryGirl.define do
+  factory :repository do
+    name 'my_repo'
+    remote 'placeholder'
+    association :judge, factory: [:judge, :git_stubbed]
+
+    trait :git_stubbed do
+      after :build do |repository|
+        stub_git(repository)
+      end
+    end
+  end
+end

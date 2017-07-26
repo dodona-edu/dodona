05bcdcc394acd01e8e50e4c19ec885c89fdf550b
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-26 16:10:58 +0200

    exercisis test

--- test/factories/exercises.rb
+++ test/factories/exercises.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :exercise do
     sequence(:name_nl) { |n| "Oefening #{n}" }
     sequence(:name_en) { |n| "Exercise #{n}" }
-    visibility :open
+    visibility "open"
     status :ok
 
     sequence(:path) { |n| "exercise#{n}" }

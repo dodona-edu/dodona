bd52d7c3426c40bc43fa3222a4b96ec3ac22a68e
Rien Maertens <maertensrien@gmail.com>
2017-07-26 11:58:04 +0200

    User controller test: sign in user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = create(:user, permission: :zeus)
+    sign_in @user
   end
 
   test 'should get index' do
-    get users_url
+    get users_url(:nl)
     assert_response :success
   end
 
@@ -40,7 +41,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'should destroy user' do
     assert_difference('User.count', -1) do
-      delete user_url(@user)
+      delete user_url(:en, @user)
     end
 
     assert_redirected_to users_path

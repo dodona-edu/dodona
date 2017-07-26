1f24b21e55601e844c9d0949e0067548b1d69959
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-26 16:10:58 +0200

    exercisis test

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ExercisesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  extend CRUDTest
+
+  def setup
+    @instance = create(:exercise)
+    sign_in create(:zeus)
+  end
+
+  test_crud_actions Exercise,
+                    attrs: %i[visibility name_nl name_en],
+                    actions: %i[index show edit update]
 end

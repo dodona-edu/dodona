026c2ecc60c272bd604663637482e4be52d0dd31
Rien Maertens <maertensrien@gmail.com>
2017-07-27 15:44:52 +0200

    Test series specific redirects

--- test/controllers/series_controller_test.rb
+++ test/controllers/series_controller_test.rb
@@ -8,6 +8,22 @@ class SeriesControllerTest < ActionDispatch::IntegrationTest
     sign_in create(:zeus)
   end
 
+  test 'create series should redirect to edit' do
+    instance = create_request_expect
+    assert_redirected_to edit_series_url(instance)
+  end
+
+  test 'update series should redirect to course' do
+    instance = update_request_expect
+    assert_redirected_to course_url(instance.course, all: true, anchor: "series-#{instance.name.parameterize}")
+  end
+
+  test 'destroy series should redirect to course' do
+    course = @instance.course
+    destroy_request
+    assert_redirected_to course_url(course)
+  end
+
   crud_helpers Series, attrs: %i[name description course_id visibility order deadline]
-  test_crud_actions
+  test_crud_actions except: %i[create_redirect update_redirect destroy_redirect]
 end

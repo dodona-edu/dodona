8ff6c809a6faf5563d270336f4f0ba8883a06992
Rien Maertens <maertensrien@gmail.com>
2017-07-27 16:43:52 +0200

    Test submission specific creation response All tests succeed :tada:

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -3,18 +3,23 @@ require 'test_helper'
 class SubmissionsControllerTest < ActionDispatch::IntegrationTest
   extend CRUDTest
 
+  crud_helpers Submission, attrs: %i[code exercise_id]
+
   setup do
     @instance = create :submission
     sign_in create(:zeus)
   end
 
-  crud_helpers Submission, attrs: %i[code exercise_id]
-
-  test_crud_actions only: %i[index show create]
+  test_crud_actions only: %i[index show create], except: %i[create_redirect]
 
   test 'should add submissions to delayed_job queue' do
-    assert_difference("Delayed::Job.count", +1) do
+    assert_difference('Delayed::Job.count', +1) do
       create_request
     end
+  end
+
+  test 'create submission should respond with ok' do
+    create_request_expect
+    assert_response :success
   end
 end

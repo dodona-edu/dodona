b130ef061fc0d1d02ef9cb2c88eaf2ea90878e1c
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-27 09:20:47 +0200

    fix repositories controller test

--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -5,9 +5,10 @@ class RepositoriesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     stub_git(Repository.any_instance)
+    Repository.any_instance.stubs(:process_exercises)
     @instance = create :repository
     sign_in create(:zeus)
   end
 
-  test_crud_actions Repository, attrs: %i[name remote judge]
+  test_crud_actions Repository, attrs: %i[name remote judge_id]
 end

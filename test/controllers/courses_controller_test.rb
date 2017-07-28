809da251e5d35036ef1f8bc5fa2b48c64acb45cb
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-28 14:18:04 +0200

    test all courses_controller actions

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -12,4 +12,56 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test_crud_actions
+
+  test 'should get scoresheet' do
+    get scoresheet_course_path(@instance)
+    assert_response :success
+  end
+
+  test 'should subscribe current_user to course' do
+    user = create :user
+    sign_in user
+    post subscribe_course_path(@instance)
+    assert @instance.users.include? user
+  end
+
+  test 'subscribe should redirect to course' do
+    post subscribe_course_path(@instance)
+    assert_redirected_to(@instance)
+  end
+
+  test 'should subscribe current_user to course with secret' do
+    user = create :user
+    sign_in user
+    get subscribe_with_secret_course_path(@instance, @instance.secret)
+    assert @instance.users.include? user
+  end
+
+  test 'should not subscibe when already subscribed' do
+    user = create :user, courses: [@instance]
+    sign_in user
+    assert_difference('CourseMembership.count', 0) do
+      post subscribe_course_path(@instance)
+    end
+  end
+
+  test 'subscribe with secret should redirect to course' do
+    get subscribe_with_secret_course_path(@instance, @instance.secret)
+    assert_redirected_to(@instance)
+  end
+
+  test 'should not subscibe with secret when already subscribed' do
+    user = create :user, courses: [@instance]
+    sign_in user
+    assert_difference('CourseMembership.count', 0) do
+      get subscribe_with_secret_course_path(@instance, @instance.secret)
+    end
+  end
+
+  test 'should not subscribe current_user to course with invalid secret' do
+    user = create :user
+    sign_in user
+    get subscribe_with_secret_course_path(@instance, 'the cake is a lie')
+    assert !@instance.users.include?(user)
+  end
 end

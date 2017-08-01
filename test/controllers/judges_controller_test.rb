fee471136a83895019681ec64f957d07dd9b4292
Ilion Beyst <ilion.beyst@gmail.com>
2017-08-01 11:59:41 +0200

    test judges webhook

--- test/controllers/judges_controller_test.rb
+++ test/controllers/judges_controller_test.rb
@@ -13,3 +13,27 @@ class JudgesControllerTest < ActionDispatch::IntegrationTest
 
   test_crud_actions
 end
+
+class JudgesWebhookControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @remote = local_remote('judges/false')
+    @judge = create :judge, remote: @remote.path
+  end
+
+  teardown do
+    @remote.remove
+    @judge.git_repository.remove
+  end
+
+  test 'webhook should update judge repository' do
+    assert_difference('@judge.git_repository.commit_count', 1) do
+      @remote.update_file('run', 'make judge work') do |_|
+        [
+          '#! /bin/bash',
+          'true'
+        ].join('\n')
+      end
+      post webhook_judge_path(@judge)
+    end
+  end
+end

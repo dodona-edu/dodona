8898cf4a897c1743d2a1b980aa2e17ba5180dad4
Rien Maertens <maertensrien@gmail.com>
2017-08-01 10:37:52 +0200

    Rudimentary tests for course

--- test/factories/courses.rb
+++ test/factories/courses.rb
@@ -5,9 +5,23 @@ FactoryGirl.define do
     open true
 
     transient do
+      series_count 0
+      exercises_per_series 0
       start_year Time.zone.today.year
     end
 
     year { "#{start_year}-#{start_year + 1}" }
+
+    trait :with_series_and_exercises do
+      series_count 4
+      exercises_per_series 5
+    end
+
+    after :create do |course, e|
+      series = create_list(:series, e.series_count, course: course)
+      series.each do |s|
+        create_list(:exercise, e.exercises_per_series, series: [s])
+      end
+    end
   end
 end

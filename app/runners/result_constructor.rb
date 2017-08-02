d525eddfe4e75c49695e60c76591110f33145a1e
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-08-02 14:27:09 +0200

    partial output should be context unaware

--- app/runners/result_constructor.rb
+++ app/runners/result_constructor.rb
@@ -3,7 +3,7 @@ require 'json-schema'  # json schema validation, from json-schema gem
 
 class ResultConstructorError < StandardError
   attr_accessor :title, :description
-  def initialize(title, description)
+  def initialize(title, description=nil)
     @title = title
     @description = description
   end
@@ -13,16 +13,13 @@ class ResultConstructor
   FULL_SCHEMA = JSON.parse(File.read(Rails.root.join('public/schemas/judge_output.json')))
   PART_SCHEMA = JSON.parse(File.read(Rails.root.join('public/schemas/partial_output.json')))
 
-  LEVELSA = [:tab, :context, :testcase, :test]
-  LEVELSH = { tab: 0, context: 1, testcase: 2, test: 3 }
+  LEVELSA = [:judgement, :tab, :context, :testcase, :test]
+  LEVELSH = { judgement: 0, tab: 1, context: 2, testcase: 3, test: 4 }
   GATHER = { tab: :groups, context: :groups, testcase: :groups, test: :tests }
 
-  attr_reader :result
-
   def initialize
+    @level = nil
     @result = Hash.new
-    @focus = []
-    @stack = []
   end
 
   def feed(judge_output)
@@ -34,92 +31,119 @@ class ResultConstructor
       else
         raise ResultConstructorError.new(
           "Judge output is not a valid json",
-          judge_output
+          json.to_s
         )
       end
     end
   end
 
+  def result
+    if @level.nil?
+      @result
+    else
+      # unclosed judgement. timeout?
+    end
+  end
+
   # below are the methods open to partial updates
 
-  def focus(focus={})
-    if focus.empty?
-      @focus = []
-    else
-      LEVELSA.each_with_index do |key, level|
-        if focus.key?(key)
-          @focus.slice!(level, @focus.length)
-          @focus[level] = focus[key]
-        end
-      end
-    end
+  def start_judgement
+    check_level(nil, "judgement started")
+    @level = :judgement
+    @judgement = Hash.new
+    @judgement[:accepted] = true
+    @judgement[:status] = "correct"
   end
 
-  def push_focus
-    @stack << @focus.dup
+  def start_tab(title: nil)
+    check_level(:judgement, "tab started")
+    @tab = Hash.new
+    @tab[:description] = title
+    @level = :tab
   end
 
-  def pop_focus
-    @focus = @stack.pop
+  def start_context(description: nil)
+    check_level(:tab, "context started")
+    @context = Hash.new
+    @context[:description] = description if description.present?
+    @context[:accepted] = true
+    @level = :context
   end
 
-  def focus_relative(level: nil)
-    level = level.to_sym
-    until @focus.empty? || level == LEVELSA[@focus.length - 1]
-      @focus.pop
-    end
+  def start_testcase(description: nil)
+    check_level(:context, "testcase started")
+    @testcase = Hash.new
+    @testcase[:description] = description
+    @testcase[:accepted] = true
+    @level = :testcase
   end
 
-  def new_tab; new(:tab); end
-  def new_context; new(:context); end
-  def new_testcase; new(:testcase); end
-  def new_test; new(:test); end
+  def start_test(description: nil, expected: nil)
+    check_level(:testcase, "test started")
+    @test = Hash.new
+    @test[:description] = description if description.present?
+    @test[:expected] = expected
+    @level = :test
+  end
 
   def append_message(message: nil)
     messages = current_item[:messages] ||= []
     messages << message if message.present?
   end
 
-  def set_properties(properties)
-    current_item.update(properties)
+  def close_test(generated: nil, accepted: nil, status: nil)
+    check_level(:test, "test closed")
+    @test[:generated] = generated
+    status = Submission::normalize_status(status)
+    @test[:accepted] = if accepted.nil?
+                       then status == 'correct'
+                       else accepted
+                       end
+    @judgement[:status] = worsen(@judgement[:status], status)
+    @testcase[:accepted] &&= @test[:accepted]
+    (@testcase[:tests] ||= []) << @test
+    @test = nil
+    @level = :testcase
   end
 
-  def increment_badgecount
-    current_item[:badgecount] += 1
+  def close_testcase(accepted: nil)
+    check_level(:testcase, "testcase closed")
+    @testcase[:accepted] = accepted if accepted.present?
+    @context[:accepted] &&= @testcase[:accepted]
+    (@context[:groups] ||= []) << @testcase
+    @testcase = nil
+    @level = :context
+  end
+
+  def close_context(accepted: nil)
+    check_level(:context, "context closed")
+    @context[:accepted] = accepted if accepted.present?
+    @judgement[:accepted] &&= @context[:accepted]
+    (@tab[:groups] ||= []) << @context
+    @context = nil
+    @level = :tab
+  end
+
+  def close_tab(badgeCount: nil)
+    check_level(:tab, "tab closed")
+    @tab[:badgeCount] = badgeCount if badgeCount.present?
+    (@judgement[:groups] ||= []) << @tab
+    @tab = nil
+    @level = :judgement
+  end
+
+  def close_judgement(accepted: nil, status: nil)
+    if @level != :judgement
+      raise ResultConstructorError.new "Judgement closed during level #{@level.to_s}"
+    end
+    @judgement[:accepted] = accepted if accepted.present?
+    @judgement[:status] = status if status.present?
+    @result = @judgement
+    @judgement = nil
+    @level = nil
   end
 
   private
-
-  # returns the list and index for the given level
-  def current(what)
-    subresult = @result
-    LEVELSA.each_with_index do |key, level|
-      collection = subresult[GATHER[key]]          ||= []
-      subresult  = collection[@focus[level] || 0]  ||= {}
-      if key == what
-        return [collection, @focus[level]]
-      else
-        @focus[level] ||= 0
-      end
-    end
-  end
-
-  # return the current focussed item
-  def current_item
-    subresult = @result
-    @focus.each_with_index do |index, level|
-      collection = subresult[GATHER[LEVELSA[level]]] ||= []
-      subresult  = collection[index]                 ||= {}
-    end
-    subresult
-  end
-
-  # used by new tab, ...
-  def new(what)
-    tabs, index = current(what)
-    @focus.slice!(LEVELSH[what], @focus.length)
-    @focus[LEVELSH[what]] = if index.nil? then 0 else index + 1 end
-  end
 
   def split_jsons(string)
     parse_exception = nil
@@ -149,6 +173,30 @@ class ResultConstructor
     else
       send command, json
     end
+  end
+
+  def check_level(should, situation)
+    if should != @level
+      raise ResultConstructorError.new "#{situation} during #{situation}"
+    end
+  end
+
+  EVILNESS = [
+    "internal error",
+    "compilation error",
+    "runtime error",
+    "wrong",
+    "correct"
+  ].each_with_index.reduce(Hash.new) do |memo,pair|
+    memo.merge(pair[0] => pair[1])
+  end
+
+  def worsen(current, new)
+    [current, new].min_by { |e| EVILNESS[e] }
+  end
+
+  def current_item
+    instance_variable_get("@#{@level}")
   end
 
 end

a065fc80a4cf11d7a7fc567f64b51bf36d241809
Rien Maertens <maertensrien@gmail.com>
2017-08-02 16:32:18 +0200

    Stub exercise configuration (fixes a complication with deep_merge)

--- test/factories/exercises.rb
+++ test/factories/exercises.rb
@@ -2,12 +2,19 @@ FactoryGirl.define do
   factory :exercise do
     sequence(:name_nl) { |n| "Oefening #{n}" }
     sequence(:name_en) { |n| "Exercise #{n}" }
-    visibility "open"
+    visibility 'open'
     status :ok
 
     sequence(:path) { |n| "exercise#{n}" }
 
-    association :repository, factory: [:repository, :git_stubbed]
+    association :repository, factory: %i[repository git_stubbed]
     judge { repository.judge }
+
+    trait :config_stubbed do
+      after :create do |exercise|
+        exercise.stubs(:config)
+                .returns({ 'evaluation': {} }.stringify_keys)
+      end
+    end
   end
 end

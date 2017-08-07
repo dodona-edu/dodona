9e8b33c962283c44d1f99ad56c44ee5f409a93d8
Rien Maertens <maertensrien@gmail.com>
2017-08-07 11:27:10 +0200

    Use course_membership status and add relationships to course

--- app/models/course_membership.rb
+++ app/models/course_membership.rb
@@ -11,11 +11,14 @@
 #
 
 class CourseMembership < ApplicationRecord
+  enum status: %i[pending moderator student]
+
   belongs_to :course
   belongs_to :user
 
   validates :course_id, uniqueness: { scope: :user_id }
 
+  before_create { self.status ||= :student }
   after_create :invalidate_stats_cache
 
   def invalidate_stats_cache

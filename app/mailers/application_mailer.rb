75f98907581e27e08c978610c43d445968510dfe
Ilion Beyst <ilion.beyst@gmail.com>
2017-08-10 15:04:44 +0200

    first version of error mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'zeus@dodona.ugent.be'
   layout 'mailer'
 end
---
+++ test/mailers/error_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ErrorMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

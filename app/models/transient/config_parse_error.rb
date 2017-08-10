2601356be8deaaebaabfc75c521f55b213630dc7
Ilion Beyst <ilion.beyst@gmail.com>
2017-08-10 15:02:46 +0200

    interpret json parser messages for a bit

--- app/models/transient/config_parse_error.rb
+++ app/models/transient/config_parse_error.rb
@@ -1,8 +1,11 @@
 class ConfigParseError < StandardError
-  attr_reader :path, :message
+  attr_reader :path, :error_type, :json
 
-  def initialize(path, message)
+  def initialize(path, parse_error)
     @path = path
-    @message = message
+    # ew.
+    groups = /\d+:(?<error_type>.*) at '(?<json>.*)'/m.match(parse_error)
+    @error_type = groups[:error_type]
+    @json = groups[:json]
   end
 end

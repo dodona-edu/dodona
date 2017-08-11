3a13cd3a91b90f5c7565ce603f30149cf8a95bbc
Ilion Beyst <ilion.beyst@gmail.com>
2017-08-11 10:05:04 +0200

    i18n mails

--- app/models/transient/config_parse_error.rb
+++ app/models/transient/config_parse_error.rb
@@ -15,11 +15,17 @@ class ConfigParseError < StandardError
 end
 
 class AggregatedConfigErrors < StandardError
+  include Enumerable
+
   attr_reader :repository,
               :errors
 
   def initialize(repository, errors)
     @repository = repository
     @errors = errors.uniq(&:path)
+  end
+
+  def each(&block)
+    errors.each(&block)
   end
 end

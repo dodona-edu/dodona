8d5252c041580d82e2b5f04c21f3384cd388ee6f
Ilion Beyst <ilion.beyst@gmail.com>
2017-08-11 10:41:14 +0200

    try using user locale for emails

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,15 +1,25 @@
 class ErrorMailer < ApplicationMailer
   helper :repository
 
-  def json_error(name, email, error)
+  def json_error(error, user: nil, name: nil, email: nil)
     @error = error
-    @name = name
-    addressee = %("#{name}" <#{email}>)
-    mail to: addressee,
-         subject: I18n.t(
-           'error_mailer.json_error.subject',
-           count: error.count,
-           repository: error.repository.name
-         )
+    @user = user || User.find_by(email: email)
+
+    if @user
+      @name = @user.full_name
+      @email = @user.email
+    else
+      @name = name || ''
+      @email = email
+    end
+
+    I18n.with_locale(@user&.lang) do
+      mail to: %("#{@name}" <#{@email}>),
+           subject: I18n.t(
+             'error_mailer.json_error.subject',
+             count: error.count,
+             repository: error.repository.name
+           )
+    end
   end
 end

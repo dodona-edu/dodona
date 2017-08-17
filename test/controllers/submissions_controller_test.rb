f026cbab969623d6e58aa4ce6302693325feddaa
Rien Maertens <maertensrien@gmail.com>
2017-08-17 15:08:00 +0200

    Fix another test failing

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -13,10 +13,11 @@ class SubmissionsControllerTest < ActionDispatch::IntegrationTest
   test_crud_actions only: %i[index show create], except: %i[create_redirect]
 
   test 'should add submissions to delayed_job queue' do
+    submission = nil
     assert_jobs_enqueued(1) do
       submission = create_request_expect
     end
-    assert submission.pending?
+    assert submission.queued?
   end
 
   test 'create submission should respond with ok' do

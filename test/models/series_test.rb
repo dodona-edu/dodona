847e0d7214f0105866ac4e15d8eb6b31dde863d9
Rien Maertens <maertensrien@gmail.com>
2017-08-24 09:56:22 +0200

    Add unit tests for token presence

--- test/models/series_test.rb
+++ test/models/series_test.rb
@@ -17,7 +17,24 @@
 require 'test_helper'
 
 class SeriesTest < ActiveSupport::TestCase
-  test 'factory' do
-    create :series
+  setup do
+    @series = create :series
+  end
+
+  test 'factory should create series' do
+    assert_not_nil @series
+  end
+
+  test 'score_token should be set' do
+    assert @series.score_token.present?
+  end
+
+  test 'access_token should only be set when hidden' do
+    @series.update(visibility: 'open')
+    assert_nil @series.access_token
+    @series.update(visibility: 'hidden')
+    assert @series.access_token.present?
+    @series.update(visibility: 'closed')
+    assert_nil @series.access_token
   end
 end

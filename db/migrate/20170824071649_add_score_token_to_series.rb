dd955701391506a45e41e91fff2ca2ac90e33eb2
Rien Maertens <maertensrien@gmail.com>
2017-08-24 09:42:59 +0200

    Index on score_token

--- db/migrate/20170824071649_add_score_token_to_series.rb
+++ db/migrate/20170824071649_add_score_token_to_series.rb
@@ -2,11 +2,13 @@ class AddScoreTokenToSeries < ActiveRecord::Migration[5.0]
   def change
     reversible do |dir|
       dir.up do
-        add_column :series, :score_token, :string
+        add_column    :series, :score_token, :string
+        add_index     :series, :score_token
         rename_column :series, :token, :access_token
       end
       dir.down do
         remove_column :series, :score_token
+        remove_index  :series, :score_token
         rename_column :series, :access_token, :token
       end
     end

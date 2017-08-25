5fff55cc92e9aaad2856d369f4dcc1f23b28fae2
Rien Maertens <maertensrien@gmail.com>
2017-08-25 13:40:14 +0200

    jQuery works now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import jQuery from 'jquery'
+
+// jQuery aliases
+window.jQuery = jQuery;
+window.jquery = jQuery;
+window.$      = jQuery;

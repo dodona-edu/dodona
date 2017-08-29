5477422d91cf0c2f494b94a637fdf94484b9fd9f
Rien Maertens <maertensrien@gmail.com>
2017-08-29 11:23:46 +0200

    Skip ace

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@
 // layout file, like app/views/layouts/application.html.erb
 
 import jQuery from 'jquery'
-import 'ace/ace'
-import 'ace/ext-language-tools'
 
 // jQuery aliases
 window.jQuery = jQuery;

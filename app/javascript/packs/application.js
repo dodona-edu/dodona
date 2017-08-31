56b2685ce476b4f1742ac33691a1949d3dc0aaee
Rien Maertens <maertensrien@gmail.com>
2017-08-31 13:32:07 +0200

    Migrate rails-ujs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import Rails from "rails-ujs";
+Rails.start();
+
 import jQuery from "jquery";
 
 // jQuery aliases

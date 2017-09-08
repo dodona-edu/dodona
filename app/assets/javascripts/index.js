608df5a3115eac6e501ff8d856e4419e53e97494
Rien Maertens <maertensrien@gmail.com>
2017-09-08 10:30:11 +0200

    Fix bug and cleaner searchbar initialization

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,28 +1,51 @@
 import {showNotification} from "./notifications.js";
 import {delay, updateURLParameter, getURLParameter} from "./util.js";
 
-function initFilterIndex(baseUrl, eager, actions) {
-    let PARAM = "filter";
+let PARAM = "filter";
+let FILTER_ID = "#filter-query";
+
+function search(baseUrl, _query) {
+    let getUrl = () => baseUrl || window.location.href;
+    let query = _query || $(FILTER_ID).val();
+    let url = updateURLParameter(getUrl(), PARAM, query);
+    url = updateURLParameter(url, "page", 1);
+    if (!baseUrl) {
+        window.history.replaceState(null, "Dodona", url);
+    }
+    $("#progress-filter").css("visibility", "visible");
+    $.get(url, {
+        format: "js",
+    }, function (data) {
+        eval(data);
+        $("#progress-filter").css("visibility", "hidden");
+    });
+}
+
+function initFilter(baseUrl, eager) {
+    let $filter = $(FILTER_ID);
+    let doSearch = () => search(baseUrl, $filter.val());
+    $filter.off("keyup"); // Remove previous handler
+    $filter.on("keyup", function () {
+        delay(doSearch, 300);
+    });
+    let param = getURLParameter(PARAM);
+    if (param !== "") {
+        $filter.val(param);
+    }
+    if (eager) {
+        doSearch();
+    }
+}
+
+function initFilterIndex(baseUrl, eager, actions, doInitFilter) {
     let $filter;
 
     function init() {
-        initFilter();
+        if (doInitFilter) {
+            initFilter(eager);
+        }
         if (actions) {
             initActions();
-        }
-        if (eager) {
-            search();
-        }
-    }
-
-    function initFilter() {
-        $filter = $("#filter-query");
-        $filter.keyup(function () {
-            delay(search, 300);
-        });
-        let param = getURLParameter(PARAM);
-        if (param !== "") {
-            $filter.val(param);
         }
     }
 
@@ -44,7 +67,7 @@ function initFilterIndex(baseUrl, eager, actions) {
                         if (data.js) {
                             eval(data.js);
                         } else {
-                            search();
+                            search(baseUrl, $filter.val());
                         }
                     });
                 }
@@ -53,27 +76,7 @@ function initFilterIndex(baseUrl, eager, actions) {
         });
     }
 
-    function search() {
-        let val = $filter.val();
-        let url = updateURLParameter(getUrl(), PARAM, val);
-        url = updateURLParameter(url, "page", 1);
-        if (!baseUrl) {
-            window.history.replaceState(null, "Dodona", url);
-        }
-        $("#progress-filter").css("visibility", "visible");
-        $.get(url, {
-            format: "js",
-        }, function (data) {
-            eval(data);
-            $("#progress-filter").css("visibility", "hidden");
-        });
-    }
-
-    function getUrl() {
-        return baseUrl || window.location.href;
-    }
-
     init();
 }
 
-export {initFilterIndex};
+export {initFilterIndex, initFilter, search};

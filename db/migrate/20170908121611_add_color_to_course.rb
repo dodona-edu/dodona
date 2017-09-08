4c495af3ea81f13fbbbc12922e23c379a402dd90
Rien Maertens <maertensrien@gmail.com>
2017-09-08 17:28:56 +0200

    Add teacher to course

--- db/migrate/20170908121611_add_color_to_course.rb
+++ db/migrate/20170908121611_add_color_to_course.rb
@@ -2,7 +2,7 @@ class AddColorToCourse < ActiveRecord::Migration[5.1]
   def change
     reversible do |dir|
       dir.up do
-        #add_column :courses, :color, :integer
+        add_column :courses, :color, :integer
         Course.all.each do |c|
           c.update(color: Course.colors.keys.sample)
         end

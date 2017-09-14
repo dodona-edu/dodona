cedc1adcf25943bf47748d6a3f0796f82270e931
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-09-14 12:03:07 +0200

    load fewer unneeded series

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -18,7 +18,12 @@ class CoursesController < ApplicationController
     @title = @course.name
     @series = policy_scope(@course.series)
     @total_series = @series.count
-    @series = @series.limit(5) unless params[:all]
+    number_of_series = if params[:series]
+                         @series.find_index { |s| s.id == params[:series].to_i }.to_i + 3
+                       else
+                         5
+                       end
+    @series = @series.limit(number_of_series) unless params[:all]
     @series = @series.offset(params[:offset]) if params[:offset]
   end
 

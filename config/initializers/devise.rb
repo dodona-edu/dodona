ba514b604bdf66538dbc7d6caa1acd74a180b262
Bart Mesuere <Bart.Mesuere@UGent.be>
2018-01-17 16:38:56 +0100

    add org and contact meta data by monkey patching the gem

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,6 @@
+
+require_relative("../../lib/SAML/metadata.rb")
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|

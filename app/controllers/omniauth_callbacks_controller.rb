07ecf24080125014b3dc7277ae2224569a803e1d
Rien Maertens <maertensrien@gmail.com>
2018-06-11 10:32:41 +0200

    Add institution whitelist

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,7 +7,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user_login
   end
 
-
   def failure
     reason = request.params['error_message'] \
               || request.params['error_description'] \
@@ -23,17 +22,49 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   private
 
+  WHITELIST = {
+    'https://slow.smartschool.be'          => { provider: :oauth,     name: 'SLO Wetenschappen' },
+    'https://college-ieper.smartschool.be' => { provider: :oauth,     name: 'College Ieper'     },
+    'd7811cde-ecef-496c-8f91-a1786241b99c' => { provider: :office365, name: 'UGent Office365'   }
+  }.freeze
+
+  def provider
+    request.env['omniauth.auth']&.provider
+  end
+
+  def institution
+    request.env['omniauth.auth']&.info&.institution
+  end
+
+  def whitelisted?
+    if WHITELIST.key?(institution) && WHITELIST[institution][:provider] == provider.to_sym
+      return true
+    end
+
+    if is_navigational_format?
+      set_flash_message :notice,
+                        :failure,
+                        kind: provider,
+                        reason: t('devise.omniauth_callbacks.not_whitelisted')
+    end
+    redirect_to root_path
+    false
+  end
+
   def user_login
+    return unless whitelisted?
+    institution_name = WHITELIST[institution][:name]
+
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user&.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'OAuth2') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
     else
       if is_navigational_format?
         set_flash_message :notice,
                           :failure,
-                          kind: 'OAuth2',
+                          kind: provider,
                           reason: t('devise.omniauth_callbacks.user_not_created')
       end
       redirect_to root_path

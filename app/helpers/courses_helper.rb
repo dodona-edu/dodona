4db7b02efce3e9d345c8b8abcc90d8f2e28b4297
Rien Maertens <maertensrien@gmail.com>
2018-06-19 15:18:49 +0200

    Already load series when coming from another page

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,10 +1,16 @@
 module CoursesHelper
   def series_drawer_group(course)
+    needs_series_param = !current_page?(action: 'show')
     items = course.series.map do |series|
+      url = if needs_series_param
+              course_path(course, series: series, anchor: series.anchor)
+            else
+              course_path(course, anchor: series.anchor)
+            end
       [series.name.to_sym,
        {
          text: series.name,
-         url: '#' + series.anchor
+         url: url
        }]
     end
     drawer_group title: 'Reeksen', items: items.to_h

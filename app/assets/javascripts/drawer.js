99973dcd868fe5444b169816c826765a5f1e059c
Rien Maertens <maertensrien@gmail.com>
2018-06-21 17:21:02 +0200

    Always make drawer collapsible, fix main container positioning when drawer is shown

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -1,12 +1,17 @@
 export default class Drawer {
-  constructor(toggleSelector =".drawer-toggle",
-              drawerSelector ="#drawer") {
+  constructor(toggleSelector = ".drawer-toggle",
+              drawerSelector = "#drawer",
+              mainSelector = "#main-container") {
     this.$drawer = $(drawerSelector);
     this.$toggle = $(toggleSelector);
+    this.$main = $(mainSelector);
 
-    this.$toggle.click(() => {
-      this.$drawer.toggleClass("active");
-    });
+    this.$toggle.click(() => this.toggle());
+  }
+
+  toggle(){
+    this.$drawer.toggleClass("active");
+    this.$main.toggleClass("drawer-shown");
   }
 }
 

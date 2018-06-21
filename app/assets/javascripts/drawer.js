fae43f979b45d606a7d8e7b8782ab82bce0c753c
Rien Maertens <maertensrien@gmail.com>
2018-06-21 17:46:04 +0200

    Store whether user wants to see his drawer or not

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -1,3 +1,5 @@
+const breakpoint_width = 1170;
+
 export default class Drawer {
   constructor(toggleSelector = ".drawer-toggle",
               drawerSelector = "#drawer",
@@ -6,12 +8,43 @@ export default class Drawer {
     this.$toggle = $(toggleSelector);
     this.$main = $(mainSelector);
 
+    if(localStorage.getItem("show_drawer") == null){
+      localStorage.setItem("show_drawer", "true");
+    }
+
+    this.shown = false;
+    // localStorage somehow only saves strings,
+    // so it can retutn the "false" string, which is truhty
+    if(localStorage.getItem("show_drawer") === "true"
+       && window.innerWidth > breakpoint_width) {
+      this.show();
+    } else {
+      this.hide();
+    }
+
+
     this.$toggle.click(() => this.toggle());
   }
 
   toggle(){
-    this.$drawer.toggleClass("active");
-    this.$main.toggleClass("drawer-shown");
+    if(this.shown){
+      this.hide();
+    } else {
+      this.show();
+    }
+    localStorage.setItem("show_drawer", this.shown);
+  }
+
+  show() {
+    this.shown = true;
+    this.$drawer.addClass("active");
+    this.$main.addClass("drawer-shown");
+  }
+
+  hide() {
+    this.shown = false;
+    this.$drawer.removeClass("active");
+    this.$main.removeClass("drawer-shown");
   }
 }
 

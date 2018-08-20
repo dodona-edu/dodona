bf0c71a81250c471170a8cc1709e19ce507156d0
Evert Van Petegem <Evert.VanPetegem@UGent.be>
2018-08-20 11:19:33 +0200

    Fix failing tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  Delayed::Worker.delay_jobs = ->(job) { job.queue != 'repositories' }
+  Delayed::Worker.delay_jobs = ->(job) { job.queue != 'default' }
 end

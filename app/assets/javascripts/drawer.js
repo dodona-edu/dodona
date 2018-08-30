c0d9625844333cf491f229936ab8eb3dc203ad34
Rien Maertens <maertensrien@gmail.com>
2018-08-30 12:14:37 +0200

    Always hide sidebar

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -4,13 +4,16 @@ const $ = jQuery;
 export default class Drawer {
   constructor(toggleSelector = ".drawer-toggle",
               drawerSelector = "#drawer",
+              handleSelector = ".drawer-handle",
               backgroundSelector = ".drawer-background") {
 
     this.$drawer = $(drawerSelector);
+    this.$handle = $(handleSelector);
     this.$toggle = $(toggleSelector);
     this.$background = $(backgroundSelector);
 
     this.$toggle.click(() => this.toggle());
+    this.$handle.click(() => this.toggle());
     this.$background.click(() => this.hide());
   }
 

2c28129121ffec87c87706a2feefff2ed8e2fdf5
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-08-30 17:43:59 +0200

    Create tag management pages

---
+++ app/policies/tag_policy.rb
@@ -0,0 +1,31 @@
+class TagPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user&.admin?
+  end
+
+  def update?
+    user&.admin?
+  end
+
+  def destroy?
+    user&.zeus?
+  end
+
+  def permitted_attributes
+    %i[name color]
+  end
+end

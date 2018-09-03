0396b417d1373710e35cd1e7dd66db7cc182a857
Bart Mesuere <Bart.Mesuere@UGent.be>
2018-09-03 15:01:38 +0200

    update to rails 5.2

--- config/puma.rb
+++ config/puma.rb
@@ -26,22 +26,9 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory. If you use this option
-# you need to make sure to reconnect any threads in the `on_worker_boot`
-# block.
+# process behavior so workers use less memory. 
 #
 # preload_app!
-
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
-# cannot share connections between processes.
-#
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart

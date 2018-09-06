af70e1320f42e6ba10af52e9f743e34e3e22d260
Rien Maertens <maertensrien@gmail.com>
2018-09-06 15:26:18 +0200

    Stub exercise config checks to fix failing test

--- test/helpers/stub_helper.rb
+++ test/helpers/stub_helper.rb
@@ -7,6 +7,13 @@ module StubHelper
     obj.stubs(:repo_is_accessible).returns(true)
   end
 
+  def stub_status(exercise, status)
+    exercise.stubs(:status).returns(status)
+    Exercise.statuses.keys.each do |key|
+      exercise.stubs("#{key}?".to_sym).returns(key == status)
+    end
+  end
+
   refine FactoryBot::SyntaxRunner do
     include StubHelper
   end

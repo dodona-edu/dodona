0775640d9dd94fb0ccbb4a28f1625e1ee3d0aab2
Charlotte Van Petegem <charlotte@vanpetegem.me>
2018-09-11 09:46:31 +0200

    Remove markdown name from programming language

--- test/factories/programming_languages.rb
+++ test/factories/programming_languages.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :programming_language do
     name { Faker::ProgrammingLanguage.unique.name }
-    markdown_name { name }
     editor_name { name }
     extension { name }
   end

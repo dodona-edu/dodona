20a75a98c8784f6251da1e877ce88392024b9e75
Louise Deconinck <louise.deconinck@ugent.be>
2018-10-05 11:38:58 +0200

    Format data for use in punchcard

--- app/assets/javascripts/punchcard.js
+++ app/assets/javascripts/punchcard.js
@@ -13,8 +13,19 @@ let initPunchcard = function (url) {
     });
 };
 
+function formatData(data){
+    let mapDates = data.map(s => {
+        let d = new Date(s.created_at);
+        return [d.getDay(), d.getHours()];
+    });
+    let dayHours = Array(7).fill().map(() => Array(24).fill(0));
+    mapDates.forEach(d => dayHours[d[0]][d[1]] += 1);
+    return dayHours;
+}
+
 function drawPunchCard(data) {
-    console.log(data);
+    const submissionData = formatData(data);
+    console.log(submissionData);
 }
 
 export {initPunchcard};

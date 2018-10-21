ad7e8364143bec80e6eb943ae6f629b6187ca5a1
Louise Deconinck <louise.deconinck@ugent.be>
2018-10-21 16:54:52 +0200

    Update aggregation files in model and controller if file not present.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,20 +3,43 @@ class StatisticsController < ApplicationController
   before_action :set_user
 
   def index
-    unless (current_user.administrating_courses).include? @course || @user == current_user
+    print @user.id
+    print current_user.id
+    unless (@user.subscribed_courses & current_user.administrating_courses).include?(@course) || @user == current_user
       raise Pundit::NotAuthorizedError
     end
 
     submissions_matrix_path = File.join('data', 'aggregates', "#{@course.id}_#{@user.id}.json")
-    submissions_matrix = JSON.parse File.read submissions_matrix_path
+
+    submissions_matrix = if File.exist?(submissions_matrix_path)
+                           JSON.parse File.read submissions_matrix_path
+                         else
+                           calculate_submissions_matrix submissions_matrix_path
+                         end
 
     @submissions_aggregate = submissions_matrix.map do |key, val|
       key = JSON.parse key
       key.push(val)
     end
+
   end
 
   private
+
+  def calculate_submissions_matrix(pathname)
+    submissions = Submission.of_user(@user).in_course(@course)
+    submissions_matrix = Hash.new(0)
+    submissions.each do |s|
+      day = created_at.wday > 0 ? created_at.wday - 1 : 6
+      submissions_matrix[[day, s.created_at.hour]] += 1
+    end
+
+    f = File.new(pathname, 'w')
+    f.write(submissions_matrix.to_json)
+    f.close
+
+    submissions_matrix
+  end
 
   def set_course
     @course = Course.find(params[:course_id])
@@ -24,8 +47,6 @@ class StatisticsController < ApplicationController
 
   def set_user
     @user = User.find(params[:member_id])
-    unless @user.courses.include? @course
-      raise ActiveRecord::RecordNotFound
-    end
+    raise ActiveRecord::RecordNotFound unless @user.courses.include? @course
   end
 end

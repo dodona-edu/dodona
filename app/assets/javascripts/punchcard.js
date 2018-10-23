61528aa1b33a39b82de23e03767b151f5a169ee5
Louise Deconinck <louise.deconinck@ugent.be>
2018-10-23 14:33:05 +0200

    Replace ajax call with d3.json fetch call and use color scale.

--- app/assets/javascripts/punchcard.js
+++ app/assets/javascripts/punchcard.js
@@ -1,16 +1,4 @@
 let initPunchcard = function (url) {
-    $.ajax({
-        type: "GET",
-        contentType: "application/json",
-        url: url,
-        dataType: "json",
-        success: function (data) {
-            initChart(d3.entries(data));
-        },
-    });
-};
-
-function initChart(data) {
     const margin = {top: 10, right: 10, bottom: 20, left: 70};
     const labelsX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
     // If this is defined outside of a function, the locale always defaults to "en".
@@ -24,6 +12,7 @@ function initChart(data) {
     const height = innerHeight + margin.top + margin.bottom;
 
     const chart = container.append("svg")
+        .attr("id", "punchcard-svg")
     // When resizing, the svg will scale as well. Doesn't work perfectly.
         .attr("viewBox", `0,0,${width},${height}`)
         .style("overflow-x", "scroll")
@@ -38,6 +27,8 @@ function initChart(data) {
         .domain([0, 6])
         .range([unitSize / 2, innerHeight - unitSize / 2]);
 
+    d3.json(url).then(data => renderCard(d3.entries(data), unitSize, chart, x, y));
+
     const xAxis = d3.axisBottom(x)
         .ticks(24)
         .tickSize(0)
@@ -50,8 +41,7 @@ function initChart(data) {
         .tickPadding(10);
 
     renderAxes(xAxis, yAxis, chart, innerHeight);
-    renderCard(data, unitSize, chart, x, y);
-}
+};
 
 function renderAxes(xAxis, yAxis, chart, innerHeight) {
     chart.append("g")
@@ -69,27 +59,21 @@ function renderAxes(xAxis, yAxis, chart, innerHeight) {
 
 function renderCard(data, unitSize, chart, x, y) {
     const maxVal = d3.max(data, d => d.value);
-    const minVal = d3.min(data, d => d.value);
-
-    let radius = d3.scaleSqrt()
+    const radius = d3.scaleSqrt()
         .domain([0, maxVal])
         .range([0, unitSize / 2]);
 
-    let gradient = d3.scaleLinear()
-        .domain([minVal, maxVal])
-        .rangeRound([255 * 0.8, 0]);
+    const gradient = d3.scaleSequential(d3.interpolateGreys)
+        .domain([0, maxVal]);
 
-    let circles = chart.selectAll("circle")
+    const circles = chart.selectAll("circle")
         .data(data);
 
-    let updates = circles.enter().append("circle");
+    const updates = circles.enter().append("circle");
     updates.attr("cx", d => x(parseInt(d.key.split(",")[1])))
         .attr("cy", d => y(parseInt(d.key.split(",")[0])))
         .attr("r", d => radius(d.value))
-        .style("fill", d => {
-            const gr = gradient(d.value);
-            return `rgb(${gr},${gr},${gr})`;
-        })
+        .style("fill", d => gradient(d.value))
         .append("svg:title")
         .text(d => d.value);
     circles.exit().remove();

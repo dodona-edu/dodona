f7822f58908d5d2fb9d6547c55ac45480c78d5bd
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-10-25 10:30:39 +0200

    Fix callback error when migrating from master schema to develop schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_130518) do
+ActiveRecord::Schema.define(version: 2018_10_15_124238) do
 
   create_table "api_tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"

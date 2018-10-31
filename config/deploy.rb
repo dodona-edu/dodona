e3842cee471dec7adbf97a52ce9e81576c62d1e4
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-10-31 17:54:35 +0100

    Use data/storage instead of data/submissions as linked dir in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/dodona/rails'
 set :linked_files, fetch(:linked_files, []).push('config/keys.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'data/exercises', 'data/judges', 'data/submissions', 'node_modules')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'data/exercises', 'data/judges', 'data/storage', 'node_modules')
 
 set :passenger_restart_with_touch, true
 

412b8f2eb2cfce9cec52fac6a5ac836e8c4bf2c1
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-11-12 12:04:05 +0100

    Allow switching between split and unified diff for a tab
    The buttons for switching are only shown if there is a diff that we can switch.

--- app/assets/javascripts/submission.js
+++ app/assets/javascripts/submission.js
@@ -1,32 +1,19 @@
 function initSubmissionShow() {
-    let currentMarkerId;
-
     function init() {
-        initTabLinks();
+        initDiffSwitchButtons();
     }
 
-    function initTabLinks() {
-        $("a.tab-link").click(function () {
-            const tab = $(this).data("tab") || "code";
-            const element = $(this).data("element");
-            const line = $(this).data("line");
-
-            $(".tab-link-marker").removeClass("tab-link-marker");
-            $(".feedback-table .nav-tabs > li a").filter(function () {
-                return $(this).attr("href").indexOf("#" + tab) === 0;
-            }).tab("show");
-            if (element !== undefined) {
-                $("#element").addClass("tab-link-marker");
-            }
-            if (line !== undefined) {
-                const Range = ace.require("ace/range").Range;
-                const editor = ace.edit("editor-result");
-                if (typeof currentMarkerId !== "undefined") {
-                    editor.getSession().removeMarker(currentMarkerId);
-                }
-                currentMarkerId = editor.getSession().addMarker(new Range(line - 1, 0, line, 0), "ace_active-line tab-link-marker", "line");
-            }
-            return false;
+    function initDiffSwitchButtons() {
+        const buttons = $(".diff-switch-buttons .btn");
+        buttons.click(e => {
+            const button = $(e.target);
+            const tab = button.parents(".tab-pane");
+            buttons.removeClass("active");
+            button.addClass("active");
+            const diffs = tab.find(".diffs");
+            diffs.removeClass("show-split");
+            diffs.removeClass("show-unified");
+            diffs.addClass(button.data("show_class"));
         });
     }
 

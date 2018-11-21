6566cc2dbcd992069e3f47aa784edd4e4c435001
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-11-21 09:31:29 +0100

    Remove destructive database migrations

--- db/migrate/20181105135613_remove_cache_from_database.rb
+++
@@ -1,7 +0,0 @@
-class RemoveCacheFromDatabase < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :series_memberships, :users_correct
-    remove_column :series_memberships, :users_attempted
-    remove_column :courses, :correct_solutions
-  end
-end

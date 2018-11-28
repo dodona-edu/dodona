68b40f6f1f1b10d4504d1d655bc52115733d25bc
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-11-28 13:31:34 +0100

    Use memcache on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   end
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, 'elysium.ugent.be'
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque

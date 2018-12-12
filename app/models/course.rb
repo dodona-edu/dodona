5088a6004adca675fb7a84ca5f9eddbf596d897e
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-12-12 13:25:44 +0100

    Expire submission statistics caches instead of invalidating them every time

--- app/models/course.rb
+++ app/models/course.rb
@@ -143,31 +143,27 @@ class Course < ApplicationRecord
   end
 
   def invalidate_subscribed_members_count_cache
-    Rails.cache.delete(SUBSCRIBED_MEMBERS_COUNT_CACHE_STRING % {id: id})
+    Rails.cache.delete(format(SUBSCRIBED_MEMBERS_COUNT_CACHE_STRING, id: id))
   end
 
   def subscribed_members_count
-    Rails.cache.fetch(SUBSCRIBED_MEMBERS_COUNT_CACHE_STRING % {id: id}) do
+    Rails.cache.fetch(format(SUBSCRIBED_MEMBERS_COUNT_CACHE_STRING, id: id)) do
       subscribed_members.count
     end
   end
 
   def invalidate_exercises_count_cache
-    Rails.cache.delete(EXERCISES_COUNT_CACHE_STRING % {id: id})
+    Rails.cache.delete(format(EXERCISES_COUNT_CACHE_STRING, id: id))
   end
 
   def exercises_count
-    Rails.cache.fetch(EXERCISES_COUNT_CACHE_STRING % {id: id}) do
+    Rails.cache.fetch(format(EXERCISES_COUNT_CACHE_STRING, id: id)) do
       exercises.count
     end
   end
 
-  def invalidate_correct_solutions_cache
-    Rails.cache.delete(CORRECT_SOLUTIONS_CACHE_STRING % {id: id})
-  end
-
   def correct_solutions
-    Rails.cache.fetch(CORRECT_SOLUTIONS_CACHE_STRING % {id: id}) do
+    Rails.cache.fetch(format(CORRECT_SOLUTIONS_CACHE_STRING, id: id), expires_in: 1.hour) do
       Submission.where(status: 'correct',
                        course: self)
           .select(:exercise_id,

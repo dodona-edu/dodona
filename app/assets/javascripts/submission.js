6dc0cbc12df0d2a83b0c916ed84681008f468f8d
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2018-12-12 10:37:09 +0100

    Only remove active from the buttons in the current tab

--- app/assets/javascripts/submission.js
+++ app/assets/javascripts/submission.js
@@ -13,7 +13,8 @@ function initSubmissionShow() {
         buttons.click(e => {
             const button = $(e.currentTarget);
             const tab = button.parents(".tab-pane");
-            buttons.removeClass("active");
+            const tabButtons = tab.find(".diff-switch-buttons .btn");
+            tabButtons.removeClass("active");
             button.addClass("active");
             const diffs = tab.find(".diffs");
             diffs.removeClass("show-split");
@@ -27,7 +28,8 @@ function initSubmissionShow() {
         buttons.click(e => {
             const button = $(e.currentTarget);
             const tab = button.parents(".tab-pane");
-            buttons.removeClass("active");
+            const tabButtons = tab.find(".correct-switch-buttons .btn");
+            tabButtons.removeClass("active");
             button.addClass("active");
             if (button.data("show")) {
                 tab.find(".group.correct").show();

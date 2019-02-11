392db1c52f6acf03284732fa8eddc0401d82759c
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-02-11 15:41:00 +0100

    Add marker value to all cacheables so we don't reset the expiry time if it is present

--- app/models/concerns/cacheable.rb
+++ app/models/concerns/cacheable.rb
@@ -6,18 +6,18 @@ module Cacheable
       define_method(name) do |options = {}|
         if Random.rand < 0.001
           value = calculator.call(self, options)
-          Rails.cache.write(cache_string.call(self, options), value)
+          Rails.cache.write(cache_string.call(self, options), [false, value])
           return value
         end
         Rails.cache.fetch(cache_string.call(self, options)) do
-          calculator.call(self, options)
-        end
+          [false, calculator.call(self, options)]
+        end[1]
       end
 
       define_method("invalidate_#{name}".to_sym) do |options = {}|
-        name = cache_string.call(self, options)
-        value = Rails.cache.read(name)
-        Rails.cache.write(name, value, expires_in: 5.minutes) if value.present?
+        lookup_string = cache_string.call(self, options)
+        value = Rails.cache.read(lookup_string)
+        Rails.cache.write(lookup_string, [true, value[1]], expires_in: 5.minutes) if value.present? && !value[0]
       end
     end
   end

57cc310532b122e3f46dbccf943e189f59b94ba0
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-02-11 11:50:09 +0100

    Keep values in cache indefinitely if they weren't changed

--- app/models/concerns/cacheable.rb
+++ app/models/concerns/cacheable.rb
@@ -6,12 +6,18 @@ module Cacheable
       define_method(name) do |options = {}|
         if Random.rand < 0.001
           value = calculator.call(self, options)
-          Rails.cache.write(cache_string.call(self, options), value, expires_in: 1.hour)
+          Rails.cache.write(cache_string.call(self, options), value)
           return value
         end
-        Rails.cache.fetch(cache_string.call(self, options), expires_in: 1.hour) do
+        Rails.cache.fetch(cache_string.call(self, options)) do
           calculator.call(self, options)
         end
+      end
+
+      define_method("invalidate_#{name}".to_sym) do |options = {}|
+        name = cache_string.call(self, options)
+        value = Rails.cache.read(name)
+        Rails.cache.write(name, value, expires_in: 5.minutes) if value.present?
       end
     end
   end

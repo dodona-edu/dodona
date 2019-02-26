28231bd39c4877d1600d394e89647750dcb8ad5a
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-02-26 15:33:35 +0100

    Filter fetched submissions by scoped users

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -182,7 +182,7 @@ class SeriesController < ApplicationController
     @exercises = @series.exercises
     @users = apply_scopes(@course.users)
     @course_labels = CourseLabel.where(course: @course)
-    @submission_hash = Submission.in_series(@series)
+    @submission_hash = Submission.in_series(@series).where(user: @users)
     @submission_hash = @submission_hash.before_deadline(@series.deadline) if @series.deadline.present?
     @submission_hash = @submission_hash.group([:user_id, :exercise_id]).most_recent.map {|s| [[s.user_id, s.exercise_id], s]}.to_h
     @crumbs = [[@course.name, course_path(@course)], [@series.name, series_path(@series)], [I18n.t("crumbs.overview"), "#"]]

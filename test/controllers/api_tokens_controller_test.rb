ac110fa113bc1f6d1754c8d6bb8ca2bf53967eae
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-03-05 10:48:16 +0100

    Make sure tests work and fix infinite redirects for media and OAuth sign in

--- test/controllers/api_tokens_controller_test.rb
+++ test/controllers/api_tokens_controller_test.rb
@@ -13,14 +13,14 @@ class ApiTokensControllerTest < ActionDispatch::IntegrationTest
   # override CRUDTest's method, because url needs the user
   def create_request(attr_hash: nil)
     attr_hash ||= generate_attr_hash
-    post user_api_tokens_url(:nl, @instance.user), params: model_params(attr_hash)
+    post user_api_tokens_url(@instance.user), params: model_params(attr_hash)
   end
 
   test_crud_actions only: %i[create destroy],
                     except: %i[create_redirect destroy_redirect]
 
   test 'should get index for user' do
-    get user_api_tokens_url(:nl, @instance.user, @instance), params: { format: :json }
+    get user_api_tokens_url(@instance.user), params: {format: :json}
     assert_response :success
   end
 

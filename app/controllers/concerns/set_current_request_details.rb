8bb0171afa24d381711622537bdcb94fbd3fd544
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-03-11 09:55:17 +0100

    Fix "A copy of SetCurrentRequestDetails has been removed from the module tree but is still active"
    This happened in development upon any change to models/controllers/...

--- app/controllers/concerns/set_current_request_details.rb
+++ app/controllers/concerns/set_current_request_details.rb
@@ -2,6 +2,6 @@ module SetCurrentRequestDetails
   extend ActiveSupport::Concern
 
   Warden::Manager.after_set_user do |user, _auth, _opts|
-    Current.user = user
+    ::Current.user = user
   end
 end

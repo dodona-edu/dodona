665926e6bb195056ec97993eb5edd04a5bfaf4f1
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-03-14 10:09:18 +0100

    Remove ugent id from all code

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -23,7 +23,6 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     username { Faker::Internet.unique.user_name(5..32) }
-    ugent_id { Faker::Number.number(8).to_s }
     email { "#{first_name}.#{last_name}.#{username}@UGent.BE".downcase }
     permission { :student }
     institution

be7f3de19dbd9ed470bfa8a7ce27b7cc76e423cb
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-03-28 10:24:05 +0100

    Remove ugent_id from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_092228) do
+ActiveRecord::Schema.define(version: 2019_03_28_092326) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -270,7 +270,6 @@ ActiveRecord::Schema.define(version: 2019_03_28_092228) do
 
   create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username"
-    t.string "ugent_id"
     t.string "first_name"
     t.string "last_name"
     t.string "email"

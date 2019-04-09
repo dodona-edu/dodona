578a880793def90259f509fb0846390b71506609
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-04-09 17:35:25 +0200

    Allow staff to copy courses if they can see the series of a course

--- app/javascript/packs/course.js
+++ app/javascript/packs/course.js
@@ -1,6 +1,7 @@
-import {initCourseEdit, initCourseMembers, initCourseShow, loadUsers} from "course.js";
+import {initCourseEdit, initCourseMembers, initCourseNew, initCourseShow, loadUsers} from "course.js";
 
 window.dodona.initCourseEdit = initCourseEdit;
+window.dodona.initCourseNew = initCourseNew;
 window.dodona.initCourseShow = initCourseShow;
 window.dodona.initCourseMembers = initCourseMembers;
 window.dodona.loadUsers = loadUsers;

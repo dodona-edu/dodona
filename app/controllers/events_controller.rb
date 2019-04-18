efd391b0cb4c5ff071044cb20a0200e29e6eea0a
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-04-18 15:28:32 +0200

    Allow searching by event type

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+
+  has_scope :by_type, as: "type"
+
   def index
     authorize Event
     @events = apply_scopes(policy_scope(Event)).paginate(page: parse_pagination_param(params[:page]))

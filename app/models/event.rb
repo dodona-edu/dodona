2503fbe9a3d7d16bc7f9b9f723fdfcc596f1c45b
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-04-18 13:24:53 +0200

    Add some extra event types

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  enum event_type: %i[rejudge]
-  belongs_to :user
+  enum event_type: %i[rejudge permission_change exercise_repository error]
+  belongs_to :user, optional: true
 
   validates :event_type, presence: true
   validates :message, presence: true

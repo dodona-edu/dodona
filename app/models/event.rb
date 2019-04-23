50485fcd13104145416125d8354ec3962ce0576a
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-04-23 09:02:35 +0200

    Run annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint(8)        not null, primary key
+#  event_type :integer          not null
+#  user_id    :integer
+#  message    :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Event < ApplicationRecord
   enum event_type: %i[rejudge permission_change exercise_repository error]
   belongs_to :user, optional: true
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint(8)        not null, primary key
+#  event_type :integer          not null
+#  user_id    :integer
+#  message    :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase

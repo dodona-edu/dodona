209b9709910c7c2784da396dac3febceb36d0893
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-05-03 17:35:19 +0200

    Use eduPersonPrincipalName as username if no other username was delivered

--- lib/SAML/my_resource_validator.rb
+++ lib/SAML/my_resource_validator.rb
@@ -3,10 +3,12 @@ class MyResourceValidator
     return true if user.present?
     institution = Institution.find_by(entity_id: saml_response.issuers[0], provider: :saml)
     return false if institution.nil?
+
+    username = saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.1"] || saml_response.attributes["urn:oid:1.3.6.1.4.1.5923.1.1.1.6"]
     # If we don't have a username it can't conflict either
     # The user will get a new account, but won't see errors
-    return true if saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.1"].nil?
-    user = User.find_by(username: saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.1"], institution: institution)
+    return true if username.nil?
+    user = User.find_by(username: username, institution: institution)
     return true if user.nil?
     user.email == saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.3"]
   end

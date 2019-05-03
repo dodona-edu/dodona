db52c145ad58c89303da23d63564e817353c97e1
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-05-03 17:14:23 +0200

    Don't do custom validation on user if we don't have a username

--- lib/SAML/my_resource_validator.rb
+++ lib/SAML/my_resource_validator.rb
@@ -3,6 +3,9 @@ class MyResourceValidator
     return true if user.present?
     institution = Institution.find_by(entity_id: saml_response.issuers[0], provider: :saml)
     return false if institution.nil?
+    # If we don't have a username it can't conflict either
+    # The user will get a new account, but won't see errors
+    return true if saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.1"].nil?
     user = User.find_by(username: saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.1"], institution: institution)
     return true if user.nil?
     user.email == saml_response.attributes["urn:oid:0.9.2342.19200300.100.1.3"]

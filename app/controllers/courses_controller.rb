660977015819093f227657c91eb0708b33b08bde
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-05-09 17:13:50 +0200

    Allow logged out users to see a (hidden) course page with the secret link

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -321,9 +321,7 @@ class CoursesController < ApplicationController
   end
 
   def redirect_unless_secret_correct
-    if !current_user
-      redirect_back(fallback_location: root_url, notice: I18n.t('courses.registration.not_logged_in'))
-    elsif current_user.member_of?(@course) || current_user.zeus?
+    if current_user&.member_of?(@course) || current_user&.zeus?
       nil
     elsif params[:secret] != @course.secret
       redirect_back(fallback_location: root_url, alert: I18n.t('courses.registration.key_mismatch'))

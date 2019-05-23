e7b51f05eb07bb3eebe1dbb1c773f1fa64a162d5
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-05-23 10:20:17 +0200

    Add JS to disable institution-specific options when no institution was selected

--- app/javascript/packs/course.js
+++ app/javascript/packs/course.js
@@ -1,6 +1,7 @@
-import {initCourseEdit, initCourseMembers, initCourseNew, initCourseShow, loadUsers} from "course.js";
+import {initCourseEdit, initCourseMembers, initCourseForm, initCourseNew, initCourseShow, loadUsers} from "course.js";
 
 window.dodona.initCourseEdit = initCourseEdit;
+window.dodona.initCourseForm = initCourseForm;
 window.dodona.initCourseNew = initCourseNew;
 window.dodona.initCourseShow = initCourseShow;
 window.dodona.initCourseMembers = initCourseMembers;

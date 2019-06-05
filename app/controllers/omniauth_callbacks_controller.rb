ab2d2b7df83819f2c0976dd68a10de3990d829c2
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-06-05 11:55:25 +0200

    Move name for new institutions to a constant

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -69,8 +69,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       else
         institution = Institution.from_identifier(institution_identifier)
         if institution.blank?
-          institution = Institution.create(name: 'n/a',
-                                           short_name: 'n/a',
+          institution = Institution.create(name: Institution.NEW_INSTITUTION_NAME,
+                                           short_name: Institution.NEW_INSTITUTION_NAME,
                                            provider: provider,
                                            identifier: institution_identifier)
           institution_created

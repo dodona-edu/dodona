4b9f6df98e3ce6328bb25806386aa075de0f02e5
LucianosLionakis <lucianos.lionakis@ugent.be>
2019-08-08 20:11:07 +0200

    Corrected vertical alignment of navbar icons.

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,7 +37,7 @@ if Rails.env.development?
   students = Array.new(500) do
     first_name = Faker::Name.first_name
     last_name = Faker::Name.last_name
-    username = Faker::Internet.unique.user_name(5..8)
+    username = Faker::Internet.unique.user_name()
     User.create first_name: first_name,
                 last_name: last_name,
                 username: username,

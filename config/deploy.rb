e6bbe90d4cd8abfaf940f7dcd7ebcf97f7abbc79
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-08-08 16:47:29 +0200

    Switch to the credentials system introduced in Rails 5.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/dodona/rails'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/keys.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'data/exercises', 'data/judges', 'data/storage', 'node_modules')

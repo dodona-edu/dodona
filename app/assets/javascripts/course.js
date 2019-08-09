ed4c2c733577753b9b025d8f142821ef65316572
Winnie De Ridder <Winnie.De.Ridder@hotmail.com>
2019-08-09 16:26:17 +0200

    Moved javascript code around to prevent duplication

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -1,6 +1,6 @@
 /* globals ga, I18n, ace, MathJax, initStrip, Strip */
 import {setBaseUrl} from "./index.js";
-import dragula from "dragula";
+import {initDragAndDrop} from "./drag_and_drop.js";
 
 function loadUsers(_baseUrl, _status) {
     const baseUrl = _baseUrl || $("#user-tabs").data("baseurl");
@@ -220,30 +220,19 @@ function initCourseForm() {
     init();
 }
 
+const DRAG_AND_DROP_ARGS = {
+    table_selector: ".course-series-list tbody",
+    item_selector: ".course-series-list",
+    item_data_selector: "course_id",
+    order_selector: ".course-series-list tbody .series-name",
+    order_data_selector: "series_id",
+    url_from_id: function(courseId){
+        return `/courses/${courseId}/reorder_series.js`
+    }
+}
+
 function initSeriesReorder() {
-    function init() {
-        initDragAndDrop();
-    }
-
-    function initDragAndDrop() {
-        const tableBody = $(".course-series-list tbody").get(0);
-        dragula([tableBody], {
-            moves: function (el, source, handle, sibling) {
-                return $(handle).hasClass("drag-handle") || $(handle).parents(".drag-handle").length;
-            },
-            mirrorContainer: tableBody,
-        }).on("drop", () => {
-            let courseId = $(".course-series-list").data("course_id");
-            let order = $(".course-series-list tbody .series-name").map(function () {
-                return $(this).data("series_id");
-            }).get();
-            $.post(`/courses/${courseId}/reorder_series.js`, {
-                order: JSON.stringify(order),
-            });
-        });
-    }
-
-    init();
+    initDragAndDrop(DRAG_AND_DROP_ARGS);
 }
 
 function initCourseNew() {
---
+++ app/assets/javascripts/drag_and_drop.js
@@ -0,0 +1,52 @@
+import dragula from "dragula";
+
+function copyWidth(clone, original, tag=undefined){
+    if(clone && original && original.getBoundingClientRect){
+        $(clone).width(original.getBoundingClientRect().width); // getBoundingClientRect.width is more accurate than jQuery-width()
+        let cloneChildren, originalChildren;
+        if(tag){
+            cloneChildren = clone.getElementsByTagName(tag);
+            originalChildren = original.getElementsByTagName(tag);
+        } else {
+            cloneChildren = clone.childNodes;
+            originalChildren = original.childNodes;
+        }
+        for(let i = 0; i < cloneChildren.length; i++){ // recursively make all children equally big
+            copyWidth(cloneChildren[i], originalChildren[i]);
+        }
+    }
+}
+
+/**
+ * Initializes drag and drop on the page
+ * @param args: a JSON with the following keys:
+ * --table_selector: the id used to find the table that is used for drag and drop
+ * --item_selector: the id used to find the selected item
+ * --item_data_selector: the key used to retrieve data of the item
+ * --order_selector: the id used to find the ordered items
+ * --order_data_selector: the key used to retrieve data of the order
+ * --url_from_id: a function that constructs the URL given an id
+ */
+function initDragAndDrop(args) {
+    const tableBody = $(args.table_selector).get(0);
+    dragula([tableBody], {
+        moves: function (el, source, handle, sibling) {
+            return $(handle).hasClass("drag-handle") || $(handle).parents(".drag-handle").length;
+        },
+        mirrorContainer: tableBody,
+    })
+    .on("cloned", function(clone, original, type){
+        copyWidth(clone, original, "td");
+    })
+    .on("drop", function () {
+        let id = $(args.item_selector).data(args.item_data_selector);
+        let order = $(args.order_selector).map(function () {
+            return $(this).data(args.order_data_selector);
+        }).get();
+        $.post(args.url_from_id(id), {
+            order: JSON.stringify(order),
+        });
+    });
+}
+
+export {initDragAndDrop}

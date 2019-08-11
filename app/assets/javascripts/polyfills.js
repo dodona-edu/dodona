7568e9e8400e039d2ead19f8c9aef1c0541a7001
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-08-11 12:21:17 +0200

    remove old animation frame polyfill

--- app/assets/javascripts/polyfills.js
+++ app/assets/javascripts/polyfills.js
@@ -102,15 +102,3 @@
     window.fullScreenApi = fullScreenApi;
 })();
 
-/**
- * requestAnimationFrame shim
- * source: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
- */
-window.requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-})();

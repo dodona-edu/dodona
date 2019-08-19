0231e771fddfe737813b3a070a6d4c380ac46f5d
Rien Maertens <rien.maertens@posteo.be>
2019-08-19 17:09:44 +0200

    Webpacker production environment

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
 const environment = require("./environment");
 
+const tsloader = environment.loaders.get('typescript').use.find(l => l.loader === 'ts-loader');
+tsloader.options = {
+  ...tsloader.options,
+  transpileOnly: true,
+  reportFiles: ["!test/**/*"]
+};
+
+
 module.exports = environment.toWebpackConfig();
+

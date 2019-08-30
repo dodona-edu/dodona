deba11cdeed6f1c3fbbed0f013e749dffb566030
Rien Maertens <rien.maertens@posteo.be>
2019-08-30 15:43:37 +0200

    Batches give problems

--- db/migrate/20190829143205_add_repository_token_to_exercise.rb
+++ db/migrate/20190829143205_add_repository_token_to_exercise.rb
@@ -1,10 +1,8 @@
 class AddRepositoryTokenToExercise < ActiveRecord::Migration[6.0]
   def change
     add_column :exercises, :repository_token, :string, limit: 64
-    Exercise.all.in_batches do |batch|
-      batch.each_record do |exercise|
-        exercise.update(repository_token: exercise.token)
-      end
+    Exercise.all.each do |exercise|
+      exercise.update(repository_token: exercise.token)
     end
     change_column :exercises, :repository_token, :string, limit: 64, null: false
   end

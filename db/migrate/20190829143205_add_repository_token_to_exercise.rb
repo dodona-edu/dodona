f7c6260f0f504b25bbf077446a0b4d58eed23048
Rien Maertens <rien.maertens@posteo.be>
2019-09-12 09:39:03 +0200

    Use batches to set Exercise.repository_token

--- db/migrate/20190829143205_add_repository_token_to_exercise.rb
+++ db/migrate/20190829143205_add_repository_token_to_exercise.rb
@@ -1,7 +1,7 @@
 class AddRepositoryTokenToExercise < ActiveRecord::Migration[6.0]
   def change
     add_column :exercises, :repository_token, :string, limit: 64
-    Exercise.all.each do |exercise|
+    Exercise.all.find_each do |exercise|
       exercise.update(repository_token: exercise.token)
     end
     change_column :exercises, :repository_token, :string, limit: 64, null: false

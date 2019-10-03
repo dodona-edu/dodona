526205e4a48de74b39d7da7d8f306c04f9309273
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-10-03 09:28:06 +0200

    Notify us if there are requests that take over 2 seconds
    The StatisticsController is excluded since we know that there are long-running requests there. (This is something that should probably be fixed.)

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,3 +1,14 @@
 ExceptionNotifier.add_notifier :event_logger, ->(e, options) {
   Event.create(event_type: :error, user: Current.user, message: e.message)
 }
+
+ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
+  if finish - start > 2.seconds && payload[:controller] != "StatisticsController"
+    headers = payload.delete :headers
+    ExceptionNotifier.notify_exception(
+      Exception.new("A request took over 2 seconds"),
+      env: headers.env,
+      data: payload
+    )
+  end
+end

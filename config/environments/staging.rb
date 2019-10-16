18446af4b8f40b202950587e606706fbab03af4e
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-10-16 09:28:20 +0200

    Also bump caching namespace for naos

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.cache_store = :mem_cache_store, {namespace: :"1"}
+  config.cache_store = :mem_cache_store, {namespace: :"2"}
   config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
   }

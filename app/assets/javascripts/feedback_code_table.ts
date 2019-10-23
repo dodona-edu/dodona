a372ed2005c67db376c7110e38bdd42efaed7627
Mathieu Coussens <mathieu_coussens@hotmail.com>
2019-10-23 14:41:27 +0200

    Single row, multiple annotations

--- app/assets/javascripts/feedback_code_table.ts
+++ app/assets/javascripts/feedback_code_table.ts
@@ -1,7 +1,6 @@
 export class FeedbackCodeTable {
     table: Element;
     messages;
-
     annotationCounter: number = 0;
 
     constructor(feedbackTableSelector = ".feedback-code-table") {
@@ -17,45 +16,50 @@ export class FeedbackCodeTable {
         for (const message of this.messages) {
             // Linter counts from 0, rouge counts from 1
             const correspondingLine = this.table.querySelector(`#line-${message.row + 1}`);
-            const annotationRow = this.createAnnotation(message, this.annotationCounter);
-            message.id = this.annotationCounter;
-            this.annotationCounter += 1;
+            const annotationRow = this.createAnnotation(message);
 
-            // Decide the location in the list of siblings where we should insert
-            // Skip over any previous annotations to conserve the order
-            const parentElement = correspondingLine.parentElement;
-            let sibling = correspondingLine.nextElementSibling;
-
-            // Sibling is either a line in the middle of the code or the last line
-            if (sibling === null) {
-                parentElement.appendChild(annotationRow);
-            } else {
-                while (sibling != null && sibling.classList.contains("annotation")) {
-                    sibling = sibling.nextElementSibling;
-                }
-                parentElement.insertBefore(annotationRow, sibling);
-            }
+            correspondingLine.parentElement.insertBefore(annotationRow, correspondingLine.nextSibling);
         }
     }
 
-    private createAnnotation(message, lineId): Element {
-        const annotationRow = document.createElement("tr");
-        annotationRow.setAttribute("class", "annotation");
-        annotationRow.setAttribute("id", `annotation-id-${lineId}`);
-        annotationRow.classList.add(`annotation-line-${message.row}`);
+    private createAnnotationRow(lineNumber: number): object {
+        const annotationRow: HTMLTableRowElement = document.createElement("tr");
+        annotationRow.setAttribute("class", "annotation-set");
+        annotationRow.setAttribute("id", `annotation-row-id-${lineNumber}`);
 
-        const annotationLine = document.createElement("td");
-        annotationLine.setAttribute("colspan", "2");
+        const annotationTD: HTMLTableDataCellElement = document.createElement("td");
+        annotationTD.setAttribute("colspan", "2");
+        annotationTD.setAttribute("class", "annotation-cell");
 
-        const annotationCell = document.createElement("div");
+        annotationRow.appendChild(annotationTD);
+        return {
+            "row": annotationRow,
+            "datacell": annotationTD,
+        };
+    }
 
-        const textNode = document.createTextNode(message.text.replace(/-*$/, ""));
-        annotationCell.setAttribute("class", "annotation-text");
+    private createAnnotation(message): Element {
+        let annotationRow: HTMLTableRowElement = this.table.querySelector(`#annotation-row-id-${message.row}`);
+        let annotationTD: HTMLTableDataCellElement = null;
+
+        if (annotationRow === null) {
+            const created: object = this.createAnnotationRow(message.row);
+            annotationRow = created["row"];
+            annotationTD = created["datacell"];
+        } else {
+            annotationTD = annotationRow.firstChild as HTMLTableDataCellElement;
+        }
+
+        const annotationCell: HTMLDivElement = document.createElement("div");
+        annotationCell.setAttribute("class", "annotation");
+        annotationCell.setAttribute("id", `annotation-id-${this.annotationCounter}`);
+        this.annotationCounter += 1;
+
+        const textNode: Text = document.createTextNode(message.text.replace(/-*$/, ""));
         annotationCell.classList.add(message.type);
         annotationCell.appendChild(textNode);
 
-        annotationLine.appendChild(annotationCell);
-        annotationRow.appendChild(annotationLine);
+        annotationTD.appendChild(annotationCell);
         return annotationRow;
     }
 }

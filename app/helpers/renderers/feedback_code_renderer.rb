600082ce6552b9fb771bfc0567d56444b55724cd
Mathieu Coussens <mathieu_coussens@hotmail.com>
2019-11-30 19:50:42 +0100

    No global scope variables

--- app/helpers/renderers/feedback_code_renderer.rb
+++ app/helpers/renderers/feedback_code_renderer.rb
@@ -18,13 +18,10 @@ class FeedbackCodeRenderer
     @builder << table_formatter.format(lexed_c)
 
     @builder.script(type: 'application/javascript') do
-      @builder << 'var messages = '
-      @builder << @messages.map { |o| Hash[o.each_pair.to_a] }.to_json
-      @builder << ';'
-
-      @builder << 'var codeListing = new window.dodona.codeListingClass();'
-      @builder << 'window.dodona.codeListing = codeListing;'
-      @builder << 'codeListing.addAnnotations(messages);'
+      @builder.script(type: 'application/javascript') do
+        @builder << 'window.dodona.codeListing = new window.dodona.codeListingClass();'
+        @builder << 'window.dodona.codeListing.addAnnotations(' + @messages.map { |o| Hash[o.each_pair.to_a] }.to_json + ');'
+      end
     end
   end
 end

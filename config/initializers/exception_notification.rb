d033084fd1cd759182deb91360447a5435c19f8f
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-12-09 13:42:29 +0100

    Merge pull request #1566 from dodona-edu/fix/ignore-some-unfixable-long-running-requests
    Ignore some long-running requests we know can be slow

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,14 +1,17 @@
-ExceptionNotifier.add_notifier :event_logger, ->(e, options) {
+ExceptionNotifier.add_notifier :event_logger, lambda { |e, _options|
   Event.create(event_type: :error, user: Current.user, message: e.message)
 }
 
-ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
-  if finish - start > 5.seconds
-    headers = payload.delete :headers
-    ExceptionNotifier.notify_exception(
-      Exception.new("A request took #{finish - start} seconds"),
-      env: headers.env,
-      data: payload
-    )
-  end
+ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |_name, start, finish, _id, payload|
+  next if finish - start < 5.seconds
+  next if (payload[:action] == 'hook') && (payload[:controller] == 'RepositoriesController')
+  next if (payload[:action] == 'create') && (payload[:controller] == 'RepositoriesController')
+  next if (payload[:action] == 'scoresheet') && (payload[:controller] == 'CoursesController')
+
+  headers = payload.delete :headers
+  ExceptionNotifier.notify_exception(
+    Exception.new("A request took #{finish - start} seconds"),
+    env: headers.env,
+    data: payload
+  )
 end

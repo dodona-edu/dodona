afbcc135ed4b5d43ecb8ec324e6cc2d48e2fc237
Mathieu Coussens <mathieu_coussens@hotmail.com>
2019-12-15 17:22:51 +0100

    Style buttons like the feedback tab

--- app/helpers/renderers/feedback_code_renderer.rb
+++ app/helpers/renderers/feedback_code_renderer.rb
@@ -14,17 +14,23 @@ class FeedbackCodeRenderer
     lexer = (Rouge::Lexer.find(@programming_language) || Rouge::Lexers::PlainText).new
     lexed_c = lexer.lex(@code)
 
-    only_errors = @messages.select { |message| message[:type] == :error }
+    only_errors = @messages.select { |message| message[:type] == :error || message[:type] == 'error' }
 
     if !only_errors.empty? && only_errors.size != @messages.size
-      @builder.div(class: 'code-listing-options') do
-        @builder.span(class: 'flex-spacer') {}
-        @builder.div(class: 'btn-group btn-toggle') do
-          @builder.button(class: 'btn btn-secondary', id: 'show_extra_annotations') do
-            @builder.text!(I18n.t('submissions.show.annotations.show_all'))
+      @builder.div(class: 'feedback-table-options') do
+        @builder.span(class: 'flex-spacer') do
+        end
+        @builder.span(class: 'diff-switch-buttons switch-buttons') do
+          @builder.span do
+            @builder.text!(I18n.t('submissions.show.annotations.title'))
           end
-          @builder.button(class: 'btn btn-secondary active', id: 'hide_extra_annotations') do
-            @builder.text!(I18n.t('submissions.show.annotations.hide_all'))
+          @builder.div(class: 'btn-group btn-toggle') do
+            @builder.button(class: 'btn btn-secondary', id: 'show_extra_annotations', title: I18n.t('submissions.show.annotations.show_all'), 'data-toggle': 'tooltip', 'data-placement': 'top') do
+              @builder.i(class: 'mdi mdi-18 mdi-arrow-expand-all') {}
+            end
+            @builder.button(class: 'btn btn-secondary active', id: 'hide_extra_annotations', title: I18n.t('submissions.show.annotations.hide_all'), 'data-toggle': 'tooltip', 'data-placement': 'top') do
+              @builder.i(class: 'mdi mdi-18 mdi-arrow-collapse-all') {}
+            end
           end
         end
       end

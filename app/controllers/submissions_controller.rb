e14e7dcda835e49ad7702bb041aba8e9bbe3db57
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-01-30 12:02:08 +0100

    Make sure a user can only search by course labels if they are a course admin

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -12,7 +12,8 @@ class SubmissionsController < ApplicationController
   has_scope :by_status, as: 'status'
 
   has_scope :by_course_labels, as: 'course_labels', type: :array do |controller, scope, value|
-    if controller.params[:course_id].present? && controller.params[:user_id].nil?
+    course = Course.find_by(controller.params[:course_id]) if controller.params[:course_id].present?
+    if course.present? && controller.current_user&.course_admin(course) && controller.params[:user_id].nil?
       scope.by_course_labels(value, controller.params[:course_id])
     else
       scope

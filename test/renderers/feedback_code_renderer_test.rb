6da08ab1838feaa817b11aded4cce04ce3cd534c
Mathieu Coussens <mathieu_coussens@hotmail.com>
2020-03-04 21:28:27 +0100

    More test cases

--- test/renderers/feedback_code_renderer_test.rb
+++ test/renderers/feedback_code_renderer_test.rb
@@ -2,24 +2,31 @@ require 'test_helper'
 require 'builder'
 
 class FeedbackCodeRendererTest < ActiveSupport::TestCase
-  include Rails.application.routes.url_helpers
-  include ApplicationHelper
 
   test 'No weird carriage return or linefeeds in the generated html' do
-    code_orig = "print(x)\nprint(y)\n"
+
     programming_language = 'python'
-    renderer_orig = FeedbackCodeRenderer.new(code_orig, programming_language)
-    gen_html_orig = renderer_orig.parse.html
+    examples = [
+      "print(x)\nprint(y)\n",
+      "# First line comment\nprint(5)\nprint(6)",
+      "# Doctest\n\"\"\"\n>>> 5 + 4\n9\n\"\"\"",
+      "def plus(n, m):\n\treturn n+m\n\nprint(plus(5, 4))"
+    ]
 
-    code = code_orig.encode(crlf_newline: true)
-    renderer_crlf = FeedbackCodeRenderer.new(code, programming_language)
-    gen_html_crlf = renderer_crlf.parse.html
+    examples.each do |code_orig|
+      renderer_orig = FeedbackCodeRenderer.new(code_orig, programming_language)
+      gen_html_orig = renderer_orig.parse.html
 
-    code = code_orig.encode(cr_newline: true)
-    renderer_cr = FeedbackCodeRenderer.new(code, programming_language)
-    gen_html_cr = renderer_cr.parse.html
+      code = code_orig.encode(crlf_newline: true)
+      renderer_crlf = FeedbackCodeRenderer.new(code, programming_language)
+      gen_html_crlf = renderer_crlf.parse.html
 
-    assert_equal gen_html_orig, gen_html_crlf
-    assert_equal gen_html_orig, gen_html_cr
+      code = code_orig.encode(cr_newline: true)
+      renderer_cr = FeedbackCodeRenderer.new(code, programming_language)
+      gen_html_cr = renderer_cr.parse.html
+
+      assert_equal gen_html_orig, gen_html_crlf
+      assert_equal gen_html_orig, gen_html_cr
+    end
   end
 end

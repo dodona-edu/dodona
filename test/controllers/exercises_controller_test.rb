f104161627d05f21ec3c1e4277d53eedd21c57ec
Niko Strijbol <strijbol.niko@gmail.com>
2020-04-08 11:23:13 +0200

    Attach multiple solutions and courses
    This should maybe fix the coverage

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -29,6 +29,8 @@ class ExercisesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should show exercise info' do
     stub_all_exercises!
+    # Attach exercise to courses to test sorting
+    create_list(:course, 5).each { |s| s.series << create(:series, exercises: [@instance]) }
     get info_exercise_url(@instance)
     assert_response :success
   end

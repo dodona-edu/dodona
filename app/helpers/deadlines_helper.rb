e0a32c7a2af7621be37065bb8ea44b5cae35b941
Pieter De Clercq <pieterdeclercq@outlook.com>
2020-04-16 11:06:47 +0200

    Extract DeadlinesHelper, start deadline partials

---
+++ app/helpers/deadlines_helper.rb
@@ -0,0 +1,21 @@
+module DeadlinesHelper
+  def deadline_class(met, deadline)
+    return 'deadline-ok' if met
+    return 'deadline-future' if deadline.future?
+
+    'deadline-passed'
+  end
+
+  def deadline_icon(met, deadline)
+    return 'mdi-alarm-check' if met
+    return 'mdi-alarm' if deadline.future?
+
+    'mdi-alarm-off'
+  end
+
+  def deadline_relative_time(deadline)
+    return t('deadlines.in', time_left: time_ago_in_words(deadline)) if deadline.future?
+
+    t('deadlines.ago', time_ago: time_ago_in_words(deadline))
+  end
+end

eca856310a0bfebb7f2b5a3de942027e0196e072
Mathieu Coussens <mathieu_coussens@hotmail.com>
2020-04-19 15:56:28 +0200

    More specific testing

--- test/system/annotations_test.rb
+++ test/system/annotations_test.rb
@@ -185,12 +185,30 @@ class AnnotationsTest < ApplicationSystemTestCase
 
     find('.annotation .annotation-control-button.annotation-edit i.mdi.mdi-pencil').click
     replacement = (Faker::Lorem.words number: 512).join(' ')
+    assert_selector 'form.annotation-submission', count: 1
     within 'form.annotation-submission' do
       find('textarea.annotation-submission-input').fill_in with: replacement
       click_button 'Update'
     end
 
-    assert_no_text replacement
+    within 'form.annotation-submission' do
+      click_button 'Cancel'
+    end
+
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text annot.annotation_text
+      assert_no_text replacement
+    end
+
+    # After reload, make sure no replacing has taken place
+    visit(submission_path(id: @instance.id))
+    click_link 'Code'
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text annot.annotation_text
+      assert_no_text replacement
+    end
   end
 
   test 'Edit valid annotation -- Zero length input text' do
@@ -210,7 +228,26 @@ class AnnotationsTest < ApplicationSystemTestCase
       click_button 'Update'
     end
 
-    assert_text annot.annotation_text
+    # Cancel the form
+    within 'form.annotation-submission' do
+      click_button 'Cancel'
+    end
+
+    # Check if the view is correct without reloading
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text annot.annotation_text
+    end
+
+    # After reload, make sure no replacing has taken place
+    visit(submission_path(id: @instance.id))
+    click_link 'Code'
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text annot.annotation_text
+    end
+
+    # Can't check for NOT having empty string since the empty string is a part of any string
   end
 
   test 'Enter invalid annotation and send - No content' do
@@ -224,9 +261,18 @@ class AnnotationsTest < ApplicationSystemTestCase
     within 'form.annotation-submission' do
       find('textarea.annotation-submission-input').fill_in with: initial
       click_button 'Annotate'
-    end
 
-    assert_css 'form.annotation-submission'
+      # Assuming the update did not go trough
+      # If the creation went trough, the cancel button would not exist anymore
+
+      click_button 'Cancel'
+    end
+    assert_selector '.annotation', count: 0
+
+    # After reload, make sure no creation has taken place
+    visit(submission_path(id: @instance.id))
+    click_link 'Code'
+    assert_selector '.annotation', count: 0
   end
 
   test 'Enter invalid annotation and send - Content too long' do
@@ -240,9 +286,19 @@ class AnnotationsTest < ApplicationSystemTestCase
     within 'form.annotation-submission' do
       find('textarea.annotation-submission-input').fill_in with: initial
       click_button 'Annotate'
+
+      # Assuming the update did not go trough
+      # If the creation went trough, the cancel button would not exist anymore
+      click_button 'Cancel'
     end
 
-    assert_css 'form.annotation-submission'
+    assert_selector '.annotation', count: 0
+    assert_no_text initial
+
+    # After reload, make sure no creation has taken place
+    visit(submission_path(id: @instance.id))
+    click_link 'Code'
+    assert_selector '.annotation', count: 0
     assert_no_text initial
   end
 
@@ -258,7 +314,19 @@ class AnnotationsTest < ApplicationSystemTestCase
       click_button 'Annotate'
     end
 
-    assert_text initial
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text initial
+    end
+    assert_no_css 'form.annotation-submission'
+
+    # After reload, make sure creation has taken place
+    visit(submission_path(id: @instance.id))
+    click_link 'Code'
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text initial
+    end
     assert_no_css 'form.annotation-submission'
   end
 
@@ -266,16 +334,31 @@ class AnnotationsTest < ApplicationSystemTestCase
     annot = create :annotation, submission: @instance, user: @zeus, line_nr: nil
     visit(submission_path(id: @instance.id))
     click_link 'Code'
-    assert_text annot.annotation_text
+
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text annot.annotation_text
+    end
     old_text = annot.annotation_text
 
     find('.annotation .annotation-control-button.annotation-edit i.mdi.mdi-pencil').click
-    replacement = Faker::Lorem.words(number: 128).join(' ')
+    replacement = Faker::Lorem.words(number: 32).join(' ')
     within 'form.annotation-submission' do
       find('textarea.annotation-submission-input').fill_in with: replacement
       click_button 'Update'
     end
-    assert_no_text old_text
-    assert_text replacement
+    within '.annotation' do
+      assert_no_text old_text
+      assert_text replacement
+    end
+
+    # After reload, make sure creation has taken place
+    visit(submission_path(id: @instance.id))
+    click_link 'Code'
+    assert_selector '.annotation', count: 1
+    within '.annotation' do
+      assert_text replacement
+    end
+    assert_no_css 'form.annotation-submission'
   end
 end

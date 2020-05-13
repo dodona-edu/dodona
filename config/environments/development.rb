b12385b9001e4f9e7a3b90b47c2b379a9d0bc500
Bart Mesuere <Bart.Mesuere@UGent.be>
2020-05-13 14:47:34 +0200

    only generate translations in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,6 +84,10 @@ Rails.application.configure do
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
 
+
+  # Regenerate js translation files
+  config.middleware.use I18n::JS::Middleware
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker

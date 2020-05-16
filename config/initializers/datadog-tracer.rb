0c8dcf126b9119f3da6e0c3e9755689c912534c9
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-05-16 12:13:36 +0200

    Update config/initializers/datadog-tracer.rb
    Co-authored-by: Bart Mesuere <Bart.Mesuere@UGent.be>

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,6 +2,7 @@ if Rails.env.production? || Rails.env.staging?
   Datadog.configure do |c|
     c.use :rails, service_name: 'dodona'
     c.use :delayed_job
+    c.use :dalli
     c.analytics_enabled = true
     c.version = Dodona::Application::VERSION
   end

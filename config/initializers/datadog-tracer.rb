8b8a2214fda755046659edc376d908e4dc29086f
Bart Mesuere <Bart.Mesuere@UGent.be>
2020-05-17 16:51:39 +0200

    Enable analytics for delayed job traces

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,7 +1,7 @@
 if Rails.env.production? || Rails.env.staging?
   Datadog.configure do |c|
     c.use :rails, service_name: 'dodona'
-    c.use :delayed_job
+    c.use :delayed_job, analytics_enabled: true
     c.use :dalli
     c.analytics_enabled = true
     c.version = Dodona::Application::VERSION

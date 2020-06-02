617622dda2561c0971b1a1eddae08a7f3302785e
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-06-02 09:56:46 +0200

    Replace button text with spinner when multi-adding users

--- app/assets/javascripts/evaluation.ts
+++ app/assets/javascripts/evaluation.ts
@@ -79,4 +79,25 @@ function interceptFeedbackActionClicks(
     });
 }
 
-export { interceptFeedbackActionClicks };
+function interceptAddMultiUserClicks(): void {
+    let running = false;
+    document.querySelectorAll(".user-select-option a").forEach(option => {
+        option.addEventListener("click", async event => {
+            if (!running) {
+                running = true;
+                event.preventDefault();
+                const button = option.querySelector(".button");
+                const loader = option.querySelector(".loader");
+                button.classList.add("hidden");
+                loader.classList.remove("hidden");
+                const response = await fetch(option.getAttribute("href"), { method: "POST" });
+                eval(await response.text());
+                loader.classList.add("hidden");
+                button.classList.remove("hidden");
+                running = false;
+            }
+        });
+    });
+}
+
+export { interceptAddMultiUserClicks, interceptFeedbackActionClicks };

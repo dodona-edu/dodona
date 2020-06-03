7c60e0a009145e18f08cb189fb7d28fc9354c808
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-06-03 10:02:56 +0200

    Bump version

--- config/initializers/00_version.rb
+++ config/initializers/00_version.rb
@@ -2,8 +2,8 @@ module Dodona
   class Application
     module Version
       MAJOR = 3
-      MINOR = 6
-      PATCH = 14
+      MINOR = 7
+      PATCH = 0
 
       STRING = [MAJOR, MINOR, PATCH].compact.join('.')
     end

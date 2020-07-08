800068a2c1eabad06bfd276b9aae09c9c6f04dbb
Pieter De Clercq <pieterdeclercq@outlook.com>
2020-07-08 12:56:47 +0200

    Add institutions integration test

--- test/controllers/institution_controller_test.rb
+++ test/controllers/institution_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class InstitutionControllerTest < ActionDispatch::IntegrationTest
+  extend CRUDTest
+
+  crud_helpers Institution, attrs: %i[name short_name]
+
+  setup do
+    @instance = create(:institution)
+    sign_in create(:zeus)
+  end
+
+  test_crud_actions only: %i[index show]
 end

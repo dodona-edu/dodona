8cc8b0f04999c86c21eb60d40b1209d81969ca15
Mathieu Coussens <mathieu_coussens@hotmail.com>
2020-07-09 17:00:45 +0200

    Update annotate in models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: annotations
+#
+#  id              :bigint           not null, primary key
+#  line_nr         :integer
+#  submission_id   :integer
+#  user_id         :integer
+#  annotation_text :text(65535)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  evaluation_id   :bigint
+#  type            :string(255)
+#  question_state  :integer
+#
 class Question < Annotation
   before_create :set_question_state
 

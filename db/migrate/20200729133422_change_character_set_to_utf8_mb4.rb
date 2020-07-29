d07e0384f117d699e4a56c6c97025a91de57cf8f
Rien Maertens <rien.maertens@posteo.be>
2020-07-29 17:02:17 +0200

    Add migration to convert database to utf8mb4

---
+++ db/migrate/20200729133422_change_character_set_to_utf8_mb4.rb
@@ -0,0 +1,29 @@
+class ChangeCharacterSetToUtf8Mb4 < ActiveRecord::Migration[6.0]
+  CHARSET = 'utf8mb4'
+  COLLATION = 'utf8mb4_0900_ai_ci'
+
+  def db
+    ActiveRecord::Base.connection
+  end
+
+  def up
+    execute "ALTER DATABASE `#{db.current_database}` CHARACTER SET #{CHARSET} COLLATE #{COLLATION};"
+    db.tables.each do |table|
+      execute "ALTER TABLE `#{table}` CONVERT TO CHARACTER SET #{CHARSET} COLLATE #{COLLATION};"
+
+      db.columns(table).each do |column|
+        case column.sql_type
+          when /([a-z]*)text/i
+            default = (column.default.blank?) ? '' : "DEFAULT '#{column.default}'"
+            null = (column.null) ? '' : 'NOT NULL'
+            execute "ALTER TABLE `#{table}` MODIFY `#{column.name}` #{column.sql_type.upcase} CHARACTER SET #{CHARSET} COLLATE #{COLLATION} #{default} #{null};"
+          when /varchar\(([0-9]+)\)/i
+            sql_type = column.sql_type.upcase
+            default = (column.default.blank?) ? '' : "DEFAULT '#{column.default}'"
+            null = (column.null) ? '' : 'NOT NULL'
+            execute "ALTER TABLE `#{table}` MODIFY `#{column.name}` #{sql_type} CHARACTER SET #{CHARSET} COLLATE #{COLLATION} #{default} #{null};"
+        end
+      end
+    end
+  end
+end

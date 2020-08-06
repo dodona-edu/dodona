7cf20b80f64ddf10ebe2945c79e275ff85984ed8
winniederidder <Winnie.De.Ridder@hotmail.com>
2020-08-06 14:04:22 +0200

    Update booleans in the migration

--- db/migrate/20200805113636_add_description_present_to_activities.rb
+++ db/migrate/20200805113636_add_description_present_to_activities.rb
@@ -2,5 +2,15 @@ class AddDescriptionPresentToActivities < ActiveRecord::Migration[6.0]
   def change
     add_column :activities, :description_nl_present, :boolean, default: false
     add_column :activities, :description_en_present, :boolean, default: false
+
+    Activities.all.each do |activity|
+      languages = activity.description_languages
+      if languages.include? 'en'
+        activity.update description_en_present: true
+      end
+      if languages.include? 'nl'
+        activity.update description_nl_present: true
+      end
+    end
   end
 end

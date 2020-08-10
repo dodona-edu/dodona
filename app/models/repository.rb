36c08e5a30eb74b842cc659d8d467f39f847d2bf
winniederidder <Winnie.De.Ridder@hotmail.com>
2020-08-10 09:03:12 +0200

    Restyling

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -53,8 +53,9 @@ class Repository < ApplicationRecord
   def public_files
     pathname_dir = Pathname.new public_path
     Dir[File.join(public_path, '**/*')] # uses entries in the locally stored Dodona copy of the repository
-      .select { |f| File.file? f } # skip directories such as ., icons/
-      .map { |f| Pathname.new(f).relative_path_from pathname_dir } # make them relative again to be able to create urls
+      .map { |f| Pathname.new f }
+      .select { |path| path.file? && path.readable? } # skip directories such as ., icons/
+      .map { |path| path.relative_path_from pathname_dir } # make them relative again to be able to create urls
   end
 
   def media_path

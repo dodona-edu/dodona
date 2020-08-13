8fae36b0e136896d65b19f2d95c316d0963a191b
LucianosLionakis <lucianos.lionakis@ugent.be>
2020-08-13 12:29:58 +0200

    Added searchbar, by_institution scope to the provider model and applyed the scope in the authentication_controller. Also created an view to render the institutions.

--- app/controllers/auth/authentication_controller.rb
+++ app/controllers/auth/authentication_controller.rb
@@ -1,12 +1,14 @@
 class Auth::AuthenticationController < Devise::SessionsController
   # A sign-out route is inherited from the parent controller.
 
+  has_scope :by_institution, as: 'institution_id'
+
   skip_before_action :verify_authenticity_token, raise: false
 
   def sign_in
     @providers = Provider.all
     @title = I18n.t('auth.sign_in.sign_in')
-
+    @institution_providers = apply_scopes(@providers.where.not(type: Provider::Saml.name))
     render 'auth/sign_in'
   end
 end

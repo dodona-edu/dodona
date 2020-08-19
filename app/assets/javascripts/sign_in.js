3f8d04531874ab4622ae0773ba570cba292394c5
LucianosLionakis <lucianos.lionakis@ugent.be>
2020-08-19 10:49:50 +0200

    Added use of localStorage

--- app/assets/javascripts/sign_in.js
+++ app/assets/javascripts/sign_in.js
@@ -1,31 +1,43 @@
+/* globals Bloodhound */
+
 function initInstitutionAutoSelect(institutions, links) {
-    let institution = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    const institution = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         local: institutions,
     });
 
-    $('#scrollable-dropdown-menu .typeahead').typeahead({
-            minLength: 0,
-            highlight: true
-        }, {
-            name: 'institution',
-            display: 'name',
-            limit: institutions.length,
-            source: institutionsWithDefaults
-        }); 
+    $("#scrollable-dropdown-menu .typeahead").typeahead({
+        minLength: 0,
+        highlight: true
+    }, {
+        name: "institution",
+        display: "name",
+        limit: institutions.length,
+        source: institutionsWithDefaults
+    });
 
     function institutionsWithDefaults(q, sync) {
-        if (q === '') {
+        if (q === "") {
             sync(institution.all()); // This is the only change needed to get 'ALL' items as the defaults
         } else {
             institution.search(q, sync);
         }
     }
 
-    $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
-        $('#sign-in').attr("href", links[suggestion.type]);
+    $(".typeahead").bind("typeahead:select", function (ev, suggestion) {
+        $("#sign-in").attr("href", links[suggestion.type]);
+
+        localStorage.setItem("institution", JSON.stringify(suggestion));
     });
+
+    // Check if there is an institution in localStorage if so set it as default selection.
+    const localStorageInstitution = localStorage.getItem("institution");
+    if (localStorageInstitution !== undefined) {
+        const institution = JSON.parse(localStorageInstitution);
+        $(".typeahead").typeahead("val", institution.name);
+        $("#sign-in").attr("href", links[institution.type]);
+    }
 }
 
 export { initInstitutionAutoSelect };

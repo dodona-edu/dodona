348805b31a9ee52db8cc659275e843e92188a215
Pieter De Clercq <pieterdeclercq@outlook.com>
2020-09-02 19:28:32 +0200

    Add deployment id

--- lib/LTI/messages/types/deep_linking_response.rb
+++ lib/LTI/messages/types/deep_linking_response.rb
@@ -39,6 +39,7 @@ module LTI::Messages::Types
       }
       base[LTI::Messages::Claims::DEEP_LINKING_CONTENT_ITEMS] = @items.map(&:as_json)
       base[LTI::Messages::Claims::DEEP_LINKING_DATA] = 'Brightspace'
+      base["https://purl.imsglobal.org/spec/lti/claim/deployment_id"] = 'c6899818-7062-44d1-b377-5a08097daeb3'
       base[LTI::Messages::Claims::MESSAGE_TYPE] = TYPE
       base[LTI::Messages::Claims::VERSION] = "1.3.0"
       base

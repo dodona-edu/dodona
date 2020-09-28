25ebc961857fec8cc17346f7b5ce4d9903a5e92b
Niko Strijbol <strijbol.niko@gmail.com>
2020-09-28 17:12:58 +0200

    Login page improvements
    - Don't focus on typeahead
    - Fix button selection
    - Small improvements in login JS
    - Some clean-up: less hardcoded providers

--- app/controllers/auth/authentication_controller.rb
+++ app/controllers/auth/authentication_controller.rb
@@ -6,11 +6,23 @@ class Auth::AuthenticationController < Devise::SessionsController
   skip_before_action :verify_authenticity_token, raise: false
 
   def sign_in
+    # Providers that are not necessarily specific to one institution.
+    @generic_providers = {
+      Provider::Smartschool => { image: 'smartschool.png', name: 'Smartschool' },
+      Provider::Office365 => { image: 'office365.png', name: 'Office 365' },
+      Provider::GSuite => { image: 'google_oauth2.png', name: 'G Suite' }
+    }
+
+    # Calculate some information for these providers.
+    @generic_providers.each do |key, value|
+      value[:link] = omniauth_authorize_path(:user, key.sym)
+    end
+
     @providers = Provider.all
     @title = I18n.t('auth.sign_in.sign_in')
     @oauth_providers = apply_scopes(@providers
       .includes(:institution)
-      .where(type: [Provider::Smartschool, Provider::Office365, Provider::GSuite])
+      .where(type: @generic_providers.keys)
       .where(mode: :prefer)
       .where.not(institutions: { name: Institution::NEW_INSTITUTION_NAME }))
     render 'auth/sign_in'

11f750a0121f8ccfecc2b9ab2949fbd6be86b106
Niko Strijbol <strijbol.niko@gmail.com>
2021-02-18 21:51:18 +0100

    Add test for max score on evaluation

---
+++ test/models/evaluation_test.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: evaluations
+#
+#  id         :bigint           not null, primary key
+#  series_id  :integer
+#  released   :boolean          default(FALSE), not null
+#  deadline   :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class EvaluationTest < ActiveSupport::TestCase
+  test 'maximum score is correct' do
+    evaluation = create :evaluation, :with_submissions
+    exercises = evaluation.evaluation_exercises
+    r1 = create :rubric, maximum: '12.0', evaluation_exercise: exercises.first
+    r2 = create :rubric, maximum: '5.0', evaluation_exercise: exercises.first
+    r3 = create :rubric, maximum: '7.0', evaluation_exercise: exercises[1]
+
+    assert evaluation.maximum_score == r1.maximum + r2.maximum + r3.maximum
+  end
+end

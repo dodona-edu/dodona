baf82c2ee8d3c01aacc7ae68b483835f3e18af00
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-03-15 12:03:20 +0100

    robots.txt on non-production servers

--- config/deploy/mestra.rb
+++ config/deploy/mestra.rb
@@ -4,7 +4,7 @@ server 'mestra.ugent.be', user: 'dodona', roles: %i[web app worker], ssh_options
     port: 4840
 }
 
-set :branch, ENV['GITHUB_SHA']
+set :branch, ENV['GITHUB_SHA'] || 'develop'
 set :rails_env, :development
 
 set :delayed_job_workers, 3
@@ -13,36 +13,18 @@ set :bundle_without, ''
 
 #set :linked_files, fetch(:linked_files, []).push('tmp/development_secret.txt')
 
-# Perform yarn install before precompiling the assets in order to pass the
-# integrity check.
-# namespace :deploy do
-#   namespace :assets do
-#     before :precompile, :yarn_install do
-#       on release_roles(fetch(:assets_roles)) do
-#         within release_path do
-#           with rails_env: fetch(:rails_env) do
-#             execute :yarn, "install"
-#           end
-#         end
-#       end
-#     end
-#   end
-#   before :publishing, :asset_stuff do
-#     on roles :web do
-#       within release_path do
-#         with rails_env: fetch(:rails_env) do
-#           execute :rake, 'assets:nodigest'
-#         end
-#       end
-#     end
-#   end
-# end
-
 namespace :deploy do
   before :restart, :reset_db do
     on roles(:web) do
       within release_path do
         execute :rake, 'db:reset'
+      end
+    end
+  end
+  before :restart, :mv_robots do
+    on roles(:web) do
+      within release_path do
+        execute :mv, 'public/robots.development.txt', 'public/robots.txt'
       end
     end
   end

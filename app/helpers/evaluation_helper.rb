f573f964d0fe8fee63b8845ccbf1a342c229b5cc
Niko Strijbol <strijbol.niko@gmail.com>
2021-03-16 13:23:21 +0100

    Fix bug with no score

--- app/helpers/evaluation_helper.rb
+++ app/helpers/evaluation_helper.rb
@@ -3,9 +3,13 @@ require 'csv'
 module EvaluationHelper
   include ActionView::Helpers::NumberHelper
 
-  def format_score(score, lang = nil)
+  def format_score(score, lang = nil, numeric_only = false)
     if score.nil?
-      '-'
+      if numeric_only
+        ''
+      else
+        '-'
+      end
     else
       number_with_precision(score, precision: 2, strip_insignificant_zeros: true, locale: lang)
     end

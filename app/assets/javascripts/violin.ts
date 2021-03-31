f177926d6ebcc9a4ca9d8c8c6515b84038b008f7
tibdhond <tibo.dhondt@telenet.be>
2021-03-31 21:49:59 +0200

    timeseries data prep

--- app/assets/javascripts/violin.ts
+++ app/assets/javascripts/violin.ts
@@ -1,8 +1,9 @@
 import * as d3 from "d3";
 
 function vtest() {
-    console.log(d3.json('/nl/stats/violin?course_id=5&series_id=108'));
-    console.log(d3.json('/nl/stats/stacked_status?course_id=5&series_id=108'))
+    // console.log(d3.json('/nl/stats/violin?course_id=5&series_id=108'));
+    // console.log(d3.json('/nl/stats/stacked_status?course_id=5&series_id=108'));
+    console.log(d3.json('/nl/stats/timeseries?course_id=5&series_id=108'));
 }
 
 

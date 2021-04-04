c6bb6a853d6b35c34b68256fafa000eceb31a0f1
tibdhond <tibo.dhondt@telenet.be>
2021-04-04 22:49:20 +0200

    added timeseries + remove debug statements

--- app/javascript/packs/visualisations.js
+++ app/javascript/packs/visualisations.js
@@ -2,11 +2,13 @@ import { initHeatmap } from "heatmap.ts";
 import { initPunchcard } from "punchcard.js";
 import { initViolin, vtest } from "violin.ts"
 import { initStacked } from "stacked_status.ts"
+import { initTimeseries } from "timeseries.ts"
 
 window.dodona.initHeatmap = initHeatmap;
 window.dodona.initPunchcard = initPunchcard;
 window.dodona.initViolin = initViolin;
-window.dodona.initStacked = initStacked;    
+window.dodona.initStacked = initStacked;
+window.dodona.initTimeseries = initTimeseries
 
 // for figuring out input
 window.dodona.vtest = vtest;

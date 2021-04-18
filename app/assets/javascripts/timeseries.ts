1f1a22c89385cf154cebce92d196867ce1cca107
tibdhond <tibo.dhondt@telenet.be>
2021-04-19 00:34:54 +0200

    timeseries: cumulative version + fixed height calculation

--- app/assets/javascripts/timeseries.ts
+++ app/assets/javascripts/timeseries.ts
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 
-let selector = undefined;
+let selector = "";
 const margin = { top: 40, right: 10, bottom: 0, left: 70 };
 let width = 0;
 let height = 0;
@@ -8,13 +8,14 @@ const statusOrder = [
     "correct", "wrong", "compilation error", "runtime error",
     "time limit exceeded", "memory limit exceeded", "output limit exceeded",
 ];
-const commonAxis = true;
+const commonAxis = false;
+const rSum = false; // use running sum?
 
 function drawTimeSeries(data, metaData, exMap): void {
-    height = 200 * Object.keys(data).length;
+    const yDomain: string[] = Array.from(new Set(Object.keys(data)));
+    height = 100 * yDomain.length;
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
-
 
     // position graph
     const graph = d3.select(selector)
@@ -28,24 +29,24 @@ function drawTimeSeries(data, metaData, exMap): void {
     // Show the Y scale for exercises (Big Y scale)
     const y = d3.scaleBand()
         .range([innerHeight, 0])
-        .domain(Object.keys(data))
+        .domain(yDomain)
         .paddingInner(.2)
         .paddingOuter(.4);
     graph.append("g")
         .attr("transform", `translate(0, ${-y.bandwidth() - 10})`)
-        .call(d3.axisLeft(y).tickSize(0).tickFormat(t => exMap[t]))
+        .call(d3.axisLeft(y).tickSize(0).tickFormat(id => exMap[id]))
         .select(".domain").remove();
 
     // common y scale per exercise
     const interY = d3.scaleLinear()
-        .domain([0, metaData.maxStack])
+        .domain([0, rSum ? metaData.maxCumulative : metaData.maxStack])
         .range([y.bandwidth(), 0]);
 
     // y scales unique to each exercise (just in case we still want to offer the option)
     const interYs = Object.entries(metaData["per_ex"]).reduce((acc, v) => {
         acc[v[0]] =
             d3.scaleLinear()
-                .domain([0, v[1].maxStack])
+                .domain([0, rSum ? v[1].maxCumulative : v[1].maxStack])
                 .range([y.bandwidth(), 0]);
         return acc;
     }, {}
@@ -128,12 +129,11 @@ function drawTimeSeries(data, metaData, exMap): void {
                 return d3.area()
                     .x(r => x(r.date))
                     .y0(r => commonAxis ?
-                        interY(r.stack_sum - r.count) - y.bandwidth() :
-                        interYs[exId](r.stack_sum - r.count) - y.bandwidth())
+                        interY((rSum ? r.cSumStart : r.stack_sum - r.count)) - y.bandwidth() :
+                        interYs[exId]((rSum ? r.cSumStart : r.stack_sum - r.count)) - y.bandwidth())
                     .y1(r => commonAxis ?
-                        interY(r.stack_sum) - y.bandwidth() :
-                        interYs[exId](r.stack_sum) - y.bandwidth())
-                    .curve(d3.curveCatmullRom)(d[1]);
+                        interY(rSum ? r.cSumEnd : r.stack_sum) - y.bandwidth() :
+                        interYs[exId](rSum ? r.cSumEnd : r.stack_sum) - y.bandwidth())(d[1]);
             });
 
         // y axis
@@ -147,7 +147,7 @@ function initTimeseries(url, containerId): void {
     selector = containerId;
     const container = d3.select(selector);
     container.html(""); // clean up possible previous visualisations
-
+    //
     width = (container.node() as Element).getBoundingClientRect().width;
     container.attr("class", "text-center").append("span")
         .text(I18n.t("js.loading"));
@@ -169,6 +169,7 @@ function initTimeseries(url, containerId): void {
         metaData["minDate"] = Date.parse(data[Object.keys(data)[0]][0].date);
         metaData["maxDate"] = Date.parse(data[Object.keys(data)[0]][0].date);
         metaData["maxStack"] = 0;
+        metaData["maxCumulative"] = 0;
         metaData["per_ex"] = {};
         // let minDate = Date.parse(data[Object.keys(data)[0]][0].date);
         // let maxDate = Date.parse(data[Object.keys(data)[0]][0].date);
@@ -192,7 +193,8 @@ function initTimeseries(url, containerId): void {
             let prevDate = records[0].date;
             let stackSum = 0; // cumulative sum (per ex) per day
             let maxStack = 0; // Used to calculate extent for y-axis
-            // let cum_sum = 0      // cum_sum per ex per status
+            const cSum = {}; // cSum per ex per status
+            statusOrder.forEach(s => cSum[s] = 0);
             // Construct a new data array where every status is represented
             const newRecords = [];
             // Used to track if for every day that has datepoints, every status is represented
@@ -202,12 +204,15 @@ function initTimeseries(url, containerId): void {
                     // insert empty datapoints to prevent weird jumps in the graph
                     // if not all statusses have been visted (for this date),
                     // append the unvisited ones
-                    while (statusVisited !== statusOrder.length - 1) {
+                    while (statusVisited !== statusOrder.length) {
+                        cSum[statusOrder[statusVisited]] += stackSum;
                         newRecords.push({
                             "date": prevDate,
                             "status": statusOrder[statusVisited],
                             "count": 0,
-                            "stack_sum": stackSum
+                            "stack_sum": stackSum,
+                            "cSumEnd": cSum[statusOrder[statusVisited]],
+                            "cSumStart": !statusVisited ? 0 : cSum[statusOrder[statusVisited-1]]
                         });
                         statusVisited += 1;
                     }
@@ -219,22 +224,38 @@ function initTimeseries(url, containerId): void {
                 // insert empty datapoints to prevent weird jumps in the graph
                 // if one or more statusses were skipped, insert them
                 while (statusOrder[statusVisited] !== d.status) {
+                    cSum[statusOrder[statusVisited]] += stackSum;
                     newRecords.push({
                         "date": prevDate,
                         "status": statusOrder[statusVisited],
                         "count": 0,
-                        "stack_sum": stackSum
+                        "stack_sum": stackSum,
+                        "cSumEnd": cSum[statusOrder[statusVisited]],
+                        "cSumStart": !statusVisited ? 0 : cSum[statusOrder[statusVisited-1]]
                     });
                     statusVisited += 1;
                 }
                 metaData["minDate"] = Math.min(metaData["minDate"], d.date);
                 metaData["maxDate"] = Math.max(metaData["maxDate"], d.date);
                 stackSum += d.count;
+                cSum[d.status] += stackSum;
+                newRecords.push({
+                    ...d,
+                    "stack_sum": stackSum,
+                    "cSumEnd": cSum[d.status],
+                    "cSumStart": !statusVisited ? 0 : cSum[statusOrder[statusVisited-1]]
+                });
                 statusVisited += 1;
-                newRecords.push({ ...d, "stack_sum": stackSum });
             });
             metaData["maxStack"] = Math.max(maxStack, metaData["maxStack"]);
+            if (newRecords.length) {
+                metaData["maxCumulative"] = Math.max(
+                    newRecords[newRecords.length - 1].cSumEnd,
+                    metaData["maxCumulative"]);
+            }
             metaData["per_ex"][exId]["maxStack"] = maxStack;
+            metaData["per_ex"][exId]["maxCumulative"] = newRecords.length ?
+                newRecords[newRecords.length - 1].cSumEnd : 0;
             data[exId] = d3.groups(newRecords, d => d.status);
         });
 
@@ -243,5 +264,4 @@ function initTimeseries(url, containerId): void {
     d3.json(url)
         .then(processor);
 }
-
 export { initTimeseries };

2752a587f2e6bf808f6d2a7b0806cdfafef0c4c4
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-04-25 16:05:57 +0200

    UI tweaks

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,4 @@
 module UsersHelper
-  def editable_permissions
-    perms = User.permissions.to_h
-    perms.delete('zeus') if current_user.staff?
-    perms
-  end
-
-  def can_edit_permissions?(user)
-    return true if current_user.zeus?
-    return false if current_user.student?
-
-    user.student? || user.staff?
-  end
-
   def user_page_navigation_links(users, opts, action = 'index')
     opts ||= {}
     controller = 'users' if users.try(:total_pages)

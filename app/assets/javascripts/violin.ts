8d6ab4e5774dc02c003373b7c9055ca2dd489b52
tibdhond <tibo.dhondt@telenet.be>
2021-04-27 13:28:28 +0200

    violin more informative tooltips + all tooltips at once

--- app/assets/javascripts/violin.ts
+++ app/assets/javascripts/violin.ts
@@ -3,14 +3,14 @@ import { formatTitle } from "graph_helper.js";
 
 
 let selector = undefined;
-const margin = { top: 20, right: 10, bottom: 40, left: 105 };
+const margin = { top: 20, right: 50, bottom: 40, left: 105 };
 let width = 0;
 let height = 0;
 
 function drawViolin(data: {
     "ex_id": string;
-    "counts": [number];
-    "freq": Record<string, {label: string; freq: number}>;
+    "counts": number[];
+    "freq": number[][];
     "median": number;
 }[], exMap: Record<string, string>): void {
     const min = d3.min(data, d => d3.min(d.counts));
@@ -24,7 +24,7 @@ function drawViolin(data: {
     const yAxisPadding = 5; // padding between y axis (labels) and the actual graph
 
     const maxFreq = d3.max(data, d => d3.max(
-        Object.values(d.freq), (f: {label: string; freq: number}) => f.freq
+        d.freq, (bin: number[]) => bin.length
     ));
 
     const graph = d3.select(selector)
@@ -67,7 +67,7 @@ function drawViolin(data: {
     // Add X axis label:
     graph.append("text")
         .attr("text-anchor", "end")
-        .attr("x", 0)
+        .attr("x", 20)
         .attr("y", innerHeight+margin.top+10)
         .text(I18n.t("js.n_submissions"))
         .attr("class", "violin-label")
@@ -81,14 +81,14 @@ function drawViolin(data: {
         .attr("transform", d => `translate(0, ${y(d.ex_id) + y.bandwidth() / 2})`)
         .append("path")
         .datum(ex => {
-            return Object.values(ex.freq);
+            return ex.freq;
         }
         )
         .attr("class", "violin-path")
         .attr("d", d3.area()
-            .x(d => x(d.label))
-            .y0(d => -yBin(d.freq))
-            .y1(d => yBin(d.freq))
+            .x((_, i) => x(i+1))
+            .y0(d => -yBin(d.length))
+            .y1(d => yBin(d.length))
             .curve(d3.curveCatmullRom)
         );
 
@@ -100,61 +100,82 @@ function drawViolin(data: {
         .attr("transform", d => `translate(0, ${y(d.ex_id) + y.bandwidth() / 2})`);
 
 
-    function onMouseOver(d, groupName): void {
-        const location = graph.selectAll("#cursor").data([d]).enter()
-            .append("g")
-            .attr("id", "cursor");
-        location
-            .selectAll("cursorLine")
-            .data([d])
-            .enter()
-            .append("line")
-            .attr("x1", d => x(d))
-            .attr("x2", d => x(d))
-            .attr("y1", y(groupName))
-            .attr("y2", y(groupName) + y.bandwidth())
-            .attr("pointer-events", "none")
-            .attr("stroke", "currentColor")
-            .style("width", 40);
-        location
-            .selectAll("cursorText")
-            .data([d])
-            .enter()
-            .append("text")
-            .attr("class", "label")
-            .text(d => d)
-            .attr("x", d => x(d))
-            .attr("y", y(groupName) + y.bandwidth() * 1.5)
-            .attr("text-anchor", "middle")
-            .attr("font-family", "sans-serif")
-            .attr("pointer-events", "none")
-            .attr("fill", "currentColor")
-            .attr("font-size", "11px");
+    function onMouseOver(d): void {
+        for (const ex of data) {
+            const location = graph
+                .append("g")
+                .attr("class", "cursor");
+            location
+                .selectAll("cursorLine")
+                .data([d])
+                .enter()
+                .append("line")
+                .attr("x1", d => x(d))
+                .attr("x2", d => x(d))
+                .attr("y1", y(ex.ex_id))
+                .attr("y2", y(ex.ex_id) + y.bandwidth())
+                .attr("pointer-events", "none")
+                .attr("stroke", "currentColor")
+                .style("width", 40);
+            location
+                .selectAll("cursorTextTop")
+                .data([d])
+                .enter()
+                .append("text")
+                .attr("class", "label")
+                .text(d => {
+                    const length = ex.freq[d-1].length;
+                    return `${length} ${length !== 1 ?
+                        I18n.t("js.users"):
+                        I18n.t("js.user")} ${I18n.t("js.with")}`;
+                })
+                .attr("x", d => x(d))
+                .attr("y", y(ex.ex_id) - y.bandwidth() * .1)
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-serif")
+                .attr("pointer-events", "none")
+                .attr("fill", "currentColor")
+                .attr("font-size", "11px");
+            location
+                .selectAll("cursorTextBottom")
+                .data([d])
+                .enter()
+                .append("text")
+                .attr("class", "label")
+                .text(d => `${d} ${d !== 1 ?
+                    I18n.t("js.submissions") :
+                    I18n.t("js.submission")
+                }`)
+                .attr("x", d => x(d))
+                .attr("y", y(ex.ex_id) + y.bandwidth() * 1.25)
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-serif")
+                .attr("pointer-events", "none")
+                .attr("fill", "currentColor")
+                .attr("font-size", "11px");
+        }
     }
 
     function onMouseOut(): void {
-        graph.selectAll("#cursor").remove();
+        graph.selectAll(".cursor").remove();
     }
 
     // add invisible bars between each tick to support cursor functionality
-    for (const ex of data) {
-        const group = graph.selectAll(`#e${ex.ex_id}`); // html doesn"t seem to like numerical id"s
-        group
-            .selectAll("invisibars")
-            // hack to quickly make list from 1 to 10
-            .data(Array.from(Array(max+2).keys()).slice(1, -1))
-            .enter()
-            .append("rect")
-            .attr("x", d => x(d) - elWidth/2)
-            .attr("y", -yBin.range()[1] / 2)
-            .attr("width", elWidth)
-            .attr("height", y.bandwidth())
-            .attr("stroke", "black")
-            .attr("class", "violin-invisibar")
-            .attr("pointer-events", "all")
-            .on("mouseover", (_, d) => onMouseOver(d, ex.ex_id))
-            .on("mouseout", onMouseOut);
-    }
+    graph
+        .selectAll("invisibars")
+        // hack to quickly make list from 1 to 10
+        .data(d3.range(1, max+1))
+        .enter()
+        .append("rect")
+        .attr("x", d => x(d) - elWidth/2)
+        .attr("y", margin.top)
+        .attr("width", elWidth)
+        .attr("height", innerWidth)
+        .attr("stroke", "black")
+        .attr("class", "violin-invisibar")
+        .attr("pointer-events", "all")
+        .on("mouseover", (_, d) => onMouseOver(d))
+        .on("mouseout", onMouseOut);
     graph
         .selectAll("medianDot")
         .data(data)
@@ -175,6 +196,7 @@ function initViolin(url: string, containerId: string, containerHeight: number): 
     if (!height) {
         height = container.node().clientHeight - 5;
     }
+    container.node().style.height = height;
     container.html(""); // clean up possible previous visualisations
     container.attr("class", "text-center").append("span").text(I18n.t("js.loading"));
 
@@ -186,21 +208,28 @@ function initViolin(url: string, containerId: string, containerHeight: number): 
         }
         d3.select(`${selector} *`).remove();
 
+        height = 150 * Object.keys(raw.data).length;
+        container.node().style.height = height;
+
         const data = Object.keys(raw.data).map(k => ({
             "ex_id": k,
             // sort so median is calculated correctly
-            "counts": raw.data[k].map(x => parseInt(x)).sort((a, b) => a-b),
+            "counts": raw.data[k].map(x => parseInt(x)).sort((a: number, b: number) => a-b),
             "freq": {},
             "median": 0
-        }));
+        })) as {
+            "ex_id": string;
+            "counts": number[];
+            "freq": number[][];
+            "median": number;
+        }[];
+
+        const maxCount: number = d3.max(data, d => d3.max(d.counts));
+
 
         data.forEach(ex => {
-            ex["freq"] = ex.counts.reduce((acc, v) => {
-                Object.prototype.hasOwnProperty.call(acc, v) ?
-                    acc[v].freq += 1 :
-                    acc[v] = { label: v, freq: 1 };
-                return acc;
-            }, {});
+            ex["freq"] = d3.bin().thresholds(d3.range(1, maxCount+1))
+                .domain([1, maxCount])(ex.counts);
 
             ex.median = d3.quantile(ex.counts, .5);
         });

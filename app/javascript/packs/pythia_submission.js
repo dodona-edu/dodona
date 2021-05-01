de4ba202b1cdd703a519d653ce20aab0065c6b6e
Bart Mesuere <Bart.Mesuere@UGent.be>
2021-05-01 11:00:06 +0200

    add iframe lib to other pages

--- app/javascript/packs/pythia_submission.js
+++ app/javascript/packs/pythia_submission.js
@@ -1,3 +1,6 @@
 import { initPythiaSubmissionShow } from "pythia_submission.js";
 
 window.dodona.initPythiaSubmissionShow = initPythiaSubmissionShow;
+
+// will automaticaly bind to window.iFrameResize()
+require("iframe-resizer"); // eslint-disable-line no-undef

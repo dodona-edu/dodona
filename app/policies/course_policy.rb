74535d07ebe8df047d4abde2c4d37cba83bc21f1
Bart Mesuere <Bart.Mesuere@UGent.be>
2021-06-12 21:25:11 +0200

    allow zeus to mark a course as featured

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -150,7 +150,9 @@ class CoursePolicy < ApplicationPolicy
 
   def permitted_attributes
     # record is the Course class on create
-    if course_admin? || (record == Course && user&.admin?)
+    if zeus?
+      %i[name year description visibility registration color teacher institution_id moderated enabled_questions featured]
+    elsif course_admin? || (record == Course && user&.admin?)
       %i[name year description visibility registration color teacher institution_id moderated enabled_questions]
     else
       []

4d55260a38cc194d85faeea762e7321407c48392
Bart Mesuere <Bart.Mesuere@UGent.be>
2021-08-22 15:48:41 +0200

    fix tests

--- test/system/series_visualisations_test.rb
+++ test/system/series_visualisations_test.rb
@@ -17,62 +17,38 @@ class SeriesVisualisationsTest < ApplicationSystemTestCase
     visit(course_path(id: @c1.id))
   end
 
-  test 'Can toggle between exercises list and visualisations' do
-    assert_selector '.stats-active', count: 0
-    toggle = find('.mdi.mdi-chart-line')
-
-    toggle.click
-    assert_selector '.stats-active', count: 1 # card should have received this class
-    assert_selector '.mdi.mdi-chart-line', count: 0 # button class should have been replaced
-    assert_selector '.mdi.mdi-format-list-bulleted', count: 1
-
-    # should be able to toggle back
-    toggle.click
-    assert_selector '.stats-active', count: 0 # this class should have been removed
-    assert_selector '.mdi.mdi-chart-line', count: 1 # button class should have been replaced
-    assert_selector '.mdi.mdi-format-list-bulleted', count: 0
-
-    # make sure nothing broke when toggling back
-    toggle.click
-    assert_selector '.stats-active', count: 1 # card should have received this class
-    assert_selector '.mdi.mdi-chart-line', count: 0 # button class should have been replaced
-    assert_selector '.mdi.mdi-format-list-bulleted', count: 1
-  end
-
   test 'Can toggle between types of graphs' do
-    find('.mdi.mdi-chart-line').click
+    assert_no_selector '.btn.graph-toggle.active .violin'
+
+    find('.btn.graph-toggle .violin').click
+
     title = find('.graph-title > span')
 
-    assert_selector '.btn.annotation-toggle.violin.active' # violin should be active by default
-    assert_selector '.btn.annotation-toggle.active', count: 1 # only one buttom active at a time
+    assert_selector '.btn.graph-toggle.active .violin'
     within title do
       assert_text 'Number of submissions per user'
     end
 
-    find('.btn.annotation-toggle.stacked').click
-    assert_selector '.btn.annotation-toggle.stacked.active'
-    assert_selector '.btn.annotation-toggle.active', count: 1 # only one active
+    find('.btn.graph-toggle .stacked-bar-chart').click
+    assert_selector '.btn.graph-toggle.active .stacked-bar-chart'
     within title do
       assert_text 'Distribution of submission statuses'
     end
 
-    find('.btn.annotation-toggle.timeseries').click
-    assert_selector '.btn.annotation-toggle.timeseries.active'
-    assert_selector '.btn.annotation-toggle.active', count: 1
-    within title do
-      assert_text 'Submissions over time'
-    end
+    # find('.btn.graph-toggle.timeseries').click
+    # assert_selector '.btn.graph-toggle.timeseries.active'
+    # within title do
+    #   assert_text 'Submissions over time'
+    # end
 
-    find('.btn.annotation-toggle.ctimeseries').click
-    assert_selector '.btn.annotation-toggle.ctimeseries.active'
-    assert_selector '.btn.annotation-toggle.active', count: 1
+    find('.btn.graph-toggle .stacked-line-chart').click
+    assert_selector '.btn.graph-toggle.active .stacked-line-chart'
     within title do
       assert_text 'Users with at least one correct submission'
     end
 
-    find('.btn.annotation-toggle.violin').click
-    assert_selector '.btn.annotation-toggle.violin.active' # violin should be active by default
-    assert_selector '.btn.annotation-toggle.active', count: 1
+    find('.btn.graph-toggle .violin').click
+    assert_selector '.btn.graph-toggle.active .violin'
     within title do
       assert_text 'Number of submissions per user'
     end

d139c6463c20015d51c8a006a5bfaa71ae6eb5de
TimonDB <timondb01@gmail.com>
2021-08-25 11:49:20 +0200

    Fix section indication in series navigation

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -133,7 +133,7 @@ function initCourseShow() {
         const nav = document.getElementById("scrollspy-nav");
         if (nav) {
             new ScrollSpy(nav, {
-                sectionSelector: ".series .anchor",
+                sectionSelector: ".series .anchor, .profile .anchor",
                 offset: 90,
             }).activate();
         }

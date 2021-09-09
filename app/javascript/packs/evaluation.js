23a2a393bcdebeb8262197df76dfc9b21eac6292
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2021-09-09 13:18:22 +0200

    Fix initCheckboxes not being available

--- app/javascript/packs/evaluation.js
+++ app/javascript/packs/evaluation.js
@@ -1,8 +1,9 @@
 import { initDeadlinePicker } from "series.js";
-import { initCheckbox, initEvaluationStepper } from "evaluation.ts";
+import { initCheckboxes, initCheckbox, initEvaluationStepper } from "evaluation.ts";
 import FeedbackActions from "feedback/actions";
 
 window.dodona.initDeadlinePicker = initDeadlinePicker;
 window.dodona.initCheckbox = initCheckbox;
+window.dodona.initCheckboxes = initCheckboxes;
 window.dodona.FeedbackActions = FeedbackActions;
 window.dodona.initEvaluationStepper = initEvaluationStepper;

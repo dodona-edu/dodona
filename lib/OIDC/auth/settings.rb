2ba3679d682901feb486e467642e604a8a2a05a7
Bart Mesuere <Bart.Mesuere@UGent.be>
2021-09-09 11:31:47 +0200

    Set response type to code

--- lib/OIDC/auth/settings.rb
+++ lib/OIDC/auth/settings.rb
@@ -11,7 +11,7 @@ module OIDC
           },
           discovery: true,
           response_mode: :form_post,
-          response_type: :id_token,
+          response_type: :code,
           scope: [:openid, :profile]
         }
       end

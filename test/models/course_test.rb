1872bf82a75f9b4ac162fbb24f76bffdd8bfda85
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2021-09-16 17:36:33 +0200

    Fix submission callbacks not being triggered when course is destroyed

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -238,7 +238,7 @@ class CourseTest < ActiveSupport::TestCase
   test 'destroying course does not destroy submissions' do
     course = create :course, series_count: 1, exercises_per_series: 1, submissions_per_exercise: 1
     assert_difference 'Submission.count', 0 do
-      course.destroy
+      assert course.destroy
     end
   end
 
@@ -246,7 +246,7 @@ class CourseTest < ActiveSupport::TestCase
     course = create :course, series_count: 1, exercises_per_series: 1, submissions_per_exercise: 1
     submission = Submission.first
     code = submission.code
-    course.destroy
+    assert course.destroy
     assert_equal code, submission.reload.code
   end
 end

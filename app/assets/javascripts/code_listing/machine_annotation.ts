18027cba0b03784dfe23b986f5e0e359b8e23efc
jorg <jorg.vr@live.be>
2022-01-07 10:10:46 +0100

    Add external url to machine annotation

--- app/assets/javascripts/code_listing/machine_annotation.ts
+++ app/assets/javascripts/code_listing/machine_annotation.ts
@@ -4,9 +4,12 @@ export interface MachineAnnotationData {
     type: AnnotationType;
     text: string;
     row: number;
+    externalUrl: string | null;
 }
 
 export class MachineAnnotation extends Annotation {
+    private readonly externalUrl: string | null;
+
     constructor(data: MachineAnnotationData) {
         // Filter out lines only containing dashes.
         let text = data.text.split("\n")
@@ -17,6 +20,7 @@ export class MachineAnnotation extends Annotation {
         node.textContent = text;
         text = node.innerHTML;
         super(data.row + 1, text, data.type);
+        this.externalUrl = data.externalUrl;
     }
 
     protected get class(): string {
@@ -29,5 +33,17 @@ export class MachineAnnotation extends Annotation {
 
     protected get title(): string {
         return I18n.t(`js.annotation.type.${this.type}`);
+    }
+
+    protected get hasNotice(): boolean {
+        return this.externalUrl !== null && this.externalUrl !== undefined;
+    }
+
+    protected get noticeUrl(): string | null {
+        return this.externalUrl;
+    }
+
+    protected get noticeInfo(): string | null {
+        return I18n.t("js.machine_annotation.external_url");
     }
 }

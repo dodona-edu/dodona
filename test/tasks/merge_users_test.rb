3581223f0a410544e2e0c8db780b78c5101fde4f
jorg-vr <jorg.vr@live.be>
2022-01-27 14:50:07 +0100

    Add unique user ids to reduce overlap when testing

--- test/tasks/merge_users_test.rb
+++ test/tasks/merge_users_test.rb
@@ -26,8 +26,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should cancel on no' do
-    u1 = create :user
-    u2 = create :user
+    u1 = create :user, id: 1_000_001
+    u2 = create :user, id: 1_000_002
 
     stub_stdin('n')
     merge_users_interactive u1.id, u2.id
@@ -37,8 +37,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should cancel on enter' do
-    u1 = create :user
-    u2 = create :user
+    u1 = create :user, id: 1_000_003
+    u2 = create :user, id: 1_000_004
 
     stub_stdin('\n')
     merge_users_interactive u1.id, u2.id
@@ -48,8 +48,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should merge on yes' do
-    u1 = create :user
-    u2 = create :user
+    u1 = create :user, id: 1_000_005
+    u2 = create :user, id: 1_000_006
 
     stub_stdin('y')
     merge_users_interactive u1.id, u2.id
@@ -59,8 +59,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should swap on swap' do
-    u1 = create :user
-    u2 = create :user
+    u1 = create :user, id: 1_000_007
+    u2 = create :user, id: 1_000_008
 
     stub_stdin('s', 'y')
     merge_users_interactive u1.id, u2.id
@@ -70,8 +70,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should ignore incorrect input' do
-    u1 = create :user
-    u2 = create :user
+    u1 = create :user, id: 1_000_009
+    u2 = create :user, id: 1_000_010
 
     stub_stdin('a', 'z', 'd', 'y')
     merge_users_interactive u1.id, u2.id
@@ -81,8 +81,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should fail on yes with different permissions' do
-    u1 = create :user, permission: 'student'
-    u2 = create :user, permission: 'zeus'
+    u1 = create :user, permission: 'student', id: 1_000_011
+    u2 = create :user, permission: 'zeus', id: 1_000_012
 
     stub_stdin('y')
     merge_users_interactive u1.id, u2.id
@@ -92,8 +92,8 @@ class MergeUserTest < ActiveSupport::TestCase
   end
 
   test 'The script should merge on force  with different permissions' do
-    u1 = create :user, permission: 'student'
-    u2 = create :user, permission: 'zeus'
+    u1 = create :user, permission: 'student', id: 1_000_013
+    u2 = create :user, permission: 'zeus', id: 1_000_014
 
     stub_stdin('f')
     merge_users_interactive u1.id, u2.id

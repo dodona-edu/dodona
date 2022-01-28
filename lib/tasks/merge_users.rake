d02e9829ad1ea1a76b23ca6ea15bb4177eb2a7b5
jorg-vr <jorg.vr@live.be>
2022-01-28 09:49:54 +0100

    Fix io stubbing in tests

--- lib/tasks/merge_users.rake
+++ lib/tasks/merge_users.rake
@@ -1,18 +1,30 @@
 
 task :merge_users, [:arg1, :arg2] => :environment do |task, args|
+  u1_id = args[:arg1].to_i
+  u2_id = args[:arg2].to_i
+
+  MergeUsers.new.merge_users_interactive u1_id, u2_id
+end
+
+class MergeUsers
   LABEL_WIDTH = 20
   USER_WIDTH = 50
 
-  def print_users(*users)
-    puts 'Username: '.ljust(LABEL_WIDTH) + users.map { |u| u.username.to_s.ljust(USER_WIDTH) }.join
-    puts 'Email: '.ljust(LABEL_WIDTH) + users.map { |u| u.email.to_s.ljust(USER_WIDTH) }.join
-    puts 'Name: '.ljust(LABEL_WIDTH) + users.map { |u| u.full_name.to_s.ljust(USER_WIDTH) }.join
-    puts 'Permission: '.ljust(LABEL_WIDTH) + users.map { |u| u.permission.to_s.ljust(USER_WIDTH) }.join
+  def initialize(input = $stdin, output = $stdout)
+    @input = input
+    @output = output
+  end
 
-    puts 'Courses: '.ljust(LABEL_WIDTH) + users.map { |u| u.courses.count.to_s.ljust(USER_WIDTH) }.join
-    puts 'Submissions: '.ljust(LABEL_WIDTH) + users.map { |u| u.submissions.count.to_s.ljust(USER_WIDTH) }.join
-    puts 'Read states: '.ljust(LABEL_WIDTH) + users.map { |u| u.activity_read_states.count.to_s.ljust(USER_WIDTH) }.join
-    puts 'Repositories: '.ljust(LABEL_WIDTH) + users.map { |u| u.repositories.count.to_s.ljust(USER_WIDTH) }.join
+  def print_users(*users)
+    @output.puts 'Username: '.ljust(LABEL_WIDTH) + users.map { |u| u.username.to_s.ljust(USER_WIDTH) }.join
+    @output.puts 'Email: '.ljust(LABEL_WIDTH) + users.map { |u| u.email.to_s.ljust(USER_WIDTH) }.join
+    @output.puts 'Name: '.ljust(LABEL_WIDTH) + users.map { |u| u.full_name.to_s.ljust(USER_WIDTH) }.join
+    @output.puts 'Permission: '.ljust(LABEL_WIDTH) + users.map { |u| u.permission.to_s.ljust(USER_WIDTH) }.join
+
+    @output.puts 'Courses: '.ljust(LABEL_WIDTH) + users.map { |u| u.courses.count.to_s.ljust(USER_WIDTH) }.join
+    @output.puts 'Submissions: '.ljust(LABEL_WIDTH) + users.map { |u| u.submissions.count.to_s.ljust(USER_WIDTH) }.join
+    @output.puts 'Read states: '.ljust(LABEL_WIDTH) + users.map { |u| u.activity_read_states.count.to_s.ljust(USER_WIDTH) }.join
+    @output.puts 'Repositories: '.ljust(LABEL_WIDTH) + users.map { |u| u.repositories.count.to_s.ljust(USER_WIDTH) }.join
   end
 
   def merge_users_interactive(u1_id, u2_id)
@@ -21,35 +33,30 @@ task :merge_users, [:arg1, :arg2] => :environment do |task, args|
 
     print_users u1, u2
 
-    puts ''
+    @output.puts ''
 
     c = ''
     until %W[\r \n y n f].include? c
       u1, u2 = u2, u1 if c == 's'
-      puts "Invalid input #{c}" unless ['s', ''].include?(c)
-      puts "Are you sure you want to merge #{u1.username} into #{u2.username}? (y)es|(N)o|(f)orce|(s)wap"
-      c = $stdin.getch.downcase
+      @output.puts "Invalid input #{c}" unless ['s', ''].include?(c)
+      @output.puts "Are you sure you want to merge #{u1.username} into #{u2.username}? (y)es|(N)o|(f)orce|(s)wap"
+      c = @input.getch.downcase
     end
 
-    puts ''
+    @output.puts ''
 
     if %W[\r \n n].include? c
-      puts 'Merge cancelled'
+      @output.puts 'Merge cancelled'
       return
     end
 
     success = u1.merge_into(u2, force: c == 'f')
     if success
-      puts "Successfully merged #{u1.username} into #{u2.username}"
+      @output.puts "Successfully merged #{u1.username} into #{u2.username}"
       print_users u2
     else
-      puts 'Merge failed'
-      puts u1.errors.full_messages.join('\n')
+      @output.puts 'Merge failed'
+      @output.puts u1.errors.full_messages.join('\n')
     end
   end
-
-  u1_id = args[:arg1].to_i
-  u2_id = args[:arg2].to_i
-
-  merge_users_interactive u1_id, u2_id
 end
--- test/tasks/merge_users_test.rb
+++ test/tasks/merge_users_test.rb
@@ -1,102 +1,87 @@
 require 'test_helper'
-require 'rake'
 require 'stringio'
+require 'rake'
+load './lib/tasks/merge_users.rake'
 
 class MergeUserTest < ActiveSupport::TestCase
   def stub_stdin(*chars)
     string_io = StringIO.new
-    string_io.puts chars.join
+    chars.each { |c| string_io.print c }
     string_io.rewind
-    $stdin = string_io
+    string_io
   end
 
-  def merge_users_interactive(u1_id, u2_id)
-    Rake.application.invoke_task "merge_users[#{u1_id}, #{u2_id}]"
-  end
-
-  setup do
-    Rake.application.rake_require 'tasks/merge_users'
-    Rake::Task.define_task(:environment)
-    $stdout = File.open(File::NULL, 'w')
-  end
-
-  teardown do
-    $stdin = STDIN
-    $stdout = STDOUT
+  def merge_users_interactive(u1_id, u2_id, *chars)
+    input = stub_stdin(*chars)
+    output = File.open(File::NULL, 'w')
+    MergeUsers.new(input, output).merge_users_interactive(u1_id, u2_id)
   end
 
   test 'The script should cancel on no' do
-    u1 = create :user, id: 1_000_001
-    u2 = create :user, id: 1_000_002
+    u1 = create :user
+    u2 = create :user
 
-    stub_stdin('n')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, 'n'
 
     assert User.exists?(u1.id)
     assert User.exists?(u2.id)
   end
 
   test 'The script should cancel on enter' do
-    u1 = create :user, id: 1_000_003
-    u2 = create :user, id: 1_000_004
+    u1 = create :user
+    u2 = create :user
 
-    stub_stdin('\n')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, '\n'
 
     assert User.exists?(u1.id)
     assert User.exists?(u2.id)
   end
 
   test 'The script should merge on yes' do
-    u1 = create :user, id: 1_000_005
-    u2 = create :user, id: 1_000_006
+    u1 = create :user
+    u2 = create :user
 
-    stub_stdin('y')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, 'y'
 
     assert_not User.exists?(u1.id)
     assert User.exists?(u2.id)
   end
 
   test 'The script should swap on swap' do
-    u1 = create :user, id: 1_000_007
-    u2 = create :user, id: 1_000_008
+    u1 = create :user
+    u2 = create :user
 
-    stub_stdin('s', 'y')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, 's', 'y'
 
     assert User.exists?(u1.id)
     assert_not User.exists?(u2.id)
   end
 
   test 'The script should ignore incorrect input' do
-    u1 = create :user, id: 1_000_009
-    u2 = create :user, id: 1_000_010
+    u1 = create :user
+    u2 = create :user
 
-    stub_stdin('a', 'z', 'd', 'y')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, 'a', 'z', 'd', 'y'
 
     assert_not User.exists?(u1.id)
     assert User.exists?(u2.id)
   end
 
   test 'The script should fail on yes with different permissions' do
-    u1 = create :user, permission: 'student', id: 1_000_011
-    u2 = create :user, permission: 'zeus', id: 1_000_012
+    u1 = create :user, permission: 'student'
+    u2 = create :user, permission: 'zeus'
 
-    stub_stdin('y')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, 'y'
 
     assert User.exists?(u1.id)
     assert User.exists?(u2.id)
   end
 
   test 'The script should merge on force  with different permissions' do
-    u1 = create :user, permission: 'student', id: 1_000_013
-    u2 = create :user, permission: 'zeus', id: 1_000_014
+    u1 = create :user, permission: 'student'
+    u2 = create :user, permission: 'zeus'
 
-    stub_stdin('f')
-    merge_users_interactive u1.id, u2.id
+    merge_users_interactive u1.id, u2.id, 'f'
 
     assert_not User.exists?(u1.id)
     assert User.exists?(u2.id)

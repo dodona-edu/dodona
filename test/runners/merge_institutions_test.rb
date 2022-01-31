45e108f2fd9ffcd7e3cb1e10d8289e24339c2ef5
jorg-vr <jorg.vr@live.be>
2022-01-31 15:16:11 +0100

    Add extra tests to check rolleback and swap

--- test/runners/merge_institutions_test.rb
+++ test/runners/merge_institutions_test.rb
@@ -145,4 +145,48 @@ class MergeInstitutionsTest < ActiveSupport::TestCase
       assert_equal 0, u.errors.count
     end
   end
+
+  test 'The script should rollback all changes if one user merge has failed' do
+    i1 = create :institution
+    i2 = create :institution
+    overlapping_users_i1 = (1..5).map { |i| create :user, username: "test#{i}", institution: i1, permission: 'student' }
+    overlapping_users_i2 = (1..5).map { |i| create :user, username: "test#{i}", institution: i2, permission: i == 4 ? 'staff' : 'student' }
+    unique_users_i1 = (1..5).map { |i| create :user, username: "foo#{i}", institution: i1 }
+    unique_users_i2 = (1..5).map { |i| create :user, username: "bar#{i}", institution: i2 }
+
+    merge_institutions_interactive i1.id, i2.id, 'y', 'y', 'y', 'y', 'y', 'y', 'y'
+
+    assert Institution.exists?(i1.id)
+    assert Institution.exists?(i2.id)
+    (overlapping_users_i1 + unique_users_i1).each do |u|
+      assert User.exists?(u.id)
+      u.reload
+      assert_equal i1, u.institution
+      u.max_one_institution
+      assert_equal 0, u.errors.count
+    end
+    (overlapping_users_i2 + unique_users_i2).each do |u|
+      assert User.exists?(u.id)
+      u.reload
+      assert_equal i2, u.institution
+      u.max_one_institution
+      assert_equal 0, u.errors.count
+    end
+  end
+
+  test 'Swapped users are still placed in the correct institution' do
+    i1 = create :institution
+    i2 = create :institution
+    u1 = create :user, username: 'test', institution: i1
+    u2 = create :user, username: 'test', institution: i2
+
+    merge_institutions_interactive i1.id, i2.id, 'y', 'y', 's', 'y'
+
+    assert_not Institution.exists?(i1.id)
+    assert Institution.exists?(i2.id)
+    assert User.exists?(u1.id)
+    assert_not User.exists?(u2.id)
+    u1.reload
+    assert_equal i2, u1.institution
+  end
 end

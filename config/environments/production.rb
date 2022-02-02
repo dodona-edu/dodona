28da15735ea5c594194798f5d661c9e114f45ef5
jorg-vr <jorg.vr@live.be>
2022-02-02 16:33:40 +0100

    Do compression using dart sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,6 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.js_compressor = Uglifier.new(harmony: true) if defined? Uglifier
-  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -69,7 +69,6 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.js_compressor = Uglifier.new(harmony: true) if defined? Uglifier
-  config.assets.css_compressor = :sass
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.

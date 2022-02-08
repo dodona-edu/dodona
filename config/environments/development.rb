054ae8c1c20fc85be189252680106226e3e606da
jorg-vr <jorg.vr@live.be>
2022-02-08 16:32:56 +0100

    Clean up comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,9 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  # config.webpacker.check_yarn_integrity = true
-
   # Application hosts
 
   config.hosts << ENV['RAILS_APPLICATION_HOST'] if ENV['RAILS_APPLICATION_HOST'].present?

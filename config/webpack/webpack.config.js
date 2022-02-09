2e96e12b722c0a6b9b04f336f629409f1cb89cf0
jorg-vr <jorg.vr@live.be>
2022-02-09 14:04:31 +0100

    Use source maps in development

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -2,14 +2,6 @@ const path = require("path");
 const glob = require("glob");
 const webpack = require("webpack");
 
-// https://stackoverflow.com/questions/34907999/best-way-to-have-all-files-in-a-directory-be-entry-points-in-webpack
-const entry = glob.sync("./app/javascript/packs/**.js").reduce(function (obj, el) {
-    obj[path.parse(el).name] = el;
-    return obj;
-}, {});
-
-// entry.assets_application = "./app/assets/javascripts/assets_application.js";
-
 const config = {
     mode: "production",
     module: {
@@ -24,7 +16,11 @@ const config = {
     optimization: {
         moduleIds: "deterministic",
     },
-    entry,
+    // https://stackoverflow.com/questions/34907999/best-way-to-have-all-files-in-a-directory-be-entry-points-in-webpack
+    entry: glob.sync("./app/javascript/packs/**.js").reduce(function (obj, el) {
+        obj[path.parse(el).name] = el;
+        return obj;
+    }, {}),
     output: {
         filename: "[name].js",
         sourceMapFilename: "[name].js.map",
@@ -43,33 +39,9 @@ const config = {
 
 if (process.env.NODE_ENV === "development") {
     config.mode = "development";
-    config.devServer = {
-        https: false,
-        host: "localhost",
-        port: 3035,
-        public: "localhost:3035",
-        hmr: false,
-        // Inline should be set to true if using HMR
-        inline: true,
-        overlay: true,
-        compress: true,
-        disable_host_check: true,
-        use_local_ip: false,
-        quiet: false,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
-        watch_options: {
-            ignored: "**/node_modules/**"
-        }
-    };
+    config.devtool = "inline-source-map";
 }
 
-if (process.env.NODE_ENV === "test") {
-    config.mode = "test";
-    config.output.path = path.resolve(__dirname, "..", "..", "app/assets/builds-test");
-}
-
-// Staging and Production use default config
+// Test, Staging and Production use default config
 
 module.exports = config;

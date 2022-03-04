2ef164c10a9609ad847bc5c2da1a82c4e5f501a4
jorg-vr <jorg.vr@live.be>
2022-03-04 14:49:47 +0100

    Add extra endpoints to options

--- lib/OIDC/auth/surf_setup.rb
+++ lib/OIDC/auth/surf_setup.rb
@@ -11,6 +11,8 @@ module OIDC
               host: 'connect.test.surfconext.nl',
               authorization_endpoint: '/oidc/authorize',
               token_endpoint: '/oidc/token',
+              userinfo_endpoint: '/oidc/userinfo',
+              jwks_uri: '/oidc/certs',
               identifier: Rails.application.credentials.surf_client_id || 'dodona.localhost',
               secret: Rails.application.credentials.surf_client_secret,
               redirect_uri: "https://naos.ugent.be/users/auth/surf/callback"

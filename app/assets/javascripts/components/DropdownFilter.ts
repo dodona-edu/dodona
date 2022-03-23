4995906ce9a9562fb151f26de92baed51a613004
jorg-vr <jorg.vr@live.be>
2022-03-23 14:06:25 +0100

    Give selecting an effect

--- app/assets/javascripts/components/DropdownFilter.ts
+++ app/assets/javascripts/components/DropdownFilter.ts
@@ -4,19 +4,17 @@ import { customElement, property } from "lit/decorators.js";
 @customElement("dodona-dropdown-filter")
 export class DropdownFilter extends LitElement {
     // @property()
-    // param: string;
-    // @property()
     // paramVal: (s: {id: number, name: string}) => unknown;
     @property({ type: Boolean })
-    multi: boolean;
+        multi: boolean;
     @property( { type: Array } )
-    labels: Array<{id: number, name: string}>=[];
+        labels: Array<{id: string | number, name: string}>=[];
     @property()
-    color: string;
+        color: string;
     @property( { type: Array } )
-    selected: [number];
-    @property({ attribute: "button-label" })
-    buttonLabel: string;
+        selected: [string | number];
+    @property()
+        type: string;
 
 
     isAnySelected(): boolean {
@@ -28,21 +26,27 @@ export class DropdownFilter extends LitElement {
         return this;
     }
 
+    selectLabel(name): void {
+        if (dodona.addTokenToSearch) {
+            dodona.addTokenToSearch(this.type, name);
+        }
+    }
+
     render(): TemplateResult {
         console.log(this);
         return html`
             <div class="dropdown dropdown-filter">
                 <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                    ${this.buttonLabel}
+                    ${I18n.t(`js.${this.type}`)}
                     ${this.isAnySelected() ? html`<i class="mdi mdi-circle mdi-12 mdi-colored-accent accent-${this.color}"></i>`: ""}
                     <i class="mdi mdi-chevron-down mdi-12"></i>
                 </a>
 
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                     ${this.labels.map(s => html`
-                        <li><a class="dropdown-item ${this.selected.includes(s.id) ? "active" : ""}" href="#">
+                        <li><a class="dropdown-item ${this.selected.includes(s.id) ? "active" : ""}" href="#" @click="${() => this.selectLabel(s.name)}">
                             ${this.multi ?
-                                html`
+        html`
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
                                         <label class="form-check-label" for="flexCheckDefault">
@@ -50,10 +54,10 @@ export class DropdownFilter extends LitElement {
                                         </label>
                                     </div>
                                 ` :
-                                html`
+        html`
                                     ${ s.name }  <span class="badge rounded-pill bg-info float-end">130</span>
                                 `
-                            }
+}
                         </a></li>
                     `)}
                 </ul>

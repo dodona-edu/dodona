9d33396977e3bdae00c89d726abe7deda1794156
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-04-24 13:22:10 +0200

    Run both tests with new timeout

--- test/system/scratchpad_test.rb
+++ test/system/scratchpad_test.rb
@@ -19,19 +19,37 @@ class ScratchpadTest < ApplicationSystemTestCase
     sign_in @zeus
   end
 
-  test 'Scratchpad can correctly run code' do
+  test 'Scratchpad can run Hello World!' do
     visit(course_activity_path(course_id: @course.id, id: @exercise.id))
     assert_selector '#scratchpad-offcanvas-show-btn'
     find('#scratchpad-offcanvas-show-btn').click
 
     assert_selector '.cm-editor'
-    find('.cm-editor').click
     # Focus editor
+    find('.cm-editor').click
     find('.cm-content').send_keys 'print("Hello World!")'
-
     find('#__papyros-run-code-btn').click
 
     output_area = find('#scratchpad-output-wrapper')
     output_area.find('span', text: 'Hello World!')
+  end
+
+  test 'Scratchpad can process user input' do
+    visit(course_activity_path(course_id: @course.id, id: @exercise.id))
+    assert_selector '#scratchpad-offcanvas-show-btn'
+    find('#scratchpad-offcanvas-show-btn').click
+
+    assert_selector '.cm-editor'
+    # Focus editor
+    find('.cm-editor').click
+    find('.cm-content').send_keys 'print(input())'
+    find('#__papyros-run-code-btn').click
+
+    scratchpad_input = 'Echo'
+    find_field('__papyros-code-input-area', disabled: false).send_keys scratchpad_input
+    find_button('__papyros-send-input-btn', disabled: false).click
+
+    output_area = find('#scratchpad-output-wrapper')
+    output_area.find('span', text: scratchpad_input)
   end
 end

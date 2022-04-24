48b4822681209380734b3367cab1acc8c555823f
winniederidder <Winnie.De.Ridder@hotmail.com>
2022-04-24 20:03:39 +0200

    Combine tests into one

--- test/system/scratchpad_test.rb
+++ test/system/scratchpad_test.rb
@@ -34,11 +34,46 @@ class ScratchpadTest < ApplicationSystemTestCase
   end
 
   test 'Scratchpad can run code' do
-    it 'Can run Hello World!' do
-      run_code 'print("Hello World!")'
+    ## Hello World!
+    code = 'print("Hello World!")'
+    run_code code
+    # First run, so wait longer
+    output_area = find('#scratchpad-output-wrapper', wait: 20)
+    output_area.find('span', text: 'Hello World!')
 
-      output_area = find('#scratchpad-output-wrapper', wait: 20)
-      output_area.find('span', text: 'Hello World!')
-    end
+    # Scratchpad can process user input in interactive mode
+    scratchpad_input = 'Echo'
+    code = 'print(input())'
+    run_code code
+    # Enter the input during the run
+    find_field('__papyros-code-input-area', disabled: false).send_keys scratchpad_input
+    find_button('__papyros-send-input-btn', disabled: false).click
+
+    output_area.find('span', text: scratchpad_input)
+
+    # Scratchpad can process user input in batch mode
+    scratchpad_input = 'Batch'
+    # Set the input before the run
+    find('#__papyros-switch-input-mode').click
+    # input area should be re-rendered
+    find_field('__papyros-code-input-area').send_keys scratchpad_input
+    # Ensure the text was entered
+    find_field('__papyros-code-input-area', with: scratchpad_input)
+    run_code ''
+
+    output_area.find('span', text: scratchpad_input)
+
+    # Scratchpad can sleep and be interrupted
+    code = "import time\nprint(\"Start\")\ntime.sleep(3)\nprint(\"Stop\")"
+    run_code code
+
+    output_area.find('span', text: 'Start')
+    output_area.find('span', text: 'Stop')
+    run_code ''
+    sleep(1)
+    find_button('__papyros-stop-btn', disabled: false).click
+
+    output_area.find('span', text: 'Start')
+    assert output_area.has_no_xpath?('.//span', text: 'Stop')
   end
 end

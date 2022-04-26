d4e1aedd3043ef1dc5e6631393ed6d645497c3c7
jorg-vr <jorg.vr@live.be>
2022-04-26 13:44:55 +0200

    Move all search components to search pack

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,3 +1,9 @@
 import { searchQuery } from "search";
 
+import "components/sort_button.ts";
+import "components/search_actions.ts";
+import "components/search_field.ts";
+import "components/search_token.ts";
+import "components/filter_button.ts";
+
 window.dodona.searchQuery = searchQuery;

cec4ef52e797fcfd6f1051e92c7866a21310cdbe
jorg-vr <jorg.vr@live.be>
2022-04-27 15:23:22 +0200

    Add commments to elements

--- app/assets/javascripts/components/shadowless_lit_element.ts
+++ app/assets/javascripts/components/shadowless_lit_element.ts
@@ -1,5 +1,16 @@
 import { LitElement } from "lit";
 
+/**
+ * This class removes the shadow dom functionality from lit elements
+ * Shadow dom allows for:
+ *   - DOM scoping
+ *   - Style Scoping
+ *   - Composition
+ * more info here: https://lit.dev/docs/components/shadow-dom/
+ *
+ * This class is often used to avoid style scoping. (To be able to use the style as defined in our general css)
+ * When shadow dom is required just use a normal LitElement
+ */
 export class ShadowlessLitElement extends LitElement {
     // don't use shadow dom
     createRenderRoot(): Element {

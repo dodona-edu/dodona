f16b4e94a7ea7ec3d9b3003b91fcfaf1e34db05d
Bart Mesuere <Bart.Mesuere@UGent.be>
2022-05-22 10:33:16 +0200

    add jsdocs

--- app/assets/javascripts/components/histogram.ts
+++ app/assets/javascripts/components/histogram.ts
@@ -1,6 +1,17 @@
 import { css, html, LitElement, TemplateResult } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+/**
+ * This component displays a simple histogram using a flex layout and styled divs.
+ * It always takes up 100% of the space it's given.
+ *
+ * @element dodona-histogram
+ *
+ * @prop {number[]} values - Pass the histogram data as an array of numbers.
+ *
+ * @cssprop [--d-histogram-baseline=lightgray] - The color of the bottom border serving as an axis.
+ * @cssprop [--d-histogram-bar=steelblue] - The color of the bars.
+ */
 @customElement("dodona-histogram")
 export class Histogram extends LitElement {
     @property({ type: Array })
@@ -12,21 +23,27 @@ export class Histogram extends LitElement {
             align-items: flex-end;
             height: 100%;
             width: 100%;
-            border-bottom: 1px solid var(--dod-histo-axis, lightgray);
+            border-bottom: 1px solid var(--d-histogram-baseline, lightgray);
         }
         .bar {
             display: inline-block;
             width: 100%;
-            background: var(--dod-histo-bar, steelblue);
+            background: var(--d-histogram-bar, steelblue);
         }
     `;
 
-    getHeight(value: number): number {
+    /**
+     * Calculates the height of a bar, proportionate to the max value of the dataset.
+     *
+     * @param {number} value - The value associated with the current bar.
+     * @return {number} The percentage value of the height of the bar.
+     */
+    private getHeight(value: number): number {
         const max = Math.max(...this.values);
         return 100 * value / max;
     }
 
-    renderRow(value: number, index: number): TemplateResult {
+    private renderBar(value: number, index: number): TemplateResult {
         return html`<div
             class="bar"
             style="height: ${this.getHeight(value)}%"
@@ -35,6 +52,6 @@ export class Histogram extends LitElement {
     }
 
     render(): TemplateResult[] {
-        return this.values.map((v, i) => this.renderRow(v, i));
+        return this.values.map((v, i) => this.renderBar(v, i));
     }
 }

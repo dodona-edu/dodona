53b1e9cac6f5e25df1055cabfed9341b8a180d73
jorg-vr <jorg.vr@live.be>
2022-06-01 14:49:50 +0200

    Rename d-search-tokens and improve docs

--- app/assets/javascripts/components/filter_button.ts
+++ app/assets/javascripts/components/filter_button.ts
@@ -13,6 +13,8 @@ import { ShadowlessLitElement } from "components/shadowless_lit_element";
  * @prop {string} param - the searchQuery param to be used
  * @prop {string} value - the value to be used in the searchQuery
  * @prop {boolean} multi - should be specified in case of array query params
+ *
+ * @slot - this contains the clickable button content
  */
 @customElement("d-filter-button")
 export class FilterButton extends LitElement {

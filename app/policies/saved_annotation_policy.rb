12ce912d9046a8a74bd3af546e8880763bb79135
jorg-vr <jorg.vr@live.be>
2022-07-01 15:24:29 +0200

    Add hardcoded closed beta courses

--- app/policies/saved_annotation_policy.rb
+++ app/policies/saved_annotation_policy.rb
@@ -1,6 +1,6 @@
 class SavedAnnotationPolicy < ApplicationPolicy
   # REMOVE AFTER CLOSED BETA
-  BETA_COURSES = [5].freeze
+  BETA_COURSES = [5, 773, 1151].freeze
 
   class Scope < ApplicationPolicy::Scope
     def resolve

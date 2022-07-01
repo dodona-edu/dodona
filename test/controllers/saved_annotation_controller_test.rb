71fea8b75b431e45143077ec89156574229db521
jorg-vr <jorg.vr@live.be>
2022-07-01 15:01:14 +0200

    Fix tests with closed beta limitation

--- test/controllers/saved_annotation_controller_test.rb
+++ test/controllers/saved_annotation_controller_test.rb
@@ -6,25 +6,23 @@ class SavedAnnotationControllerTest < ActionDispatch::IntegrationTest
   crud_helpers SavedAnnotation, attrs: %i[title annotation_text], format: :json
 
   def setup
+    @course = create :course, id: 5 # COURSE ID CAN BE REMOVED AFTER CLOSED BETA
     @user = users(:staff)
-    @instance = create :saved_annotation, user: @user
+    CourseMembership.create(course: @course, user: @user, status: :course_admin)
+    @instance = create :saved_annotation, user: @user, course: @course
     sign_in @user
   end
 
   test_crud_actions only: %i[show destroy index update], except: %i[update_redirect destroy_redirect]
 
   test 'should be able to create from existing annotation' do
-    course = create(:course)
-    CourseMembership.create(course: course, user: @user, status: :course_admin)
-    annotation = create :annotation, submission:  create(:submission, course: course), user: @user
+    annotation = create :annotation, submission:  create(:submission, course: @course), user: @user
     post saved_annotations_url, params: { format: :json, saved_annotation: { title: 'test', annotation_text: annotation.annotation_text }, from: annotation.id }
     assert_response :success
   end
 
   test 'creating a saved annotation should fail when one with the same name already exists' do
-    course = create(:course)
-    CourseMembership.create(course: course, user: @user, status: :course_admin)
-    annotation = create :annotation, submission:  create(:submission, course: course), user: @user
+    annotation = create :annotation, submission:  create(:submission, course: @course), user: @user
     post saved_annotations_url, params: { format: :json, saved_annotation: { title: 'test', annotation_text: annotation.annotation_text }, from: annotation.id }
     assert_response :success
     post saved_annotations_url, params: { format: :json, saved_annotation: { title: 'test', annotation_text: annotation.annotation_text }, from: annotation.id }

994f2cd6d62adfbaa3d3d5d69b02dece9cc51728
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-08-03 10:01:21 +0200

    fix: default empty page value to page 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,7 +81,7 @@ class ApplicationController < ActionController::Base
 
   def parse_pagination_param(page)
     # This doesn't work for negative numbers, but we don't need to handle negative numbers here anyway
-    page.to_s.match(/^\d+$/) ? [page.to_i, 1].max : nil
+    page.to_s.match(/^\d*$/) ? [page.to_i, 1].max : nil
   end
 
   def skip_session

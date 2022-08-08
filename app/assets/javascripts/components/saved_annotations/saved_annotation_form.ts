f8329a652c6da39370cdc9258e1d7ac9e5cba145
jorg-vr <jorg.vr@live.be>
2022-08-08 15:22:35 +0200

    Make form use full width

--- app/assets/javascripts/components/saved_annotations/saved_annotation_form.ts
+++ app/assets/javascripts/components/saved_annotations/saved_annotation_form.ts
@@ -42,24 +42,20 @@ export class SavedAnnotationForm extends ShadowlessLitElement {
     render(): TemplateResult {
         return html`
             <form class="form">
-                <div class="field form-group row">
-                    <label class="col-sm-4 col-form-label">
+                <div class="field form-group">
+                    <label class="form-label">
                         ${I18n.t("js.saved_annotation.title")}
                     </label>
-                    <div class="col-sm-8">
-                        <input required="required" class="form-control" type="text"
-                               .value=${this.savedAnnotation.title} @change=${e => this.updateTitle(e)}>
-                    </div>
+                    <input required="required" class="form-control" type="text"
+                           .value=${this.savedAnnotation.title} @change=${e => this.updateTitle(e)}>
                 </div>
-                <div class="field form-group row">
-                    <label class="col-sm-4 col-form-label">
+                <div class="field form-group">
+                    <label class="form-label">
                         ${I18n.t("js.saved_annotation.annotation_text")}
                     </label>
-                    <div class="col-sm-8">
-                        <textarea required="required" class="form-control" rows="4"
-                                  .value=${this.savedAnnotation.annotation_text} @change=${e => this.updateText(e)}></textarea>
-                    </div>
-                    <span class="help-block offset-sm-4 col-sm-8">
+                    <textarea required="required" class="form-control" rows="4"
+                              .value=${this.savedAnnotation.annotation_text} @change=${e => this.updateText(e)}></textarea>
+                    <span class="help-block">
                         ${unsafeHTML(I18n.t("js.saved_annotation.form.markdown_html"))}
                     </span>
                 </div>

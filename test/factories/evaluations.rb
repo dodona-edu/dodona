b273091c818d8d1d60590c540496a47b5eeb8bd6
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-08-12 10:37:29 +0200

    chore: add back code that got removed

--- test/factories/evaluations.rb
+++ test/factories/evaluations.rb
@@ -27,6 +27,20 @@ FactoryBot.define do
       released { true }
     end
 
+    trait :with_submissions do
+      series do
+        s = create :series, exercise_count: 2, deadline: DateTime.now
+        users = create_list :user, user_count
+        users.each do |u|
+          s.course.enrolled_members << u
+          s.exercises.each do |e|
+            create :correct_submission, user: u, exercise: e, course: s.course, created_at: s.deadline - 1.hour
+          end
+        end
+        s
+      end
+    end
+
     trait :is_anonymous do
       anonymous { true }
     end

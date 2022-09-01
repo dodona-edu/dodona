a1dd483f9b1b223b21b292cc10b16ba50d6c04ab
jorg-vr <jorg.vr@live.be>
2022-09-01 12:03:03 +0200

    Update identitities for correct provider in migration

--- db/migrate/20220830124315_add_identifier_based_on_username_to_identity.rb
+++ db/migrate/20220830124315_add_identifier_based_on_username_to_identity.rb
@@ -1,6 +1,6 @@
 class AddIdentifierBasedOnUsernameToIdentity < ActiveRecord::Migration[7.0]
   def change
     add_column :identities, :identifier_based_on_username, :boolean, null: false, default: false
-    Identity.joins(:provider).where(provider: {type: "Provider::Office365"}).update_all(identifier_based_on_username: true)
+    Identity.joins(:provider).where(provider: {type: "Provider::Smartschool"}).update_all(identifier_based_on_username: true)
   end
 end

a88528099fe2edaf18cef2f3f135761e9c45c8b5
jorg-vr <jorg.vr@live.be>
2022-09-15 17:09:47 +0200

    Update app/assets/javascripts/components/pagination.ts
    Co-authored-by: Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>

--- app/assets/javascripts/components/pagination.ts
+++ app/assets/javascripts/components/pagination.ts
@@ -59,7 +59,7 @@ export class Pagination extends ShadowlessLitElement {
                     ${!this.small ? this.pageButton(this.current - 1, "←") : ""}
                     ${this.pageButton(1)}
                     ${this.total > 3 + 2 * this.width && this.current == 3 + this.width ? this.pageButton(2) : ""}
-                    ${this.total > 3 + 2 * this.width && this.current > 3 + this.width ? this.pageButton(undefined, "...") : ""}
+                    ${this.total > 3 + 2 * this.width && this.current > 3 + this.width ? this.pageButton(undefined, "…") : ""}
                     ${this.range.map(i => this.pageButton(i))}
                     ${this.total > 3 + 2 * this.width && this.total - this.current > 2 + this.width ? this.pageButton(undefined, "…") : ""}
                     ${this.total > 3 + 2 * this.width && this.total - this.current == 2 + this.width ? this.pageButton(this.total - 1) : ""}

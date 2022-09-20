58889732ae0cc37e009e9bf0eb195fb54f7ab2c6
jorg-vr <jorg.vr@live.be>
2022-09-20 14:11:10 +0200

    Remove old code

--- app/javascript/packs/sign_in.js
+++ app/javascript/packs/sign_in.js
@@ -1,3 +1,1 @@
-import { initInstitutionAutoSelect } from "../../assets/javascripts/sign_in.js";
 import "components/sign_in_search_bar";
-window.dodona.initInstitutionAutoSelect = initInstitutionAutoSelect;

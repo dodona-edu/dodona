26bb24a6c154f0c64f3a9b3400518427c4ba6a70
jorg-vr <jorg.vr@live.be>
2022-09-20 13:32:18 +0200

    Fix layout

--- app/assets/javascripts/components/sign_in_search_bar.ts
+++ app/assets/javascripts/components/sign_in_search_bar.ts
@@ -2,6 +2,8 @@ import { customElement, property } from "lit/decorators.js";
 import { html, TemplateResult } from "lit";
 import { ShadowlessLitElement } from "components/shadowless_lit_element";
 import { Option } from "components/datalist_input";
+import { ready } from "util.js";
+import "components/datalist_input";
 
 /**
  * @element d-sign-in-search-bar
@@ -27,22 +29,30 @@ export class SignInSearchBar extends ShadowlessLitElement {
         return this.institutions.map(i => ({ label: i.name, value: i.provider, extra: this.providers[i.provider].name }));
     }
 
+    constructor() {
+        super();
+        ready.then(() => this.requestUpdate());
+    }
+
     handleInput(e: CustomEvent): void {
         this.selected_provider = e.detail.value;
     }
 
     render(): TemplateResult {
         return html`
-            <d-datalist-input
-                .options=${this.options}
-                @input=${e => this.handleInput(e)}
-                placeholder="${I18n.t("js.sign_in_search_bar.institution_search")}"
-            ></d-datalist-input>
-            <a class="btn btn-primary btn-lg login-button"
-               href=${this.link}
-               disabled=${this.selected_provider !== undefined}>
-                ${I18n.t("js.sign_in_search_bar.log_in")}
-            </a>
+            <div class="input-group input-group-lg autocomplete">
+                    <d-datalist-input
+                        .options=${this.options}
+                        @input=${e => this.handleInput(e)}
+                        placeholder="${I18n.t("js.sign_in_search_bar.institution_search")}"
+                    ></d-datalist-input>
+                <a class="btn btn-primary btn-lg login-button"
+                   href=${this.link}
+                   disabled=${this.selected_provider !== undefined}>
+                    ${I18n.t("js.sign_in_search_bar.log_in")}
+                </a>
+            </div>
+
         `;
     }
 }

ca058f647463b0e10a78278a217a073e11edcc28
jorg-vr <jorg.vr@live.be>
2022-11-15 14:07:04 +0100

    Add elixir button on sign in screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -108,7 +108,7 @@ class User < ApplicationRecord
   has_many :annotations, dependent: :restrict_with_error
   has_many :questions, dependent: :restrict_with_error
 
-  devise :omniauthable, omniauth_providers: %i[google_oauth2 lti office365 oidc saml smartschool surf]
+  devise :omniauthable, omniauth_providers: %i[google_oauth2 lti office365 oidc saml smartschool surf elixir]
 
   validates :username, uniqueness: { case_sensitive: false, allow_blank: true, scope: :institution }
   validates :email, uniqueness: { case_sensitive: false, allow_blank: true, scope: :institution }

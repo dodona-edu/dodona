cf4494207eb3e14b8226a2a143745253efe53899
jorg-vr <jorg.vr@live.be>
2022-11-21 13:44:25 +0100

    Add extra scopes

--- lib/Elixir/setup.rb
+++ lib/Elixir/setup.rb
@@ -21,7 +21,7 @@ module Elixir
           {
             discovery: true,
             response_mode: :form_post,
-            scope: [:openid, :email],
+            scope: [:openid, :email, :profile, :shac_home_organisation],
             client_options: {
               host: "https://login.elixir-czech.org/oidc/",
               identifier: Rails.application.credentials.elixir_client_id,

b38fdd634b71efceb51e21dd3ea11874ce070209
tibvdm <tibovandemoortele@hotmail.com>
2022-11-23 21:08:12 +0100

    updated css / december == Christmas

--- app/helpers/temporal_helper.rb
+++ app/helpers/temporal_helper.rb
@@ -1,6 +1,6 @@
 module TemporalHelper
   def christmas(current_time)
-    return current_time.month == 12 && current_time.day.between?(25, 31)
+    current_time.month == 12
   end
 
   # returns temporal class

c5dc9ae42e8f6e68a0347374586a457704c7ede2
jorg-vr <jorg.vr@live.be>
2022-12-13 11:01:41 +0100

    Limit memory load

--- db/migrate/20221212094630_add_series_count_to_activities.rb
+++ db/migrate/20221212094630_add_series_count_to_activities.rb
@@ -2,7 +2,7 @@ class AddSeriesCountToActivities < ActiveRecord::Migration[7.0]
   def change
     add_column :activities, :series_count, :integer, :default => 0, :null => false
     Activity.reset_column_information
-    Activity.all.each do |e|
+    Activity.find_each do |e|
       Activity.reset_counters e.id, :series_memberships
     end
   end

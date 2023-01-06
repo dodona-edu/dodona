ae9d40413d403a0ed152b6944452441b8e19dde3
jorg-vr <jorg.vr@live.be>
2023-01-06 10:00:20 +0100

    Make sure activity statuses always exist

--- test/models/activity_status_test.rb
+++ test/models/activity_status_test.rb
@@ -47,4 +47,63 @@ class ActivityStatusTest < ActiveSupport::TestCase
     ActivityStatus.create(user: user, activity: activity, series: nil)
     assert_equal 1, ActivityStatus.count
   end
+
+  test 'activity status should always exist if a user has a submission' do
+    user = users(:student)
+    course = courses(:course1)
+    series = create :series, course: course, exercise_count: 1
+    activity = series.activities.first
+    create :submission, exercise: activity, course: course, status: :correct, user: user
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: series)
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: nil)
+  end
+
+  test 'activity status should always exist if a user has a activity read state' do
+    user = users(:student)
+    course = courses(:course1)
+    series = create :series, course: course, content_page_count: 1
+    activity = series.activities.first
+    create :activity_read_state, activity: activity, course: course, user: user
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: series)
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: nil)
+  end
+
+  test 'Activity status should be destroyed when series membership is destroyed' do
+    user = users(:student)
+    course = courses(:course1)
+    series = create :series, course: course, content_page_count: 1
+    activity = series.activities.first
+    create :activity_read_state, activity: activity, course: course, user: user
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: series)
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: nil)
+    SeriesMembership.find_by(series: series, activity: activity).destroy
+    assert_nil ActivityStatus.find_by(user: user, activity: activity, series: series)
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: nil)
+  end
+
+  test 'Activity status should be created when series membership is created' do
+    user = users(:student)
+    course = courses(:course1)
+    series = create :series, course: course, content_page_count: 1
+    series2 = create :series, course: course
+    activity = series.activities.first
+    create :activity_read_state, activity: activity, course: course, user: user
+    assert_nil ActivityStatus.find_by(user: user, activity: activity, series: series2)
+    SeriesMembership.create(series: series2, activity: activity)
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: series2)
+  end
+
+  test 'Activity status should be updated when series deadline is updated' do
+    user = users(:student)
+    course = courses(:course1)
+    series = create :series, course: course, content_page_count: 1, deadline: 1.week.from_now
+    activity = series.activities.first
+    create :activity_read_state, activity: activity, course: course, user: user
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: series)
+    assert ActivityStatus.find_by(user: user, activity: activity, series: series).accepted_before_deadline
+    series.deadline = Time.now - 1.week
+    series.save
+    assert_not_nil ActivityStatus.find_by(user: user, activity: activity, series: series)
+    assert_not ActivityStatus.find_by(user: user, activity: activity, series: series).accepted_before_deadline
+  end
 end

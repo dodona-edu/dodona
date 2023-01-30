a25ff558583d57d0e7605fc960a7a87cf3e9d565
jorg-vr <jorg.vr@live.be>
2023-01-30 10:11:51 +0100

    Replace delete by destroy

--- db/migrate/20230127090704_add_unique_index_to_evaluations.rb
+++ db/migrate/20230127090704_add_unique_index_to_evaluations.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexToEvaluations < ActiveRecord::Migration[7.0]
   def change
-    Evaluation.joins("Join evaluations b ON b.series_id = evaluations.series_id and b.id < evaluations.id").delete_all
+    Evaluation.joins("Join evaluations b ON b.series_id = evaluations.series_id and b.id < evaluations.id").destroy_all
     add_index :evaluations, :series_id, unique: true, name: 'index_evaluations_on_unique_series_id'
     remove_index :evaluations, :series_id, name: 'index_evaluations_on_series_id'
   end

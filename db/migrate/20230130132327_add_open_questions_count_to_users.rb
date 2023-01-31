4df0208bdce4c87960bf6c0a507000699b456ab2
jorg-vr <jorg.vr@live.be>
2023-01-31 09:27:59 +0100

    Comment migratrion query

--- db/migrate/20230130132327_add_open_questions_count_to_users.rb
+++ db/migrate/20230130132327_add_open_questions_count_to_users.rb
@@ -2,9 +2,9 @@ class AddOpenQuestionsCountToUsers < ActiveRecord::Migration[7.0]
   def self.up
     add_column :users, :open_questions_count, :integer, null: false, default: 0
 
-    User.find_each do |u|
-      u.update(open_questions_count: u.questions.where.not(question_state: :answered).count)
-    end
+    # User.find_each do |u|
+    #   u.update(open_questions_count: u.questions.where.not(question_state: :answered).count)
+    # end
   end
 
   def self.down

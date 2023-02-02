9b6c03a3b5699a88317896d68e7c03b7756e86be
jorg-vr <jorg.vr@live.be>
2023-02-02 11:41:54 +0100

    Comment out slow migration code

--- db/migrate/20230202085953_add_released_annotation_count_to_submissions.rb
+++ db/migrate/20230202085953_add_released_annotation_count_to_submissions.rb
@@ -2,7 +2,8 @@ class AddReleasedAnnotationCountToSubmissions < ActiveRecord::Migration[7.0]
   def self.up
     add_column :submissions, :released_annotation_count, :integer, null: false, default: 0
 
-    Annotation.counter_culture_fix_counts
+    # This should be run in production after the migration has been deployed
+    # Annotation.counter_culture_fix_counts
   end
 
   def self.down

38f4f17eb19f0d12dabe618b60ab71927b2cd600
jorg-vr <jorg.vr@live.be>
2023-02-02 14:59:03 +0100

    Uncomment migration query

--- db/migrate/20230202123413_add_annotated_to_submission.rb
+++ db/migrate/20230202123413_add_annotated_to_submission.rb
@@ -2,7 +2,6 @@ class AddAnnotatedToSubmission < ActiveRecord::Migration[7.0]
   def change
     add_column :submissions, :annotated, :boolean, default: false, null: false
 
-    # Run in console, to update existing submissions.
-    # Submission.where(id: Annotation.released.select(:submission_id).distinct).update_all(annotated: true)
+    Submission.where(id: Annotation.released.select(:submission_id).distinct).update_all(annotated: true)
   end
 end

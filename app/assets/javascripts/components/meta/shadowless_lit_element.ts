65097f9a57a4004b42868b0990f79bd0fc9a41d9
jorg-vr <jorg.vr@live.be>
2023-02-14 10:58:21 +0100

    Use campire plugin to support slots

--- app/assets/javascripts/components/meta/shadowless_lit_element.ts
+++ app/assets/javascripts/components/meta/shadowless_lit_element.ts
@@ -1,4 +1,5 @@
 import { LitElement } from "lit";
+import "@boulevard/vampire";
 
 /**
  * This class removes the shadow dom functionality from lit elements
@@ -10,10 +11,27 @@ import { LitElement } from "lit";
  *
  * This class is often used to avoid style scoping. (To be able to use the style as defined in our general css)
  * When shadow dom is required just use a normal LitElement
+ *
+ * it reintroduces the slot functionality using https://github.com/Boulevard/vampire
+ * Usage in component:
+ * html`
+ *      <h5>Example</h5>
+ *      <v-slot></v-slot>
+ *      <div class="footer">
+ *          <v-slot name="footer">Default footer</v-slot>
+ *      </div>
+ *     `
+ *
+ * Usage in parent:
+ * <my-component>
+ *     <div slot="footer">Footer</div>
+ *     <div>Content</div>
+ *     <div>Content</div>
+ * </my-component>
  */
 export class ShadowlessLitElement extends LitElement {
-    // don't use shadow dom
-    createRenderRoot(): Element {
+    createRenderRoot(): Element | ShadowRoot {
+        this.appendChild(document.createElement("v-root"));
         return this;
     }
 }

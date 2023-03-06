57bf42bc8745437c2e7cdd9480592ab6bf5a9e1b
jorg-vr <jorg.vr@live.be>
2023-03-06 15:20:04 +0100

    Name function more logically

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -49,7 +49,7 @@ class Annotation < ApplicationRecord
   before_validation :set_last_updated_by, on: :create
   before_validation :set_course_id, on: :create
   after_create :annotate_submission
-  after_create :answer_root_question
+  after_create :answer_previous_question
   after_destroy :destroy_notification
   after_destroy :reset_submission_annotated
   after_save :create_notification
@@ -98,7 +98,7 @@ class Annotation < ApplicationRecord
     [thread_root, thread_root&.responses&.where('created_at < ?', created_at)].flatten.last
   end
 
-  def answer_root_question
+  def answer_previous_question
     previous_annotation.update(question_state: :answered) if previous_annotation.is_a?(Question) && !previous_annotation.answered?
   end
 end

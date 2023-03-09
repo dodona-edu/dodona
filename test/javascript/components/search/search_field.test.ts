d07e5bb5bc1467bba284f5c9b3d0be40a73b6f47
jorg-vr <jorg.vr@live.be>
2023-03-09 11:04:32 +0100

    Add tests for searchfield

--- test/javascript/components/search/search_field.test.ts
+++ test/javascript/components/search/search_field.test.ts
@@ -1,9 +1,9 @@
 import "components/search_field";
-import { fixture, nextFrame, oneEvent } from "@open-wc/testing-helpers";
+import { aTimeout, fixture, nextFrame, oneEvent } from "@open-wc/testing-helpers";
 import userEvent from "@testing-library/user-event";
 import { screen } from "@testing-library/dom";
-import { SearchFieldSuggestion } from "components/search_field";
-import { Label } from "components/filter_collection_element";
+import { SearchField, SearchFieldSuggestion } from "components/search_field";
+import { Label, FilterCollection } from "components/filter_collection_element";
 import { html } from "lit";
 import { searchQuery } from "search";
 
@@ -47,7 +47,8 @@ describe("SearchFieldSuggestion", () => {
                                        labels='[{ "name": "fool", "id": "1" }, { "name": "bar", "id": "2" }, { "name": "baz", "id": "3" }]'
                                        type="test"
                                        .paramVal=${(l: Label) => l.id}
-                                       filter="ba">
+                                       filter="ba"
+                                        .multi=${false}>
             </d-search-field-suggestion>`) as SearchFieldSuggestion;
     });
 
@@ -66,7 +67,7 @@ describe("SearchFieldSuggestion", () => {
     });
 
     it("should set the query param to the selected label", async () => {
-        screen.getByText(textContentMatcher("bar")).click();
+        await userEvent.click(screen.getByText(textContentMatcher("bar")));
         expect(searchQuery.queryParams.params.get("foo")).toBe("2");
         expect(searchQuery.queryParams.params.get("filter")).toBe(undefined);
     });
@@ -77,5 +78,88 @@ describe("SearchFieldSuggestion", () => {
         expect(screen.queryByText(textContentMatcher("bar"))).toBeNull();
         expect(screen.queryByText(textContentMatcher("baz"))).toBeNull();
         expect(screen.queryByText(textContentMatcher("fool"))).toBeNull();
+    });
+
+    it("should not display already selected labels", async () => {
+        await userEvent.click(screen.getByText(textContentMatcher("bar")));
+        searchFieldSuggestion.filter = "ba";
+        await nextFrame();
+        expect(screen.queryByText(textContentMatcher("bar"))).toBeNull();
+    });
+});
+
+describe("SearchField", () => {
+    let searchField: SearchField;
+    beforeEach(async () => {
+        const filterCollections: Record<string, FilterCollection> = {
+            first: {
+                param: "foo",
+                data: [
+                    { name: "fool", id: "1" },
+                    { name: "bar", id: "2" },
+                    { name: "baz", id: "3" },
+                ],
+                multi: false,
+                color: () => "red",
+                paramVal: (l: Label) => l.id,
+            },
+            second: {
+                param: "bar",
+                data: [
+                    { name: "food", id: "1" },
+                    { name: "barn", id: "2" },
+                    { name: "baz", id: "3" },
+                ],
+                multi: false,
+                color: () => "red",
+                paramVal: (l: Label) => l.id,
+            }
+        };
+
+        searchField = await fixture(html`
+            <d-search-field placeholder="Search"
+                            .filterCollections="${filterCollections}"
+            ></d-search-field>
+        `);
+    });
+
+    it("should display the placeholder", async () => {
+        const input = searchField.querySelector("input");
+        expect(input.placeholder).toBe("Search");
+    });
+
+    it("should update the filter query param when the input changes", async () => {
+        const input = searchField.querySelector("input");
+        await userEvent.type(input, "foo");
+        await aTimeout(500); // the update is delayed
+        expect(searchQuery.queryParams.params.get("filter")).toBe("foo");
+    });
+
+    it("should filter the labels", async () => {
+        const input = searchField.querySelector("input");
+        await userEvent.type(input, "foo");
+        expect(screen.queryByText(textContentMatcher("bar"))).toBeNull();
+        expect(screen.queryByText(textContentMatcher("baz"))).toBeNull();
+        expect(screen.queryByText(textContentMatcher("fool"))).not.toBeNull();
+        expect(screen.queryByText(textContentMatcher("food"))).not.toBeNull();
+    });
+
+    it("should autocomplete the input with the first label on tab", async () => {
+        const input = searchField.querySelector("input");
+        await userEvent.type(input, "foo{tab}");
+        await aTimeout(500); // the update is delayed
+        expect(searchQuery.queryParams.params.get("filter")).toBeFalsy();
+        expect(searchQuery.queryParams.params.get("foo")).toBe("1");
+        expect(searchQuery.queryParams.params.get("bar")).toBeFalsy();
+    });
+
+    it("should reset the filter when a label is selected", async () => {
+        const input = searchField.querySelector("input");
+        await userEvent.type(input, "foo");
+        await aTimeout(500); // the update is delayed
+        expect(searchQuery.queryParams.params.get("filter")).toBe("foo");
+        await userEvent.click(screen.getByText(textContentMatcher("fool")));
+        expect(searchQuery.queryParams.params.get("filter")).toBeFalsy();
+        expect(input.value).toBe("");
     });
 });

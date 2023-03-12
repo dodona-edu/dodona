d7909fbd7a39df2b99824ef077a265877d2b8c64
tibvdm <tibovandemoortele@hotmail.com>
2023-03-13 00:23:00 +0100

    a delicious pie for everyone

--- app/helpers/seasonal_helper.rb
+++ app/helpers/seasonal_helper.rb
@@ -11,6 +11,10 @@ module SeasonalHelper
     current_time.month == 3 && current_time.day == 10
   end
 
+  def pi_day(current_time)
+    current_time.month == 3 && current_time.day == 13
+  end
+
   # returns seasonal class
   def series_status_overlay
     current_time = Time.now.in_time_zone(config.time_zone)
@@ -18,6 +22,7 @@ module SeasonalHelper
     return 'christmas' if christmas(current_time)
     return 'valentine' if valentine(current_time)
     return 'mario-day' if mario_day(current_time)
+    return 'pi-day'    if pi_day(current_time)
 
     nil
   end

cbd606a3e6c8026a9b180fb3cb3e08fe546260ff
jorg-vr <jorg.vr@live.be>
2023-04-20 14:29:38 +0200

    Remove vampireslot from marker

--- app/assets/javascripts/components/annotations/machine_annotation_marker.ts
+++ app/assets/javascripts/components/annotations/machine_annotation_marker.ts
@@ -1,9 +1,6 @@
 import { customElement, property } from "lit/decorators.js";
-import { html, TemplateResult } from "lit";
+import { render, html, LitElement, TemplateResult, PropertyValues } from "lit";
 import { MachineAnnotationData } from "state/MachineAnnotations";
-import { ShadowlessLitElement } from "components/meta/shadowless_lit_element";
-import { VampireSlot } from "@boulevard/vampire";
-import { ref } from "lit/directives/ref.js";
 import tippy, { Instance as Tippy, createSingleton } from "tippy.js";
 
 /**
@@ -14,14 +11,9 @@ import tippy, { Instance as Tippy, createSingleton } from "tippy.js";
  * @element d-machine-annotation-marker
  */
 @customElement("d-machine-annotation-marker")
-export class MachineAnnotationMarker extends ShadowlessLitElement {
+export class MachineAnnotationMarker extends LitElement {
     @property({ type: Array })
     annotations: MachineAnnotationData[];
-
-    // We need this to apply different styles to the marker when it is empty.
-    // We can't use the :empty pseudo selector because the vampire slot is always present even if it is empty.
-    @property({ state: true })
-    empty: boolean;
 
     static tippyInstances: Tippy[] = [];
     static tippySingleton = createSingleton([], {
@@ -38,45 +30,39 @@ export class MachineAnnotationMarker extends ShadowlessLitElement {
         this.tippySingleton.setInstances(this.tippyInstances);
     }
 
-    tippy: Tippy;
+    protected firstUpdated(_changedProperties: PropertyValues): void {
+        super.firstUpdated(_changedProperties);
+        const tooltip = document.createElement("div");
+        tooltip.classList.add("marker-tooltip");
+        render(this.annotations.map(a => html`<d-machine-annotation .data=${a}></d-machine-annotation>`), tooltip);
 
-    setEmpty(slot: VampireSlot): void {
-        this.empty = slot.assignedNodes().length === 0;
-    }
-
-    initSlot(slot: VampireSlot): void {
-        if (!slot) {
-            return;
-        }
-
-        slot.addEventListener("v::slotchange", () => this.setEmpty(slot));
-        if (this.empty === undefined) {
-            this.setEmpty(slot);
-        }
-    }
-
-    initTooltip(tooltip: HTMLDivElement): void {
-        if (!tooltip || this.tippy) {
-            return;
-        }
-
-        this.tippy = tippy(this, {
+        const t = tippy(this, {
             content: tooltip,
         });
-        MachineAnnotationMarker.registerTippyInstance(this.tippy);
+        MachineAnnotationMarker.registerTippyInstance(t);
     }
 
-    get markClasses(): string {
-        const hiddenTypes = this.annotations.map(a => a.type);
-        return [...new Set(hiddenTypes)].map(t => `mark-${t}`).join(" ");
+    get markColor(): string {
+        const types = new Set(this.annotations.map(a => a.type));
+        if (types.has("error")) {
+            return "var(--error-color, red)";
+        } else if (types.has("warning")) {
+            return "var(--warning-color, yellow)";
+        } else if (types.has("info")) {
+            return "var(--info-color, blue)";
+        } else {
+            return undefined;
+        }
     }
 
     render(): TemplateResult {
-        return html`<span class="${this.markClasses} ${this.empty ? "mark-empty" : ""}"
-        ><v-slot ${ref(s => this.initSlot(s as VampireSlot))}
-        ></v-slot
-        ></span
-        ><div ${ref(t => this.initTooltip(t as HTMLDivElement))} class="marker-tooltip">${this.annotations.map(a => html`<d-machine-annotation .data=${a}></d-machine-annotation>`)}
-        </div>`;
+        return html`<style>
+            :host {
+                position: relative;
+                text-decoration: wavy underline ${this.markColor};
+            }
+        </style><slot><svg style="position: absolute; top: 9px; left: -7px" width="14" height="14" viewBox="0 0 24 24">
+            <path fill="${this.markColor}" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6l1.41 1.41Z"/>
+        </svg></slot>`;
     }
 }

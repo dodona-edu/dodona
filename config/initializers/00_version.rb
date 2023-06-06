5ed88f6b92d8ec3f989e890729b704e0822376f2
jorg-vr <jorg.vr@live.be>
2023-06-06 10:48:12 +0200

    Fix tests

--- config/initializers/00_version.rb
+++ config/initializers/00_version.rb
@@ -1,5 +1,7 @@
 module Dodona
   class Application
-    VERSION = Time.now.strftime("%y.%m.%d-%H:%M").freeze
+    VERSION = Time.now.strftime("%y.%m.%d-%H:%M")
+
+    MIN_SUPPORTED_CLIENT = '0.0.1'.freeze
   end
 end

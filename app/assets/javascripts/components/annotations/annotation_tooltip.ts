71187b90c7d0fd6d607909a2fc156f141a1598c0
jorg-vr <jorg.vr@live.be>
2023-07-28 15:47:18 +0200

    Improve understanadibility of annotation_tooltip

--- app/assets/javascripts/components/annotations/annotation_tooltip.ts
+++ app/assets/javascripts/components/annotations/annotation_tooltip.ts
@@ -22,6 +22,7 @@ export class AnnotationTooltip extends LitElement {
 
     state = new StateController(this);
 
+    // we need to keep track of all tippy instances to update the singleton
     static tippyInstances: Tippy[] = [];
     // Using a singleton to avoid multiple tooltips being open at the same time.
     static tippySingleton = createSingleton([], {
@@ -34,50 +35,42 @@ export class AnnotationTooltip extends LitElement {
         moveTransition: "transform 0.001s ease-out",
         appendTo: () => document.querySelector(".code-table"),
     });
+
+    /**
+     * Updates the tippy singleton with the current list of tippy instances.
+     * This method is debounced to avoid updating the singleton too often as it is expensive.
+     */
     static updateSingletonInstances(): void {
         setInstancesDelayer(() => this.tippySingleton.setInstances(this.tippyInstances), 100);
     }
+
+    /**
+     * Adds a tippy instance to the list of instances, which will be used to update the singleton.
+     */
     static registerTippyInstance(instance: Tippy): void {
         this.tippyInstances.push(instance);
         this.updateSingletonInstances();
     }
+
+    /**
+     * Removes a tippy instance from the list of instances, which will be used to update the singleton.
+     */
     static unregisterTippyInstance(instance: Tippy): void {
         this.tippyInstances = this.tippyInstances.filter(i => i !== instance);
         this.updateSingletonInstances();
     }
 
-    // Annotations that are displayed inline should show up as tooltips.
+    // Annotations that are not displayed inline should show up as tooltips.
     get hiddenAnnotations(): AnnotationData[] {
         return this.annotations.filter(a => !annotationState.isVisible(a)).sort(compareAnnotationOrders);
     }
 
     tippyInstance: Tippy;
 
-    renderTooltip(): void {
-        if (this.tippyInstance) {
-            AnnotationTooltip.unregisterTippyInstance(this.tippyInstance);
-            this.tippyInstance.destroy();
-            this.tippyInstance = undefined;
-        }
-
-        if (this.hiddenAnnotations.length === 0) {
-            return;
-        }
-
-        const tooltip = document.createElement("div");
-        tooltip.classList.add("marker-tooltip");
-        render(this.hiddenAnnotations.map(a => isUserAnnotation(a) ?
-            html`<d-user-annotation .data=${a}></d-user-annotation>` :
-            html`<d-machine-annotation .data=${a}></d-machine-annotation>`), tooltip);
-
-        this.tippyInstance = tippy(this, {
-            content: tooltip,
-        });
-        AnnotationTooltip.registerTippyInstance(this.tippyInstance);
-    }
-
     disconnectedCallback(): void {
         super.disconnectedCallback();
+        // before destroying this element, we need to clean up the tippy instance
+        // and make sure it is removed from the singleton
         if (this.tippyInstance) {
             AnnotationTooltip.unregisterTippyInstance(this.tippyInstance);
             this.tippyInstance.destroy();
@@ -86,7 +79,27 @@ export class AnnotationTooltip extends LitElement {
     }
 
     render(): TemplateResult {
-        this.renderTooltip();
+        // Clean up the previous tippy instance if it exists.
+        if (this.tippyInstance) {
+            AnnotationTooltip.unregisterTippyInstance(this.tippyInstance);
+            this.tippyInstance.destroy();
+            this.tippyInstance = undefined;
+        }
+
+        if (this.hiddenAnnotations.length > 0) {
+            const tooltip = document.createElement("div");
+            tooltip.classList.add("marker-tooltip");
+            render(this.hiddenAnnotations.map(a => isUserAnnotation(a) ?
+                html`
+                    <d-user-annotation .data=${a}></d-user-annotation>` :
+                html`
+                    <d-machine-annotation .data=${a}></d-machine-annotation>`), tooltip);
+
+            this.tippyInstance = tippy(this, {
+                content: tooltip,
+            });
+            AnnotationTooltip.registerTippyInstance(this.tippyInstance);
+        }
 
         // if slot is empty, render an empty svg to make sure the tooltip is positioned correctly
         return html`<slot><svg style="position: absolute; top: 9px; left: -7px" width="14" height="14" viewBox="0 0 24 24">

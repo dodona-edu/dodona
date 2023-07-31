f5d1626341be5b77e5803480a82d4a16733117b0
freyavs <freyavanspeybroeck@outlook.com>
2023-07-31 22:18:16 +0200

    remove headers that are already set by util fetch

--- app/assets/javascripts/question_table.ts
+++ app/assets/javascripts/question_table.ts
@@ -78,8 +78,6 @@ export class QuestionTable {
         fetch(this.getRefreshUrl(), {
             headers: {
                 "accept": "text/javascript",
-                "x-csrf-token": document.querySelector("meta[name=\"csrf-token\"]").getAttribute("content"),
-                "x-requested-with": "XMLHttpRequest",
             },
             credentials: "same-origin",
         })

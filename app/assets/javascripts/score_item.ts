18064257685f13eb31a0bdd7e98d1bcbd5608018
freyavs <freyavanspeybroeck@outlook.com>
2023-07-31 23:46:27 +0200

    remove unused functions from score_item

--- app/assets/javascripts/score_item.ts
+++ app/assets/javascripts/score_item.ts
@@ -1,23 +1,5 @@
 import { fetch } from "util.js";
 
-export function initInlineEditButton(tableElement: HTMLElement): void {
-    tableElement.querySelectorAll(".edit-button").forEach(item => {
-        item.addEventListener("click", e => {
-            e.preventDefault();
-            const clicked = (e.target as HTMLElement).closest("a") as HTMLAnchorElement;
-            const scoreItemId = clicked.dataset.scoreItem;
-            const row = document.getElementById(`form-row-${scoreItemId}`);
-            if (row.classList.contains("hidden")) {
-                row.classList.remove("hidden");
-                clicked.innerHTML = "<i class='mdi mdi-close mdi-18' aria-hidden='true'></i>";
-            } else {
-                row.classList.add("hidden");
-                clicked.innerHTML = "<i class='mdi mdi-pencil mdi-18' aria-hidden='true'></i>";
-            }
-        });
-    });
-}
-
 function commonCheckboxInit(
     element: HTMLElement,
     selector: string,
@@ -70,38 +52,6 @@ function initItemVisibilityCheckboxes(element: HTMLElement): void {
             }
         };
     });
-}
-
-export function initScoreItemPanels(): void {
-    const $choicePanel = $("#choice-panel");
-    const $itemPanel = $("#items-panel");
-
-    function init(): void {
-        initPanelLogic();
-        // // Bootstrap's automatic collapsing of other elements in the parent breaks
-        // // when doing manual shows and hides, so we have to do this.
-        $choicePanel.find(".panel-collapse").on("show.bs.collapse", function () {
-            $itemPanel.find(".panel-collapse").collapse("hide");
-        });
-        $itemPanel.find(".panel-collapse").on("show.bs.collapse", function () {
-            $choicePanel.find(".panel-collapse").collapse("hide");
-        });
-    }
-
-    function initPanelLogic(): void {
-        $("#yes-grading").on("click", function () {
-            $itemPanel.find(".step-circle").html("2");
-            $(this)
-                .closest(".panel")
-                .find(".answer")
-                .html($(this).data("answer"));
-            $itemPanel.removeClass("hidden");
-            $itemPanel.find(".panel-collapse").collapse("show");
-            window.scrollTo(0, 0);
-        });
-    }
-
-    init();
 }
 
 export function initVisibilityCheckboxes(element: HTMLElement): void {

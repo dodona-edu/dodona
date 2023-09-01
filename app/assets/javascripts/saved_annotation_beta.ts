8f0b53170d0ba681272440638612626c5f9fa125
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-09-01 17:17:06 +0200

    Add computergebruik to re-use beta

--- app/assets/javascripts/saved_annotation_beta.ts
+++ app/assets/javascripts/saved_annotation_beta.ts
@@ -2,7 +2,7 @@
 
 import { courseState } from "state/Courses";
 
-const BETA_COURSES = new Set([10, 773, 1151, 1659, 2258, 2263]);
+const BETA_COURSES = new Set([10, 773, 1151, 1659, 1662, 2258, 2263]);
 
 export function isBetaCourse(courseId?: number): boolean {
     return BETA_COURSES.has(courseId || courseState.id );

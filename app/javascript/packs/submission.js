36188c8c82efa81d89e0c5d904911300fc3e644c
jorg-vr <jorg.vr@live.be>
2023-10-23 10:01:31 +0200

    Remove pythia_submissions pack

--- app/javascript/packs/submission.js
+++ app/javascript/packs/submission.js
@@ -5,6 +5,7 @@ import { evaluationState } from "state/Evaluations";
 import codeListing from "code_listing";
 import { annotationState } from "state/Annotations";
 import { initTutor } from "tutor";
+import { initFileViewers } from "file_viewer";
 
 window.dodona.initSubmissionShow = initSubmissionShow;
 window.dodona.codeListing = codeListing;
@@ -16,6 +17,7 @@ window.dodona.setEvaluationId = id => evaluationState.id = id;
 window.dodona.setAnnotationVisibility = visibility => annotationState.visibility = visibility;
 window.dodona.showLastTab = showLastTab;
 window.dodona.initTutor = initTutor;
+window.dodona.initFileViewers = initFileViewers;
 
 // will automatically bind to window.iFrameResize()
 require("iframe-resizer"); // eslint-disable-line no-undef

2090dcaa465e51aabc8fdef2cf153c71e034b40e
jorg-vr <jorg.vr@live.be>
2023-10-26 15:26:29 +0200

    Add comment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
+# These files are fetched from within other js assets
+# so they do not support rails sprockets renaming them
+# We need to provide them as assets without digest
 Rails.application.config.assets.nodigest = ['*.nodigest.js', '*.load_by_url']

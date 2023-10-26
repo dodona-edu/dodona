e1e6b0a2b43502e96b736afef20faa7ef1f9d0e6
jorg-vr <jorg.vr@live.be>
2023-10-26 14:30:59 +0200

    Do not fallback compile on naos

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :terser
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true

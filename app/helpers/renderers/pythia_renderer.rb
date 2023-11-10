9f94da16db40b2729f1fd2f3189d0c0cf6c404ae
Bart Mesuere <Bart.Mesuere@UGent.be>
2023-11-10 13:07:27 +0100

    remove row class

--- app/helpers/renderers/pythia_renderer.rb
+++ app/helpers/renderers/pythia_renderer.rb
@@ -65,7 +65,7 @@ class PythiaRenderer < FeedbackTableRenderer
         if @exercise.access_private? && value&.dig(:location) == 'href'
       [key, value]
     end.to_json
-    @builder.div(class: "row testcase #{tc[:accepted] ? 'correct' : 'wrong'} contains-file", 'data-files': jsonfiles) do
+    @builder.div(class: "testcase #{tc[:accepted] ? 'correct' : 'wrong'} contains-file", 'data-files': jsonfiles) do
       testcase_content(tc)
     end
   end

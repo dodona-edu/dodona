5c582c8d8498b38445bca1216de7b786a0b2e381
Bart Mesuere <Bart.Mesuere@UGent.be>
2023-11-13 11:59:58 +0100

    remove terser

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :terser
+  # config.assets.js_compressor = :terser
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false

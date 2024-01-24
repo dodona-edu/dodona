0897d8f5221e157462c1ef952afe9ad0220ed52d
jorg-vr <jorg.vr@live.be>
2024-01-24 10:38:05 +0100

    Remove i18n mixin form shadowless lit element

--- app/assets/javascripts/components/meta/shadowless_lit_element.ts
+++ app/assets/javascripts/components/meta/shadowless_lit_element.ts
@@ -1,5 +1,4 @@
 import { LitElement } from "lit";
-import { i18nMixin } from "components/meta/i18n_mixin";
 
 /**
  * This class removes the shadow dom functionality from lit elements
@@ -12,7 +11,7 @@ import { i18nMixin } from "components/meta/i18n_mixin";
  * This class is often used to avoid style scoping. (To be able to use the style as defined in our general css)
  * When shadow dom is required just use a normal LitElement
  */
-export class ShadowlessLitElement extends i18nMixin(LitElement) {
+export class ShadowlessLitElement extends LitElement {
     // don't use shadow dom
     createRenderRoot(): HTMLElement {
         return this;

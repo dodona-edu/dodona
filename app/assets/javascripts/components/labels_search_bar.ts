8604ff5f15e4a559090e134463330f4dff9502cb
jorg-vr <jorg.vr@live.be>
2024-02-23 13:35:34 +0100

    Generalise existing label search

--- app/assets/javascripts/components/labels_search_bar.ts
+++ app/assets/javascripts/components/labels_search_bar.ts
@@ -5,15 +5,18 @@ import "components/datalist_input";
 import { i18n } from "i18n/i18n";
 import { DodonaElement } from "components/meta/dodona_element";
 /**
- * This component represents a list of the selected course labels
+ * This component represents a list of the selected labels
  *
  * @element d-course-labels
  * @prop {string[]} Labels - the labels of a user in a certain course
+ * @prop {string} Name - the name of the input field (used in form submit)
  */
-@customElement("d-course-labels")
-export class CourseLabelTokens extends DodonaElement {
+@customElement("d-labels")
+export class LabelTokens extends DodonaElement {
     @property({ type: Array })
     labels: string[];
+    @property({ type: String })
+    name: string;
 
     processClick(e: Event, label: string): void {
         this.labels.splice(this.labels.indexOf(label), 1);
@@ -33,31 +36,32 @@ export class CourseLabelTokens extends DodonaElement {
                         </span>
                     </span>
             `)}
-            <input type="hidden" name="course_membership[course_labels]" .value="${this.labels.join(",")}">
+            <input type="hidden" name="${this.name}" .value="${this.labels.join(",")}">
         `;
     }
 }
 
 /**
- * This component represents a search bar for course labels, also showing a list of already selected labels.
+ * This component represents a search bar for labels, also showing a list of already selected labels.
  * It allows for searching existing labels, or creating new ones, and adding them to the user of a certain course
  *
- * @element d-course-labels-search-bar
+ * @element d-labels-search-bar
  *
  * @prop {{id: number, name: string}[]} Labels - all the labels already used in a course
  * @prop {string[]} SelectedLabels - the labels that have been added to the user
+ * @prop {string} Name - the name of the input field (used in form submit)
  */
-@customElement("d-course-labels-search-bar")
-export class CourseLabelsSearchBar extends DodonaElement {
+@customElement("d-labels-search-bar")
+export class LabelsSearchBar extends DodonaElement {
     @property({ type: Array })
     labels: {id: number, name: string}[];
-
     @property({ type: Array })
     selected_labels: string[];
+    @property({ type: String })
+    name: string;
 
     @property({ state: true })
     selected_label: string;
-
     @property({ state: true })
     filter: string;
 
@@ -88,19 +92,20 @@ export class CourseLabelsSearchBar extends DodonaElement {
     render(): TemplateResult {
         return html`
             <div>
-                <d-course-labels
+                <d-labels
                     .labels=${this.selected_labels}
-                ></d-course-labels>
+                    .name=${this.name}
+                ></d-labels>
                 <div class="labels-searchbar-group input-group autocomplete">
                     <d-datalist-input
                         .filter="${this.filter}"
                         .options=${this.options}
                         @input=${e => this.handleInput(e)}
-                        placeholder="${i18n.t("js.course_labels_search_bar.placeholder")}"
+                        placeholder="${i18n.t("js.labels_search_bar.placeholder")}"
                     ></d-datalist-input>
-                    <a type="button" class="btn btn-filled add-button" @click="${this.addLabel}">${i18n.t("js.course_labels_search_bar.add")}</a>
+                    <a type="button" class="btn btn-filled add-button" @click="${this.addLabel}">${i18n.t("js.labels_search_bar.add")}</a>
                 </div>
-                <span class="help-block">${i18n.t("js.course_labels_search_bar.edit_explanation")}</span>
+                <span class="help-block">${i18n.t("js.labels_search_bar.edit_explanation")}</span>
             </div>
         `;
     }

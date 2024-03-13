b08c1e7f39e0e98bc69d159f8a0017a67462b6fd
jorg-vr <jorg.vr@live.be>
2024-03-13 14:49:31 +0100

    Improve string matcher

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -1,5 +1,5 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  class_attr_index = html_tag.index 'class="'
+  class_attr_index = html_tag.index 'class='
 
   if class_attr_index
     html_tag.insert class_attr_index+7, 'is-invalid '

76b990758c7bf76038e1ab9502c705119f3b97c6
jorg-vr <jorg.vr@live.be>
2024-03-18 16:41:29 +0100

    Add comments

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -2,8 +2,11 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   class_attr_index = html_tag.index 'class='
 
   if class_attr_index
+    # insert the is-invalid class as first class in the class attribute
+    # 7 is the length of string class="
     html_tag.insert class_attr_index+7, 'is-invalid '
   else
+    # if there is no class attribute, create one
     html_tag.insert html_tag.index('>'), ' class="is-invalid"'
   end
 end

e9e08872e57d2c47c268c2027a56525e4befa69c
jorg-vr <jorg.vr@live.be>
2024-05-13 16:14:27 +0200

    Add documentation

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -1,7 +1,16 @@
+# This concern allows models to define scopes for filtering by columns
+# These scopes are created with the name `by_#{column}`
+# and a correlating function that counts the possible values for the column is also created
 module Filterable
   extend ActiveSupport::Concern
 
   class_methods do
+    # Creates a scope called by_filter that searches for the give value in the search column
+    # Search should be a string column defined in the model
+    # It will be set to the concatenation of the values of the columns passed to the search_by method
+    # This is useful for searching for a value in multiple columns in an efficient way
+    # params:
+    # +columns+:: The columns to search for
     def search_by(*columns)
       define_method(:set_search) do
         self.search = columns.map { |column| send(column) || '' }.join(' ')
@@ -23,22 +32,30 @@ module Filterable
     # +name_hash+:: a lambda that takes a list af column values and returns a hash with the human readable name for each column value
     def filterable_by(name, column: name, associations: [], multi: false, is_enum: false, model: nil, name_hash: nil) # rubocop:disable Metrics/ParameterLists
       value_check = if is_enum
+                      # We should only allow filtering with valid options in the enum
                       ->(value) { value.in? send(column.to_s.pluralize.to_s) }
+                    elsif multi
+                      # If the scope accepts multiple values, the values should be an array
+                      ->(value) { value.is_a?(Array) }
                     else
                       ->(_) { true }
                     end
 
       name_hash ||= if is_enum
+                      # If the column is an enum, we should use the human readable name for the values
                       ->(values) { values.index_with { |s| human_enum_name(column.to_s.pluralize.to_s, s) } }
                     elsif model
+                      # If the column is a foreign key, we should use the name of the object
                       ->(values) { model.where(id: values).to_h { |s| [s.id, s.name] } }
                     else
+                      # Otherwise, we should just use the value itself
                       ->(values) { values.to_h { |s| [s, s] } }
                     end
 
       scope "by_#{name}", lambda { |value|
-        if value_check.call(value) && (!multi || value.is_a?(Array))
+        if value_check.call(value)
           scope = joins(associations).where(column => value)
+          # If the scope accepts multiple values, we should only return the results that match all the values
           scope = scope.group(:id).having("COUNT(DISTINCT(#{column})) = ?", value.uniq.length) if multi
           scope
         else
@@ -46,16 +63,24 @@ module Filterable
         end
       }
 
+      # The method that returns the possible values for the column
+      # It returns a list of hashes with the id, name and count of each value
       define_singleton_method("#{name}_filter_options") do
+        # the current scope might already have a group by clause
+        # to get be able to write a new group by clause we reselect the currently filtered elements by id
         count = unscoped.where(id: select(:id)).joins(associations).group(column).count
 
         names = name_hash.call(count.keys)
 
         count.map { |key, value| { id: key.to_s, name: names[key].to_s, count: value } }
-             .filter { |option| option[:name].present? }
+             .filter { |option| option[:name].present? } # Remove empty values
       end
     end
 
+    # Creates a scope for course_labels, with the name `by_course_labels`
+    # It also creates a method `course_labels_filter_options`
+    # that returns the possible values for the column, with the count of each value
+    # This is a special case of the filterable_by method, as the course_id is required to get the course_labels
     def filterable_by_course_labels(through_user: false)
       if through_user
         has_many :course_memberships, through: :user

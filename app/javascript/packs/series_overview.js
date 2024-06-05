273c817e2fdd8340fb4a84af5bbd01e502753b64
jorg-vr <jorg.vr@live.be>
2024-06-05 10:05:28 +0200

    Fix series overview page

--- app/javascript/packs/series_overview.js
+++ app/javascript/packs/series_overview.js
@@ -1,8 +1,11 @@
-import { initExerciseDescription } from "exercise.ts";
+import { initExerciseDescription, afterResize, onFrameMessage, onFrameScroll } from "exercise.ts";
 import { initSeriesShow } from "series";
 
 window.dodona.initExerciseDescription = initExerciseDescription;
 window.dodona.initSeriesShow = initSeriesShow;
 
+window.dodona.afterResize = afterResize;
+window.dodona.onFrameMessage = onFrameMessage;
+window.dodona.onFrameScroll = onFrameScroll;
 // will automatically bind do window.iFrameResize()
 require("iframe-resizer"); // eslint-disable-line no-undef

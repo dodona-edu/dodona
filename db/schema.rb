d8d29d42d13d9c995a013a970b50d745a521bb3f
jorg-vr <jorg.vr@live.be>
2024-06-17 16:22:05 +0200

    Also save seriesid for content pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_111643) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_17_141422) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -82,8 +82,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_111643) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "series_id"
     t.index ["activity_id", "course_id", "user_id"], name: "activity_read_states_unique", unique: true
     t.index ["course_id"], name: "fk_rails_f674cacc14"
+    t.index ["series_id"], name: "index_activity_read_states_on_series_id"
     t.index ["user_id"], name: "fk_rails_96d00253e9"
   end
 

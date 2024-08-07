0df802b55c204405c0cace1beb0f0b432930e7bd
jorg-vr <jorg.vr@live.be>
2024-08-07 14:10:05 +0200

    Disable unwanted options

--- app/assets/javascripts/components/input_table.ts
+++ app/assets/javascripts/components/input_table.ts
@@ -16,6 +16,16 @@ export class DInputTable extends DodonaElement {
 
     tableRef: Ref<HTMLDivElement> = createRef();
 
+    get tableWidth(): number {
+        return this.tableRef.value.clientWidth;
+    }
+
+    get descriptionColWidth(): number {
+        // full width - borders - name column - maximum column - visible column - index column
+        const variableWidth = this.tableWidth - 6 - 200 - 100 - 100 - 50;
+        return Math.max(200, variableWidth);
+    }
+
     protected firstUpdated(_changedProperties: PropertyValues): void {
         super.firstUpdated(_changedProperties);
         const table: JspreadsheetInstance = jspreadsheet(this.tableRef.value, {
@@ -24,11 +34,28 @@ export class DInputTable extends DodonaElement {
                 ["Test", "Test", 1, true],
             ],
             columns: [
-                { type: "text", title: "Naam", width: 120 },
-                { type: "text", title: "Beschrijving", width: 120 },
-                { type: "numeric", title: "Maximum", width: 120 },
-                { type: "checkbox", title: "Zichtbaar", width: 120 }
-            ]
+                { type: "text", title: "Naam", width: 200, align: "left" },
+                { type: "text", title: "Beschrijving", width: this.descriptionColWidth, align: "left" },
+                { type: "numeric", title: "Maximum", width: 100 },
+                { type: "checkbox", title: "Zichtbaar", width: 100 }
+            ],
+            about: false,
+            allowDeleteColumn: false,
+            allowDeleteRow: true,
+            allowInsertColumn: false,
+            allowInsertRow: true,
+            allowManualInsertColumn: false,
+            allowManualInsertRow: true,
+            allowRenameColumn: false,
+            columnResize: false,
+            columnSorting: false,
+            csvFileName: "scoresheet",
+            minSpareRows: 1,
+            parseFormulas: false,
+            selectionCopy: false,
+            wordWrap: true
+
+
         });
     }
 

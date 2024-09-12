3cbdb0514b8d50a12d005a02d925ac9f9f839c06
jorg-vr <jorg.vr@live.be>
2024-09-12 16:01:22 +0200

    Update comment

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,7 @@ module Dodona
 
     config.dodona_email = 'dodona@ugent.be'
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    # Don't eager load the lib directory, this causes issues with the autoloader
     # config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.

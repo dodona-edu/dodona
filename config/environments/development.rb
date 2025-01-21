c15ad7ff3cb0dd1c68f86a4ae818b35712bec374
jorg.vr <jorg.vanrenterghem@ugent.be>
2025-01-21 14:50:20 +0100

    Disable dev caching by default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,15 +43,15 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
+    config.cache_store = :memory_store
     config.public_file_server.headers.merge!({
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     })
   else
     config.action_controller.perform_caching = false
-  end
 
-  # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+    config.cache_store = :null_store
+  end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
